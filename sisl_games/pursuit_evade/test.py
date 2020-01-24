1da3f18c547edbd72747b5faba2c5569025d2872
AnanthHari <hariananth92@gmail.com>
2020-01-24 16:27:39 -0500

    Added RLlib wrapper to pursuit_evade

--- sisl_games/pursuit_evade/test.py
+++ sisl_games/pursuit_evade/test.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from .pursuit_evade import PursuitEvade
+from .pursuit_evade import env as _env
+import time
 
-from .utils import two_d_maps
+# from .utils import two_d_maps
 
 xs = 5
 ys = 5
 n_evaders = 1
 n_pursuers = 2
 
-map_mat = two_d_maps.rectangle_map(xs, ys) 
+# map_mat = two_d_maps.rectangle_map(xs, ys) 
 
 # obs_range should be odd 3, 5, 7, etc
-env = PursuitEvade([map_mat], n_evaders=n_evaders, n_pursuers=n_pursuers, obs_range=3, n_catch=2, surround=False,
-        reward_mech='local')
+env = _env() #n_evaders=n_evaders, n_pursuers=n_pursuers)
 
 done = False
 
 global _quit_loop, _totalReward, _actions
 _quit_loop = 0
 _totalReward = 0
-_actions = np.array([4]*env.n_pursuers)
+_actions = np.array([4]*env.num_agents)
 env.reset()
 # controlling only the pursuers
 import matplotlib.pyplot as plt
@@ -64,26 +64,27 @@ def on_key(event):
 cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
 done = False
+num_frames = 0
+# start = time.time()
 # for _ in range(100):
 while not done:
     env.render()
     # print("_quit_loop", _quit_loop)
     if _quit_loop:
         break
-    observation, rewards, done, info = env.step(_actions)
-    print("rewards", rewards, done)
+    # actions should be a dict of numpy arrays: {0: array([0,1,0])}
+    action_dict = dict(zip(env.agent_ids, [np.array([0]*env.n_act_agents) for _ in range(env.num_agents)])) # no action = [0,1,0]
+    for idx, val in enumerate(_actions):
+        action_dict[idx][val] = 1
+    observation, rewards, done_dict, info = env.step(action_dict)
+    done = done_dict[0]
+    if done:
+        print("rewards", rewards, done)
     
-    _actions = np.array([4]*env.n_pursuers)
-    # _totalReward += rewards[0]
-    # done = dones[0]
-    # env.plot_obs(observation, "obs")
-    # break
+    _actions = np.array([4]*env.num_agents)
 
-# assert (_totalReward == env.score), "Final score = {} and reward = {} are not the same".format(env.score, _totalReward)
-# print("Total final reward is {0:.2f}".format(_totalReward))
-# Uncomment next line to print FPS at which the game runs
-# print("fps = ", env.clock.get_fps())
+# end = time.time()
+# print("FPS = ", 100/(end-start))
 env.render()
-import time
-time.sleep(5)
+time.sleep(3)
 env.close()
