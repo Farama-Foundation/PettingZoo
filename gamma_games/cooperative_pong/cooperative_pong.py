73bcf5d9b7508f0dbfc6d441d7324b1b32e9efa2
Justin Terry <justinkterry@gmail.com>
2020-01-26 23:24:59 -0500

    style

--- gamma_games/cooperative_pong/cooperative_pong.py
+++ gamma_games/cooperative_pong/cooperative_pong.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-
-from cooperative_pong_gym import env as _env 
+from cooperative_pong_gym import env as _env
 import numpy as np
-
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
 def convert_to_dict(list_of_list):
@@ -11,32 +8,32 @@ def convert_to_dict(list_of_list):
         dict_of_list[idx] = i
     return dict_of_list
 
+
 class env(MultiAgentEnv):
-    
     metadata = {'render.modes': ['human']}	
-		
+
     def __init__(self, *args):
         super(env, self).__init__()
         self.env = _env(*args)
-        
+
         self.num_agents = 2
         self.agent_ids = list(range(self.num_agents))
         # spaces
         self.action_space_dict = dict(zip(self.agent_ids, [self.env.action_space[0]]*self.num_agents))
         self.observation_space_dict = dict(zip(self.agent_ids, [self.env.observation_space[0]]*self.num_agents))
-        
+
         self.reset()
-        
+
     def reset(self):
         self.env.reset()
         return self.observe()
-    
+
     def close(self):
         self.env.close()
-    
+
     def render(self):
         self.env.render()
-    
+
     def observe(self):
         obs = self.env.observe()
         return convert_to_dict(obs)
@@ -54,7 +51,7 @@ class env(MultiAgentEnv):
                 if cut >= normDist[action] and cut < normDist[action+1]:
                     break
             actions.append(action)
-        
+
         observation, reward, done, info = self.env.step(actions)
 
         observation_dict = convert_to_dict(observation)
@@ -62,5 +59,5 @@ class env(MultiAgentEnv):
         info_dict = convert_to_dict(info)
         done_dict = convert_to_dict(done)
         done_dict["__all__"] = done[0]
-        
+
         return observation_dict, reward_dict, done_dict, info_dict
