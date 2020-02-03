7045a834a26f22499abe1ded9c72376ec46e1bc5
Jordan Terry <jordan@jkterry.ai>
2020-02-03 17:20:37 -0500

    parameter sharing black magic

--- parameterSharingCooperativePong.py
+++ parameterSharingCooperativePong.py
@@ -25,47 +25,19 @@ class MLPModel(Model):
         return output, last_layer
 
 
-# ray.init()
+ray.init()
 
 ModelCatalog.register_custom_model("MLPModel", MLPModel)
-
-# pursuit
-
-
-def env_creator(args):
-    return pursuit.env()
-
-
-env = env_creator(1)
-register_env("pursuit", env_creator)
-
-obs_space = gym.spaces.Box(low=0, high=1, shape=(148,), dtype=np.float32)
-act_space = gym.spaces.Discrete(5)
-
-"""
-# cooperative pong
 
 def env_creator(args):
     return cooperative_pong.env()
 
+
 env = env_creator(1)
 register_env("cooperative_pong", env_creator)
 
-obs_space = gym.spaces.Box(low=0, high=255, shape=(flattened_shape,), dtype=np.uint8)
+obs_space = gym.spaces.Box(low=0, high=1, shape=(2688,), dtype=np.float32)
 act_space = gym.spaces.Discrete(3)
-"""
-
-"""
-# pistonball
-def env_creator(args):
-    return piston_ball.env()
-
-env = env_creator(1)
-register_env("pistonBall", env_creator)
-
-obs_space = gym.spaces.Box(low=0, high=255, shape=(1500,), dtype=np.uint8)
-act_space = gym.spaces.Discrete(3)
-"""
 
 
 def gen_policy(i):
@@ -81,7 +53,6 @@ def gen_policy(i):
 policies = {"policy_0": gen_policy(0)}
 policy_ids = list(policies.keys())
 
-"""
 tune.run(
     "DQN",
     stop={"episodes_total": 60000},
@@ -89,7 +60,7 @@ tune.run(
     config={
 
         # Enviroment specific
-        "env": "pursuit",
+        "env": "cooperative_pong",
 
         # General
         "log_level": "ERROR",
@@ -114,54 +85,10 @@ tune.run(
 )
 
 """
-
-"""
 tune.run(
     "PPO",
     stop={"episodes_total": 60000},
-    checkpoint_freq=10,
-    config={
-
-        # Enviroment specific
-        "env": "pursuit",
-
-        # General
-        "log_level": "ERROR",
-        "num_gpus": 1,
-        "num_workers": 8,
-        "num_envs_per_worker": 8,
-        "compress_observations": True,
-        "gamma": .99,
-
-
-        "lambda": 0.95,
-        "kl_coeff": 0.5,
-        "clip_rewards": True,
-        "clip_param": 0.1,
-        "vf_clip_param": 10.0,
-        "entropy_coeff": 0.01,
-        "train_batch_size": 5000,
-        "sample_batch_size": 100,
-        "sgd_minibatch_size": 500,
-        "num_sgd_iter": 10,
-        "batch_mode": 'truncate_episodes',
-        "vf_share_layers": True,
-
-        # Method specific
-
-        "multiagent": {
-            "policies": policies,
-            "policy_mapping_fn": (
-                lambda agent_id: policy_ids[0]),
-        },
-    },
-)
-"""
-
-tune.run(
-    "IMPALA",
-    stop={"episodes_total": 60000},
-    checkpoint_freq=10,
+    checkpoint_freq=100,
     config={
 
         # Enviroment specific
@@ -177,10 +104,21 @@ tune.run(
         "train_batch_size": 512,
         "gamma": .99,
 
-        "clip_rewards": True,
-        "lr_schedule": [[0, 0.0005],[20000000, 0.000000000001]],
-
         # Method specific
+        "lambda": 0.95,
+        "kl_coeff": 0.5,
+        "clip_rewards": True,
+        "clip_param": 0.1,
+        "vf_clip_param": 10.0,
+        "entropy_coeff": 0.01,
+        "train_batch_size": 5000,
+        "sample_batch_size": 100,
+        "sgd_minibatch_size": 500,
+        "num_sgd_iter": 10,
+        "batch_mode": "truncate_episodes",
+        "observation_filter": "NoFilter",
+        "vf_share_layers": True,
+
 
         "multiagent": {
             "policies": policies,
@@ -189,3 +127,50 @@ tune.run(
         },
     },
 )
+"""
+
+"""
+# old
+    tune.run(
+        "APEX",
+        stop={"episodes_total": 60000},
+        checkpoint_freq=100,
+        config={
+            "env": "pistonBall",
+            "log_level": "ERROR",
+
+            "num_gpus": 1,
+            "num_workers": 8,
+            "num_envs_per_worker": 8,
+
+            "buffer_size": int(1e5),
+            "compress_observations": True,
+
+            "double_q": False,
+            "dueling": False,
+            "num_atoms": 1,
+            "noisy": False,
+            "n_step": 3,
+            "lr": .0001,
+            "adam_epsilon": .00015,
+            "hiddens": [512],
+            "schedule_max_timesteps": 60000,
+            "exploration_final_eps": 0.01,
+            "exploration_fraction": .1,
+            "prioritized_replay_alpha": 0.5,
+            "beta_annealing_fraction": 1.0,
+            "final_prioritized_replay_beta": 1.0,
+
+            "sample_batch_size": 20,
+            "train_batch_size": 512,
+            "target_network_update_freq": 50000,
+            "timesteps_per_iteration": 25000,
+
+            "multiagent": {
+                "policies": policies,
+                "policy_mapping_fn": (
+                    lambda agent_id: policy_ids[0]),
+            },
+        },
+    )
+"""
