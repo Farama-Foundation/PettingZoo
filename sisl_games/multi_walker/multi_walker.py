5342babf7ee1bb4094c90254c0fac9b29f7892c9
Jordan Terry <jordan@jkterry.ai>
2020-02-03 19:05:58 -0500

    error checks and 500 frame cap

--- sisl_games/multi_walker/multi_walker.py
+++ sisl_games/multi_walker/multi_walker.py
@@ -17,7 +17,7 @@ class env(MultiAgentEnv):
         # spaces
         self.action_space_dict = dict(zip(self.agent_ids, self.env.action_space))
         self.observation_space_dict = dict(zip(self.agent_ids, self.env.observation_space))
-
+        self.steps = 0
         self.reset()
 
     def convert_to_dict(self, list_of_list):
@@ -25,6 +25,7 @@ class env(MultiAgentEnv):
 
     def reset(self):
         observation = self.env.reset()
+        self.steps = 0
         return self.convert_to_dict(observation)
 
     def close(self):
@@ -36,15 +37,24 @@ class env(MultiAgentEnv):
     def step(self, action_dict):
         # unpack actions
         actions = [0.0 for _ in range(len(action_dict))]
-        for key in action_dict.keys():
-            actions[key] = action_dict[key]
+
+        for i in self.agent_ids:
+            if not self.action_space_dict[i].contains(action_dict[i]):
+                raise Exception('Action for agent {} must be in {}. \
+                                It is currently {}'.format(i, self.action_space_dict[i], action_dict[i]))
+            actions[i] = action_dict[i]
 
         observation, reward, done, info = self.env.step(actions)
+
+        if self.steps >= 500:
+            done = [True]*self.num_agents
 
         observation_dict = self.convert_to_dict(observation)
         reward_dict = self.convert_to_dict(reward)
         info_dict = self.convert_to_dict(info)
         done_dict = self.convert_to_dict(done)
         done_dict["__all__"] = done[0]
+
+        self.steps += 1
 
         return observation_dict, reward_dict, done_dict, info_dict
