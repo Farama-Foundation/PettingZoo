46e5641f82a8424e819aab84471e7570c7f17151
AnanthHari <hariananth92@gmail.com>
2020-02-05 15:37:56 -0500

    Added support for arbitrary number of agents in test.py

--- sisl_games/pursuit/test.py
+++ sisl_games/pursuit/test.py
@@ -17,10 +17,10 @@ env = _env(n_pursuers = n_pursuers, n_evaders = n_evaders, xs = xs, ys = ys, obs
 
 done = False
 
-global _quit_loop, _totalReward, _actions
-_quit_loop = 0
-_totalReward = 0
+global _quit_loop, _actions, _agent_id
+_quit_loop = np.array([0])
 _actions = np.array([4]*env.num_agents)
+_agent_id = np.array([0])
 env.reset()
 # controlling only the pursuers
 import matplotlib.pyplot as plt
@@ -30,35 +30,38 @@ def on_key(event):
     # print('you pressed', event.key)
     if event.key == "escape":
         print("escape")
-        _quit_loop = 1
+        _quit_loop[0] = 1
         # break
     if event.key == "backspace":
         env.reset()
-        _totalReward = 0
+    if event.key == "j":
+        # pressing 'j' moves the focus of control to the next agent
+        # control rolls over to the first agent
+        _agent_id[0] = (_agent_id[0] + 1) % env.num_agents
     if event.key == "left":
         # p1: left
-        _actions[0] = 0
+        _actions[_agent_id[0]] = 0
     if event.key == "right":
         # p1: right
-        _actions[0] = 1
+        _actions[_agent_id[0]] = 1
     if event.key == "up":
         # p1: up
-        _actions[0] = 3
+        _actions[_agent_id[0]] = 3
     if event.key == "down":
         # p1: down
-        _actions[0] = 2
-    if event.key == "4":
-        # p2: left
-        _actions[1] = 0
-    if event.key == "6":
-        # p2: right
-        _actions[1] = 1
-    if event.key == "8":
-        # p2: up
-        _actions[1] = 3
-    if event.key == "5":
-        # p2: down
-        _actions[1] = 2
+        _actions[_agent_id[0]] = 2
+    # if event.key == "4":
+    #     # p2: left
+    #     _actions[1] = 0
+    # if event.key == "6":
+    #     # p2: right
+    #     _actions[1] = 1
+    # if event.key == "8":
+    #     # p2: up
+    #     _actions[1] = 3
+    # if event.key == "5":
+    #     # p2: down
+    #     _actions[1] = 2
 
 cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
@@ -69,7 +72,7 @@ num_frames = 0
 while not done:
     env.render()
     # print("_quit_loop", _quit_loop)
-    if _quit_loop:
+    if _quit_loop[0]:
         break
     # actions should be a dict of numpy arrays
     action_dict = dict(zip(env.agent_ids, _actions))
@@ -84,5 +87,5 @@ while not done:
 # end = time.time()
 # print("FPS = ", 100/(end-start))
 env.render()
-time.sleep(3)
+time.sleep(2)
 env.close()
