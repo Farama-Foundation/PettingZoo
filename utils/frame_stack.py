22cb697456605cb3a5a8c3bdaca78c19ede9ef8e
Jordan Terry <jordan@jkterry.ai>
2020-02-13 03:56:57 -0500

    flake8 cleaning

--- utils/frame_stack.py
+++ utils/frame_stack.py
@@ -9,10 +9,11 @@ from gamma_games.cooperative_pong.cooperative_pong import env as _env
 
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
+
 class env(MultiAgentEnv):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, stack_size = 4, *args):
+    def __init__(self, stack_size=4, *args):
         super(env, self).__init__()
         self.env = _env(*args)
 
@@ -22,17 +23,17 @@ class env(MultiAgentEnv):
         self.stack_size = stack_size
         self.action_space_dict = deepcopy(self.env.action_space_dict)
         new_obs_space_shape = (self.env.observation_space_dict[0].shape[0] * self.stack_size,)
-        self.observation_space_dict = {agent_id : Box(low=0.0, high=1.0, shape=new_obs_space_shape, dtype=np.float32)\
-                for agent_id in self.env.observation_space_dict.keys()}
-        
+        self.observation_space_dict = {agent_id: Box(low=0.0, high=1.0, shape=new_obs_space_shape, dtype=np.float32)
+                                       for agent_id in self.env.observation_space_dict.keys()}
+
         self.reset()
-    
+
     def reset(self):
         obs_dict = self.env.reset()
-        self.frame_stack = {agent_id : np.tile(obs_dict[agent_id], self.stack_size) \
+        self.frame_stack = {agent_id: np.tile(obs_dict[agent_id], self.stack_size)
                             for agent_id in obs_dict.keys()}
         # assumes all observations have the same size. All of them are flat.
-        self.frame_len = np.random.choice([len(self.frame_stack[key]) \
+        self.frame_len = np.random.choice([len(self.frame_stack[key])
                                            for key in self.frame_stack.keys()])
         return self.frame_stack
 
@@ -43,20 +44,19 @@ class env(MultiAgentEnv):
         self.env.render()
 
     def step(self, actions):
-
         observation_dict, reward_dict, done_dict, info_dict = self.env.step(actions)
 
         self.stack_obs(observation_dict)
 
         return self.frame_stack, reward_dict, done_dict, info_dict
-    
+
     def stack_obs(self, new_obs_dict):
         '''
         Parameters
         ----------
         new_obs_dict : dictionary of observations
             Rearranges frame_stack. Appends the new observation ath the end.
-            Throws away the oldest observation.            
+            Throws away the oldest observation.
 
         '''
         for agent_id in new_obs_dict.keys():
