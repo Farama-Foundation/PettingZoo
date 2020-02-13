b8f737118477204321d557fc741e0fe40ec9883c
Jordan Terry <jordan@jkterry.ai>
2020-02-13 04:23:47 -0500

    flake8 compliance

--- sisl/waterworld/test.py
+++ sisl/waterworld/test.py
@@ -4,7 +4,7 @@ import time
 
 n_pursuers = 5
 
-env = _env(n_pursuers = n_pursuers)
+env = _env(n_pursuers=n_pursuers)
 env.reset()
 
 done = False
@@ -15,10 +15,10 @@ while not done:
     # game should run at 15 FPS when rendering
     env.render()
     time.sleep(0.03)
-    
+
     action_list = np.array([env.action_space_dict[i].sample() for i in range(env.num_agents)])
     action_dict = dict(zip(env.agent_ids, action_list))
-    
+
     observation, rewards, done_dict, info = env.step(action_dict)
     done = any(list(done_dict.values()))
     total_reward += sum(rewards.values())
@@ -41,13 +41,13 @@ env.close()
 #      while not done:
 #          action_list = np.array([env.action_space_dict[i].sample() for i in range(env.num_agents)])
 #          action_dict = dict(zip(env.agent_ids, action_list))
-#          
+#
 #          observation, rewards, done_dict, info = env.step(action_dict)
 #          done = any(list(done_dict.values()))
 #          total_reward[i] += sum(rewards.values())
 #          # print("step reward = ", sum(rewards.values()))
 #          if done:
 #              print("Total reward of iter ", i, total_reward[i], done)
-#  
+#
 #  print("Average over all trials = ", sum(total_reward)/num_trials)
 #  env.close()
