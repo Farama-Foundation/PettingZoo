e80ec244e7e6afce98fb155ff3a159d623576e1d
Justin Terry <justinkterry@gmail.com>
2020-02-17 21:23:23 -0500

    checkers

---
+++ pettingzoo/classic/checkers/checkers/__init__.py
@@ -0,0 +1,4 @@
+from .game import Checkers
+
+
+__all__ = [Checkers]
---
+++ pettingzoo/classic/checkers/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='checkers',
+    version='0.0.0',
+    packages=find_packages(),
+    author='Falcon Dai',
+    author_email='me@falcondai.com',
+    keywords=['reinforcement learning', 'machine learning', 'pytorch', 'board game', 'English draughts', 'openai-gym'],
+    description='OpenAI gym-compatible implementation of two-player checkers.',
+)
