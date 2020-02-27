fe4f279839b7da2b28378a10151a716c522a0ca3
AnanthHari <hariananth92@gmail.com>
2020-02-26 19:11:52 -0500

    Fixed frame stacking

--- pettingzoo/utils/func.py
+++ pettingzoo/utils/func.py
@@ -1,4 +1,4 @@
-def save_observation(observation_dict, reverse_colors=False):
+def save_observation(observation_dict, frame_stacking=1, reverse_colors=False):
     from os.path import isfile
     import matplotlib.pyplot as plt
     import numpy as np
@@ -12,6 +12,10 @@ def save_observation(observation_dict, reverse_colors=False):
         if image.ndim < 2:
             raise ValueError("Image should be at least 2-dimensional. It is {}".format(image.ndim))
         # image is range [0, 1] - float32. Else, use np.divide
+        
+        if frame_stacking > 1:
+            # pick the last frame (most recent)
+            image = image[-1]
 
         if reverse_colors:
             image = np.array([[1-col for col in row] for row in image], dtype=np.float32)
