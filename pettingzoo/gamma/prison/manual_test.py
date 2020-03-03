2093bd17aedf752b759c6078fb2a08d75d0895fa
marioj <mario.jayakumar@gmail.com>
2020-03-02 19:55:32 -0500

    prison has action space

--- pettingzoo/gamma/prison/manual_test.py
+++ pettingzoo/gamma/prison/manual_test.py
@@ -9,6 +9,7 @@ def manual_control(**kwargs):
     y = 0
     while True:
         agent_actions = np.array([0 for _ in range(8)])
+        num_actions = 0
         for event in pygame.event.get():
             # wasd to switch prisoner, jk to move left and right
             if event.type == pygame.KEYDOWN:
@@ -21,18 +22,21 @@ def manual_control(**kwargs):
                 elif event.key == pygame.K_s:
                     y = min(3, y+1)
                 elif event.key == pygame.K_j:
+                    num_actions += 1
                     agent_actions[env.convert_coord_to_prisoner_id((x, y))] = -20
                 elif event.key == pygame.K_k:
+                    num_actions += 1
                     agent_actions[env.convert_coord_to_prisoner_id((x, y))] = 20
 
         actions = dict(zip(env.agents, agent_actions))
         test_done = False
-        for i in env.agents:
-            reward, done, info = env.last_cycle()
-            if done:
-                test_done = True
-            action = actions[i]
-            obs = env.step(action)
+        if num_actions > 0:
+            for i in env.agents:
+                reward, done, info = env.last_cycle()
+                if done:
+                    test_done = True
+                action = actions[i]
+                obs = env.step(action)
         env.render()
 
         if test_done:
