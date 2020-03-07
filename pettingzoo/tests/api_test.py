bde341ea0c5fe70061915e3d9087eb9cd83b1332
Ben Black <weepingwillowben@gmail.com>
2020-03-07 12:15:19 -0500

    fixed bugs in api_test

--- pettingzoo/tests/api_test.py
+++ pettingzoo/tests/api_test.py
@@ -16,13 +16,13 @@ def test_obervation(observation, observation_0):
             assert False, "Observation can not be an empty array"
         if observation.shape == (1,):
             warnings.warn("Observation is a single number")
-        if not isinstance(observation, observation_0):
+        if not isinstance(observation, observation_0.__class__):
             warnings.warn("Observations between agents are different classes")
         if (observation.shape != observation_0.shape) and (len(observation.shape) == len(observation_0.shape)):
             warnings.warn("Observations are different shapes")
         if len(observation.shape) != len(observation_0.shape):
             warnings.warn("Observations have different number of deminsions")
-        if (observation.dtype != np.dtype('f')) and (observation.dtype != np.dtype('u')) and (observation.dtype != np.dtype('i')):
+        if (observation.dtype > np.dtype('float64')) and (observation.dtype > np.dtype('int64')) and (observation.dtype != np.dtype('uint64')):
             warnings.warn("Observation numpy array is not a numeric dtype")
     else:
         warnings.warn("Observation is not NumPy array")
@@ -34,38 +34,38 @@ def test_observation_action_spaces(env, agent_0):
     assert len(env.observation_spaces) == len(env.action_spaces) == len(env.agents), "observation_spaces, action_spaces, and agents must have the same length"
 
     for agent in env.agents:
-        if not (isinstance(env.observation_spaces[agent], gym.spaces.box) or isinstance(env.observation_spaces[agent], gym.spaces.discrete)):
+        if not (isinstance(env.observation_spaces[agent], gym.spaces.Box) or isinstance(env.observation_spaces[agent], gym.spaces.Discrete)):
             warnings.warn("Observation space for each agent should be gym.spaces.box or gym.spaces.discrete")
-        if not (isinstance(env.action_spaces[agent], gym.spaces.box) or isinstance(env.action_spaces[agent], gym.spaces.discrete)):
+        if not (isinstance(env.action_spaces[agent], gym.spaces.Box) or isinstance(env.action_spaces[agent], gym.spaces.Discrete)):
             warnings.warn("Action space for each agent should be gym.spaces.box or gym.spaces.discrete")
         if (not isinstance(agent, int)) and agent != 'env':
             warnings.warn("Agent's are recommended to have integer names")
-        if not isinstance(env.observation_spaces[agent], env.observation_spaces[agent_0]):
+        if not isinstance(env.observation_spaces[agent], env.observation_spaces[agent_0].__class__):
             warnings.warn("The class of observation spaces is different between two agents")
-        if not isinstance(env.action_spaces[agent], env.action_spaces[agent_0]):
+        if not isinstance(env.action_spaces[agent], env.action_spaces[agent_0].__class__):
             warnings.warn("The class of action spaces is different between two agents")
         if env.observation_spaces[agent] != env.observation_spaces[agent_0]:
             warnings.warn("Agents have different observation space sizes")
         if env.action_spaces[agent] != env.action_spaces[agent_0]:
             warnings.warn("Agents have different observation space sizes")
-        if isinstance(env.action_spaces[agent], gym.spaces.box):
-            if env.action_spaces[agent].low == -np.inf:
+        if isinstance(env.action_spaces[agent], gym.spaces.Box):
+            if np.any(np.equal(env.action_spaces[agent].low, -np.inf)):
                 warnings.warn("Agent's minimum action space value is -infinity. This is probably too low.")
-            if env.action_spaces[agent].high == np.inf:
+            if np.any(np.equal(env.action_spaces[agent].high, np.inf)):
                 warnings.warn("Agent's maxmimum action space value is infinity. This is probably too high")
-            if env.action_spaces[agent].low == env.action_spaces[agent].high:
+            if np.any(np.equal(env.action_spaces[agent].low, env.action_spaces[agent].high)):
                 assert False, "Agent's maximum and minimum action space values are equal"
-            if env.action_spaces[agent].low > env.action_spaces[agent].high:
+            if np.any(np.greater(env.action_spaces[agent].low, env.action_spaces[agent].high)):
                 assert False, "Agent's mimum action space value is greater than it's maximum"
 
-        if isinstance(env.observation_spaces[agent], gym.spaces.box):
-            if env.observation_spaces[agent].low == -np.inf:
+        if isinstance(env.observation_spaces[agent], gym.spaces.Box):
+            if np.any(np.equal(env.observation_spaces[agent].low, -np.inf)):
                 warnings.warn("Agent's minimum observation space value is -infinity. This is probably too low.")
-            if env.observation_spaces[agent].high == np.inf:
+            if np.any(np.equal(env.observation_spaces[agent].high, np.inf)):
                 warnings.warn("Agent's maxmimum observation space value is infinity. This is probably too high")
-            if env.observation_spaces[agent].low == env.observation_spaces[agent].high:
+            if np.any(np.equal(env.observation_spaces[agent].low, env.observation_spaces[agent].high)):
                 assert False, "Agent's maximum and minimum observation space values are equal"
-            if env.observationn_spaces[agent].low > env.observation_spaces[agent].high:
+            if np.any(np.greater(env.observation_spaces[agent].low , env.observation_spaces[agent].high)):
                 assert False, "Agent's mimum observation space value is greater than it's maximum"
 
 
@@ -83,15 +83,17 @@ def test_rewards(env, agent_0):
             if np.isnan(env.rewards[agent]):
                 assert False, "Rewards cannot be NaN"
 
-        assert isinstance(env.rewards[agent], env.rewards[agent_0]), "Rewards for each agent must be of the same class"
+        assert isinstance(env.rewards[agent], env.rewards[agent_0].__class__), "Rewards for each agent must be of the same class"
 
 
 def play_test(env, observation_0):
+    prev_observe = env.reset()
     for agent in env.agent_order:  # step through every agent once with observe=True
         action = env.action_spaces[agent].sample()
-        observation = env.step(action)
-        assert env.observation_spaces[agent].contains(observation), "Agent's observation is outside of it's observation space"
-        test_obervation(observation, observation_0)
+        next_observe = env.step(action)
+        assert env.observation_spaces[agent].contains(prev_observe), "Agent's observation is outside of it's observation space"
+        test_obervation(prev_observe, observation_0)
+        prev_observe = next_observe
 
     for agent in env.agent_order:  # step through every agent once with observe=False
         action = env.action_spaces[agent].sample()
@@ -107,7 +109,7 @@ def test_observe(env, observation_0):
 
 def test_render(env):
     for agent in env.agent_order:
-        env.step(observation=False)
+        env.step(observe=False)
         env.render()
     env.close()
 
@@ -145,7 +147,7 @@ def api_test(env, render=False, manual_control=False):
 
     test_observe(env, observation_0)
 
-    test_render(env)
+    #test_render(env)
 
     test_manual_control(env)
 
