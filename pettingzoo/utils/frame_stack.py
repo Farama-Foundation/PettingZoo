4f040aeff3e4df5a1df544f1458468ba2b0dc41b
AnanthHari <hariananth92@gmail.com>
2020-03-07 21:42:44 -0500

    cooperative pong conforms to AECEnv; fixed frame stacking; fixed save_image_observation; fixed wrapper for fs

--- pettingzoo/utils/frame_stack.py
+++ pettingzoo/utils/frame_stack.py
@@ -20,49 +20,54 @@ def stack_obs_space(obs_space_dict, stack_size):
         obs_space = obs_space_dict[agent_id]
         assert isinstance(obs_space, Box), "Stacking is currently only allowed for Box obs space. The given obs space is {}".format(obs_space)
         dtype = obs_space.dtype
-        obs_shape = obs_space_dict[agent_id].shape
-        # stack 1-D frames
-        if len(obs_shape) == 1:
-            shape = (stack_size, 1)
+        obs_dim = obs_space_dict[agent_id].low.ndim
+        # stack 1-D frames and 3-D frames
+        if obs_dim == 1 or obs_dim == 3:
+            new_shape = (stack_size,)
         # stack 2-D frames
-        elif len(obs_shape) == 2:
-            shape = (stack_size, 1, 1)
-        # stack 3-D frames
-        elif len(obs_shape) == 3:
-            shape = (stack_size, 1, 1, 1)
-        low = np.tile(obs_space.low, shape)
-        high = np.tile(obs_space.high, shape)
+        elif obs_dim == 2:
+            new_shape = (stack_size, 1, 1)
+        low = np.tile(obs_space.low, new_shape)
+        high = np.tile(obs_space.high, new_shape)
         new_obs_space_dict[agent_id] = Box(low=low, high=high, dtype=dtype)
     return new_obs_space_dict
 
-def stack_reset_obs(obs_dict, stack_size):
+def stack_reset_obs(obs, stack_size):
     '''
-    Reset observations are only 1 obs per agent. Tile them.
+    Input: 1 agent's observation only.
+    Reset observations are only 1 obs. Tile them.
     '''
-    frame_stack = {}
-    for agent_id in obs_dict.keys():
-        obs_shape = obs_dict[agent_id].shape
-        # stack 1-D frames
-        if len(obs_shape) == 1:
-            shape = (stack_size, 1)
-        # stack 2-D frames
-        elif len(obs_shape) == 2:
-            shape = (stack_size, 1, 1)
-        # stack 3-D frames
-        elif len(obs_shape) == 3:
-            shape = (stack_size, 1, 1, 1)
-        frame_stack[agent_id] =  np.tile(obs_dict[agent_id], shape)
+    # stack 1-D frames and 3-D frames
+    if obs.ndim == 1 or obs.ndim == 3:
+        new_shape = (stack_size,)
+    # stack 2-D frames
+    elif obs.ndim == 2:
+        new_shape = (stack_size, 1, 1)
+    frame_stack =  np.tile(obs, new_shape)
     return frame_stack
 
-def stack_obs(frame_stack, new_obs_dict):
+def stack_obs(frame_stack, agent, obs, stack_size):
     '''
     Parameters
     ----------
-    obs_dict : dictionary of observations
+    frame_stack : if not None, it is the stack of frames
+    obs : new observation
         Rearranges frame_stack. Appends the new observation at the end.
         Throws away the oldest observation.
+    stack_size : needed for stacking reset observations
     '''
-    for agent_id in new_obs_dict.keys():
-        new_obs = new_obs_dict[agent_id]
-        frame_stack[agent_id][:-1] = frame_stack[agent_id][1:]
-        frame_stack[agent_id][-1] = new_obs
+    if frame_stack[agent] is None:
+        frame_stack[agent] = stack_reset_obs(obs, stack_size)
+    obs_shape = obs.shape
+    agent_fs = frame_stack[agent]
+    
+    if len(obs_shape) == 1:
+        agent_fs[:-obs_shape[-1]] = agent_fs[obs_shape[-1]:]
+        agent_fs[-obs_shape[-1]:] = obs
+    elif len(obs_shape) == 2:
+        agent_fs[:-1] = agent_fs[1:]
+        agent_fs[-1] = obs
+    elif len(obs_shape) == 3:
+        agent_fs[:, :, :-obs_shape[-1]] = agent_fs[:, :, obs_shape[-1]:]
+        agent_fs[:, :, -obs_shape[-1]:] = obs
+    
