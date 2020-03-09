9fc0505705e8189fd2d43521c2a1e35147e6ef0d
Justin Terry <justinkterry@gmail.com>
2020-03-08 23:28:17 -0400

    revert bad commit

--- test.sh
+++ test.sh
@@ -1,31 +1,31 @@
-# python3 -m pettingzoo.tests.ci_test classic/backgammon
-# python3 -m pettingzoo.tests.ci_test classic/checkers
-# python3 -m pettingzoo.tests.ci_test classic/chess
-# python3 -m pettingzoo.tests.ci_test classic/connect_four
-# python3 -m pettingzoo.tests.ci_test classic/dou_dizhu
-# python3 -m pettingzoo.tests.ci_test classic/go
-# python3 -m pettingzoo.tests.ci_test classic/leduc_holdem
-# python3 -m pettingzoo.tests.ci_test classic/mahjong
-# python3 -m pettingzoo.tests.ci_test classic/rps
-# python3 -m pettingzoo.tests.ci_test classic/rpsls
-# python3 -m pettingzoo.tests.ci_test classic/texas_holdem
-# python3 -m pettingzoo.tests.ci_test classic/texas_holdem_no_limit
-# python3 -m pettingzoo.tests.ci_test classic/tic_tac_toe
-# python3 -m pettingzoo.tests.ci_test classic/uno
-# python3 -m pettingzoo.tests.ci_test gamma/cooperative_pong
-# python3 -m pettingzoo.tests.ci_test gamma/knights_archers_zombies
-# python3 -m pettingzoo.tests.ci_test gamma/pistonball
-# python3 -m pettingzoo.tests.ci_test gamma/prison
-# python3 -m pettingzoo.tests.ci_test gamma/prospector
-# python3 -m pettingzoo.tests.ci_test mpe/simple
-# python3 -m pettingzoo.tests.ci_test mpe/simple_adversary
-# python3 -m pettingzoo.tests.ci_test mpe/simple_crypto
-# python3 -m pettingzoo.tests.ci_test mpe/simple_push
-# python3 -m pettingzoo.tests.ci_test mpe/simple_reference
-# python3 -m pettingzoo.tests.ci_test mpe/simple_speaker_listener
-# python3 -m pettingzoo.tests.ci_test mpe/simple_spread
-# python3 -m pettingzoo.tests.ci_test mpe/simple_tag
-# python3 -m pettingzoo.tests.ci_test mpe/simple_world_comm
-# python3 -m pettingzoo.tests.ci_test sisl/multiwalker
+python3 -m pettingzoo.tests.ci_test classic/backgammon
+python3 -m pettingzoo.tests.ci_test classic/checkers
+python3 -m pettingzoo.tests.ci_test classic/chess
+python3 -m pettingzoo.tests.ci_test classic/connect_four
+python3 -m pettingzoo.tests.ci_test classic/dou_dizhu
+python3 -m pettingzoo.tests.ci_test classic/go
+python3 -m pettingzoo.tests.ci_test classic/leduc_holdem
+python3 -m pettingzoo.tests.ci_test classic/mahjong
+python3 -m pettingzoo.tests.ci_test classic/rps
+python3 -m pettingzoo.tests.ci_test classic/rpsls
+python3 -m pettingzoo.tests.ci_test classic/texas_holdem
+python3 -m pettingzoo.tests.ci_test classic/texas_holdem_no_limit
+python3 -m pettingzoo.tests.ci_test classic/tic_tac_toe
+python3 -m pettingzoo.tests.ci_test classic/uno
+python3 -m pettingzoo.tests.ci_test gamma/cooperative_pong
+python3 -m pettingzoo.tests.ci_test gamma/knights_archers_zombies
+python3 -m pettingzoo.tests.ci_test gamma/pistonball
+python3 -m pettingzoo.tests.ci_test gamma/prison
+python3 -m pettingzoo.tests.ci_test gamma/prospector
+python3 -m pettingzoo.tests.ci_test mpe/simple
+python3 -m pettingzoo.tests.ci_test mpe/simple_adversary
+python3 -m pettingzoo.tests.ci_test mpe/simple_crypto
+python3 -m pettingzoo.tests.ci_test mpe/simple_push
+python3 -m pettingzoo.tests.ci_test mpe/simple_reference
+python3 -m pettingzoo.tests.ci_test mpe/simple_speaker_listener
+python3 -m pettingzoo.tests.ci_test mpe/simple_spread
+python3 -m pettingzoo.tests.ci_test mpe/simple_tag
+python3 -m pettingzoo.tests.ci_test mpe/simple_world_comm
+python3 -m pettingzoo.tests.ci_test sisl/multiwalker
 python3 -m pettingzoo.tests.ci_test sisl/pursuit
-# python3 -m pettingzoo.tests.ci_test sisl/waterworld
+python3 -m pettingzoo.tests.ci_test sisl/waterworld
