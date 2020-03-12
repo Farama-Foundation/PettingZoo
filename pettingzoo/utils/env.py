ed790f70d99a3464ab5fa5041147b8edd533edc8
Jordan Terry <jordan@jkterry.ai>
2020-03-12 12:40:12 -0400

    rm markov env

--- pettingzoo/utils/env.py
+++ pettingzoo/utils/env.py
@@ -20,19 +20,3 @@ class AECEnv(object):
 
     def close(self):
         pass
-
-class MarkovEnv(object):
-    def __init__(self):
-        pass
-    
-    def step(self, action):
-        raise NotImplementedError
-    
-    def reset(self):
-        raise NotImplementedError
-        
-    def render(self, mode='human'):
-        raise NotImplementedError
-    
-    def close(self):
-        pass
