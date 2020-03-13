b6b506a8f0e00bec6435e4a4e6bc2444627bc7c4
Ben Black <weepingwillowben@gmail.com>
2020-03-13 10:29:46 -0400

    updated chess to use discrete action space

--- pettingzoo/classic/chess/chess_env.py
+++ pettingzoo/classic/chess/chess_env.py
@@ -3,6 +3,7 @@ import chess
 from pettingzoo.utils import AECEnv
 from gym import spaces
 import numpy as np
+import warnings
 
 
 class env(AECEnv):
@@ -19,12 +20,12 @@ class env(AECEnv):
 
         self.agent_order = list(self.agents)
 
-        self.action_spaces = {i: spaces.Box(low=-np.inf, high=+np.inf, shape=(8, 8, 73), dtype=np.float32) for i in range(2)}
+        self.action_spaces = {i: spaces.Discrete(8 * 8 * 73) for i in range(2)}
         self.observation_spaces = {i: spaces.Box(low=-np.inf, high=+np.inf, shape=(8, 8, 20), dtype=np.float32) for i in range(2)}
 
         self.rewards = {i: 0 for i in range(self.num_agents)}
         self.dones = {i: False for i in range(self.num_agents)}
-        self.infos = {i: {} for i in range(self.num_agents)}
+        self.infos = {i: {'legal_moves': []} for i in range(self.num_agents)}
 
         self.agent_selection = 0
 
@@ -38,28 +39,46 @@ class env(AECEnv):
 
         self.agent_selection = 0
 
+        self.rewards = {i: 0 for i in range(self.num_agents)}
+        self.dones = {i: False for i in range(self.num_agents)}
+        self.infos = {i: {'legal_moves': []} for i in range(self.num_agents)}
+        self.infos[self.agent_selection]['legal_moves'] = chess_utils.legal_moves(self.board)
+
         if observe:
             return self.observe(0)
         else:
             return
 
+    def set_game_result(self, result_val):
+        for i in range(2):
+            self.dones[i] = True
+            result_coef = 1 if i == 0 else -1
+            self.rewards[i] = result_val * result_coef
+            self.infos[i] = {'legal_moves': []}
+
     def step(self, action, observe=True):
         current_idx = self.agent_selection
         self.agent_selection = next_agent = (self.agent_selection + 1) % self.num_agents
 
-        sampled_move = chess_utils.sample_action(self.board, action)
-        reoriented_move = sampled_move if not current_idx else chess_utils.mirror_move(sampled_move)
+        old_legal_moves = self.infos[current_idx]['legal_moves']
 
-        self.board.push(reoriented_move)
+        if action not in old_legal_moves:
+            warnings.warn("Bad chess move made. env.infos[player]['legal_moves'] contains a list of all legal moves that can be chosen.")
+            player_loses_val = -1 if current_idx == 0 else 1
+            self.set_game_result(player_loses_val)
+        else:
+            chosen_move = chess_utils.action_to_move(action, current_idx)
 
-        # claim draw is set to be true to allign with normal tournament rules
-        if self.board.is_game_over(claim_draw=True):
-            result = self.board.result(claim_draw=True)
-            result_val = chess_utils.result_to_int(result)
-            for i in range(2):
-                self.dones[i] = True
-                result_coef = 1 if i == 0 else -1
-                self.rewards[i] = result_val * result_coef
+            self.board.push(chosen_move)
+
+            # claim draw is set to be true to allign with normal tournament rules
+            if self.board.is_game_over(claim_draw=True):
+                result = self.board.result(claim_draw=True)
+                result_val = chess_utils.result_to_int(result)
+                self.set_game_result(result_val)
+            else:
+                self.infos[current_idx] = {'legal_moves': []}
+                self.infos[next_agent] = {'legal_moves': chess_utils.legal_moves(self.board)}
 
         if observe:
             next_observation = self.observe(next_agent)
