632d48693b4809b565e79d85cf880e7f537ad04e
AnanthHari <hariananth92@gmail.com>
2020-03-17 01:01:20 -0400

    Fixed observations and rendering

--- pettingzoo/classic/rps/rps.py
+++ pettingzoo/classic/rps/rps.py
@@ -1,4 +1,3 @@
-import gym
 from gym.spaces import Discrete
 import numpy as np
 from pettingzoo import AECEnv
@@ -9,6 +8,7 @@ from pettingzoo.utils import agent_selector
 rock = 0
 paper = 1
 scissors = 2
+MOVES = ["ROCK", "PAPER", "SCISSORS"]
 NUM_ITERS = 100
 
 
@@ -36,14 +36,15 @@ class env(AECEnv):
         self.rewards = {agent: 0 for agent in self.agents}
         self.dones = {agent: False for agent in self.agents}
         self.infos = {agent: {} for agent in self.agents}
-        self.observations = {agent: 0 for agent in self.agents}
+        self.state = {agent: 0 for agent in self.agents}
         self.num_moves = 0
 
     def render(self, mode="human"):
-        print("Current state: Agent1: {} , Agent2: {}".format(self.observations[0], self.observations[1]))
+        print("Current state: Agent1: {} , Agent2: {}".format(MOVES[self.state[0]], MOVES[self.state[1]]))
 
     def observe(self, agent):
-        return self.observations[agent]
+        # observation of one agent is the state of the other
+        return self.state[1 - agent]
 
     def close(self):
         pass
@@ -51,7 +52,7 @@ class env(AECEnv):
     def reset(self, observe=True):
         self.reinit()
         if observe:
-            return self.observations[0]
+            return self.observe(0)
 
     def step(self, action, observe=True):
         agent = self.agent_selection
@@ -60,7 +61,7 @@ class env(AECEnv):
         elif not self.action_spaces[agent].contains(action):
             raise Exception('Action for agent {} must be in Discrete({}).'
                             'It is currently {}'.format(agent, self.action_spaces[agent].n, action))
-        self.observations[self.agent_selection] = action
+        self.state[self.agent_selection] = action
 
         # collect reward if it is the last agent to act
         if self._agent_selector.is_last():
@@ -74,11 +75,11 @@ class env(AECEnv):
                 (scissors, rock): (-1, 1),
                 (scissors, paper): (1, -1),
                 (scissors, scissors): (0, 0),
-            }[(self.observations[0], self.observations[1])]
+            }[(self.state[0], self.state[1])]
 
             self.num_moves += 1
             self.dones = {agent: self.num_moves >= NUM_ITERS for agent in self.agents}
 
         self.agent_selection = self._agent_selector.next()
         if observe:
-            return self.observations[self.agent_selection]
+            return self.observe(self.agent_selection)
