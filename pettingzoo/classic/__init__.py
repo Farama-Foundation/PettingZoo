70c41693329c065cbf67c684963c93e600cf31fa
lssr <lss@umd.edu>
2020-03-17 16:50:50 -0400

    Updated for rlcard==0.1.10

--- pettingzoo/classic/__init__.py
+++ pettingzoo/classic/__init__.py
@@ -1,6 +1,9 @@
 from .chess import chess_env as chess
+from .rps import rps
+from .rpsls import rpsls
 from .leduc_holdem import leduc_holdem
 from .mahjong import mahjong
 from .texas_holdem import texas_holdem
 from .texas_holdem_no_limit import texas_holdem_no_limit
 from .uno import uno
+from .dou_dizhu import dou_dizhu
