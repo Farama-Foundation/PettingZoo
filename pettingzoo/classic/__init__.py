99d4cc2fa66c840e84b8e0fc3163196b3be4c29b
AnanthHari <hariananth92@gmail.com>
2020-03-17 01:41:23 -0400

    RPSLS works in ways similar to RPS now

--- pettingzoo/classic/__init__.py
+++ pettingzoo/classic/__init__.py
@@ -1,5 +1,6 @@
 from .chess import chess_env as chess
-from .rps import rps 
+from .rps import rps
+from .rpsls import rpsls
 from .leduc_holdem import leduc_holdem
 from .mahjong import mahjong
 from .texas_holdem import texas_holdem
