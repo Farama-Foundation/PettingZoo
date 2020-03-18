bb5cd8013bf48888c6908f6cf9cd6d68cbb5043c
Praveen Ravi <praveen.ravi@rallyhealth.com>
2020-03-18 12:34:34 -0400

    s/box/square/g

--- pettingzoo/classic/tictactoe/tictactoe_utils.py
+++ pettingzoo/classic/tictactoe/tictactoe_utils.py
@@ -8,7 +8,7 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 
 
-class Box(object):
+class Square(object):
     state = 0
     
     def __init__(self, x, y, size, board):
@@ -31,12 +31,12 @@ class Box(object):
 class Board(object):
     turn = 1
     
-    def __init__(self, grid_size=3, box_size=100, border=50, line_width=10):
+    def __init__(self, grid_size=3, square_size=100, border=50, line_width=10):
         self.grid_size = grid_size
-        self.box_size = box_size
+        self.square_size = square_size
         self.border = border
         self.line_width = line_width
-        surface_size = (self.grid_size * self.box_size) + (self.border * 2) + (self.line_width * (self.grid_size - 1))
+        surface_size = (self.grid_size * self.square_size) + (self.border * 2) + (self.line_width * (self.grid_size - 1))
         self.surface = pygame.display.set_mode((surface_size, surface_size), 0, 32)
         self.game_over = False
         self.setup()
@@ -45,50 +45,50 @@ class Board(object):
         pygame.display.set_caption('Tic Tac Toe')
         self.surface.fill(WHITE)
         self.draw_lines()
-        self.initialize_boxes()
+        self.initialize_squares()
         self.calculate_winners()
     
     def draw_lines(self):
         for i in range(1, self.grid_size):
-            start_position = ((self.box_size * i) + (self.line_width * (i - 1))) + self.border
+            start_position = ((self.square_size * i) + (self.line_width * (i - 1))) + self.border
             width = self.surface.get_width() - (2 * self.border)
             pygame.draw.rect(self.surface, BLACK, (start_position, self.border, self.line_width, width))
             pygame.draw.rect(self.surface, BLACK, (self.border, start_position, width, self.line_width))
     
-    def initialize_boxes(self):
-        self.boxes = []
+    def initialize_squares(self):
+        self.squares = []
         
         top_left_numbers = []
         for i in range(0, self.grid_size):
-            num = ((i * self.box_size) + self.border + (i *self.line_width))
+            num = ((i * self.square_size) + self.border + (i *self.line_width))
             top_left_numbers.append(num)
         
-        box_coordinates = list(itertools.product(top_left_numbers, repeat=2))
-        for x, y in box_coordinates:
-            self.boxes.append(Box(x, y, self.box_size, self))
+        square_coordinates = list(itertools.product(top_left_numbers, repeat=2))
+        for x, y in square_coordinates:
+            self.squares.append(Square(x, y, self.square_size, self))
     
-    def get_box_at_pixel(self, x, y):
-        for index, box in enumerate(self.boxes):
-            if box.rect.collidepoint(x, y):
-                return box
+    def get_square_at_pixel(self, x, y):
+        for index, square in enumerate(self.squares):
+            if square.rect.collidepoint(x, y):
+                return square
         return None
     
     def process_click(self, x, y):
-        box = self.get_box_at_pixel(x, y)
-        if box is not None and not self.game_over:
-            self.play_turn(box)
+        square = self.get_square_at_pixel(x, y)
+        if square is not None and not self.game_over:
+            self.play_turn(square)
             self.check_game_over()
     
-    def play_turn(self, box):
-        if box.state != 0:
+    def play_turn(self, square):
+        if square.state != 0:
             return
         if self.turn == 1:
-            box.mark_x()
-            box.state = 1
+            square.mark_x()
+            square.state = 1
             self.turn = 2
         elif self.turn == 2:
-            box.mark_o()
-            box.state = 2
+            square.mark_o()
+            square.state = 2
             self.turn = 1
         return
     
@@ -111,7 +111,7 @@ class Board(object):
         for combination in self.winning_combinations:
             states = []
             for index in combination:
-                states.append(self.boxes[index].state)
+                states.append(self.squares[index].state)
             if all(x == 1 for x in states):
                 winner = 1
             if all(x == 2 for x in states):
@@ -122,7 +122,7 @@ class Board(object):
         winner = self.check_for_winner()
         if winner:
             self.game_over = True
-        elif all(box.state in [1, 2] for box in self.boxes):
+        elif all(square.state in [1, 2] for square in self.squares):
             self.game_over = True
         if self.game_over:
             self.display_game_over(winner)
