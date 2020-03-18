e67fb262ac3546efb803bbe0fc25fc312fc02ba7
Praveen Ravi <praveen.ravi@rallyhealth.com>
2020-03-17 23:59:33 -0400

    need to add rewards

--- pettingzoo/classic/tictactoe/tictactoe_env.py
+++ pettingzoo/classic/tictactoe/tictactoe_env.py
@@ -48,11 +48,31 @@ class env(AECEnv):
 
     # action in this case is a value from 0 to 8 indicating position to move on tic tac toe board
     def step(self, action, observe=True):
-        current_idx = self.agent_selection
-        self.agent_selection = next_agent = (self.agent_selection + 1) % self.num_agents
+        # check if input action is a valid move
+        if(self.board.boxes[action] == 0):
+            # play turn
+            self.board.play_turn(self.board.boxes[action])
 
-        
+            # check if game over
+            game_over = all(box.state in [1, 2] for box in self.boxes)
 
+            # if winner = 0 no winner yet
+            # if winner = 1 agent 0 won
+            # if winner = 2 agent 1 won
+            winner = self.board.check_for_winner()
+
+            if game_over:
+                if winner == 0:
+                    pass
+                elif winner == 1:
+                    # agent 0 won
+                    pass 
+                else:
+                    # agent 1 won
+                    pass
+        else:
+            # invalid move, some sort of negative reward
+            pass
 
         # Switch selection to next agents
         self.agent_selection = self._agent_selector.next()
@@ -66,6 +86,7 @@ class env(AECEnv):
 
     def reset(self, observe=True):
         # reset environment
+        self.board = Board()
 
         # selects the first agent
         self._agent_selector.reinit(self.agent_order)
