9afe7d163a2d90c0cd578263ba93632dadd4c825
lssr <lss@umd.edu>
2020-03-21 21:23:43 -0400

    Added agent_selector for card games

--- pettingzoo/classic/texas_holdem/texas_holdem.py
+++ pettingzoo/classic/texas_holdem/texas_holdem.py
@@ -1,4 +1,5 @@
 from pettingzoo import AECEnv
+from pettingzoo.utils.agent_selector import agent_selector
 from gym import spaces
 import rlcard
 from rlcard.utils.utils import print_card
@@ -14,10 +15,18 @@ class env(AECEnv):
         self.num_agents = 2
         self.agents = list(range(self.num_agents))
         
-        self.reset()
-
+        self.rewards = self._convert_to_dict(np.array([0.0, 0.0]))
+        self.dones = self._convert_to_dict([False for _ in range(self.num_agents)])
+        self.infos = self._convert_to_dict([{'legal_moves': []} for _ in range(self.num_agents)])
         self.observation_spaces = dict(zip(self.agents, [spaces.Box(low=0.0, high=1.0, shape=(72,), dtype=np.bool) for _ in range(self.num_agents)]))
         self.action_spaces = dict(zip(self.agents, [spaces.Discrete(self.env.game.get_action_num()) for _ in range(self.num_agents)]))
+
+        obs, player_id = self.env.init_game()
+
+        self.agent_order = [player_id, 0 if player_id==1 else 1]
+        self._agent_selector = agent_selector(self.agent_order)
+        self.agent_selection = self._agent_selector.reset()
+        self.infos[player_id]['legal_moves'] = obs['legal_actions']
 
     def _convert_to_dict(self, list_of_list):
         return dict(zip(self.agents, list_of_list))
@@ -27,23 +36,23 @@ class env(AECEnv):
         return obs['obs']
 
     def step(self, action, observe=True):
-        obs, next_player_id = self.env.step(action)
-        self.agent_selection = next_player_id
-        self.dones = self._convert_to_dict([True if self.env.is_over() else False for _ in range(self.num_agents)])
-        self.infos[next_player_id]['legal_moves'] = obs['legal_actions']
         if self.env.is_over():
             self.rewards = self._convert_to_dict(self.env.get_payoffs())
+            self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
         else:
+            obs, next_player_id = self.env.step(action)
+            self.dones = self._convert_to_dict([True if self.env.is_over() else False for _ in range(self.num_agents)])
+            self.infos[next_player_id]['legal_moves'] = obs['legal_actions']
             self.rewards = self._convert_to_dict(np.array([0.0, 0.0]))
+        self.agent_selection = self._agent_selector.next()
         if observe:
-            return obs['obs']
-        else:
-            return
+            return obs['obs'] if obs else None
 
     def reset(self, observe=True):
         obs, player_id = self.env.init_game()
-        self.agent_selection = player_id
         self.agent_order = [player_id, 0 if player_id==1 else 1]
+        self._agent_selector.reinit(self.agent_order)
+        self.agent_selection = self._agent_selector.reset()
         self.rewards = self._convert_to_dict(np.array([0.0, 0.0]))
         self.dones = self._convert_to_dict([False for _ in range(self.num_agents)])
         self.infos = self._convert_to_dict([{'legal_moves': []} for _ in range(self.num_agents)])
@@ -54,10 +63,11 @@ class env(AECEnv):
             return
 
     def render(self, mode='human'):
-        public_cards = self.env.game.public_cards
-        player_hand = self.env.game.players[self.agent_selection].hand
-        print("\n=============== Player {}'s Hand ===============".format(self.agent_selection))
-        print_card([c.get_index() for c in player_hand])
+        for player in self.agents:
+            state = self.env.game.get_state(player)
+            print("\n=============== Player {}'s Hand ===============".format(player))
+            print_card(state['hand'])
+            print("\nPlayer {}'s Chips: {}".format(player,state['my_chips']))
         print('\n================= Public Cards =================')
-        print_card([c.get_index() for c in public_cards]) if public_cards else print('No public cards.')
+        print_card(state['public_cards']) if state['public_cards'] else print('No public cards.')
         print('\n')
