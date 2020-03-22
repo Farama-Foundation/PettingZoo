45d1a85c32680d498919f7b061f6a9abcd49c04e
Praveen Ravi <praveenravi77@gmail.com>
2020-03-22 12:28:17 -0400

    passes tests now, but need to check if my observation spaces is correct

--- pettingzoo/classic/tictactoe/tictactoe.py
+++ pettingzoo/classic/tictactoe/tictactoe.py
@@ -21,11 +21,11 @@ class env(AECEnv):
         self._agent_selector = agent_selector(self.agent_order)
 
         self.action_spaces = {i: spaces.Discrete(9) for i in range(2)}
-        self.observation_spaces = {i: spaces.Discrete(9) for i in range(2)}
+        self.observation_spaces = {i: spaces.Box(low=-1.0, high=1.0, shape=(1, 9), dtype=np.int16) for i in range(2)}
 
         self.rewards = {i: 0 for i in range(self.num_agents)}
         self.dones = {i: False for i in range(self.num_agents)}
-        self.infos = {i: {'legal_moves': []} for i in range(self.num_agents)}
+        self.infos = {i: {'legal_moves': list(range(0,9))} for i in range(self.num_agents)}
 
         self.agent_selection = 0
 
@@ -46,14 +46,15 @@ class env(AECEnv):
     # agent 1 = 1
     def observe(self, agent):
         # return observation of an agent
-        return np.asarray(self.board.squares)
+        return [self.board.squares]
 
     # action in this case is a value from 0 to 8 indicating position to move on tictactoe board
     def step(self, action, observe=True):
         # check if input action is a valid move (-1 == empty spot)
         if(self.board.squares[action] == -1):
             # play turn
-            self.board.play_turn(self.agent_selection, self.board.squares[action])
+            self.board.play_turn(self.agent_selection, action)
+            print("Board after play_turn: " + str(self.board.squares))
 
             # update infos
             # list of valid actions (indexes in board)
@@ -95,7 +96,7 @@ class env(AECEnv):
 
         self.rewards = {i: 0 for i in range(self.num_agents)}
         self.dones = {i: False for i in range(self.num_agents)}
-        self.infos = {i: {'legal_moves': [1] * 9} for i in range(self.num_agents)}
+        self.infos = {i: {'legal_moves': list(range(0,9))} for i in range(self.num_agents)}
 
         # selects the first agent
         self._agent_selector.reinit(self.agent_order)
@@ -110,3 +111,9 @@ class env(AECEnv):
 
     def close(self):
         pass
+
+# import pettingzoo as pz
+# env = pz.classic.tictactoe.env()
+
+# import pettingzoo as pz
+# env = pz.classic.tictactoe.manual_control()
