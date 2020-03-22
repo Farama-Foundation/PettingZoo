017fa7b9426c2953a5fd1675b369cb73f047561a
niallwphd <niallw@cs.umd.edu>
2020-03-22 11:14:10 -0400

    fixed sprites on prison

--- pettingzoo/gamma/prison/prison.py
+++ pettingzoo/gamma/prison/prison.py
@@ -7,7 +7,6 @@ import random
 from gym import spaces
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
-
 def get_image(path):
     from os import path as os_path
     cwd = os_path.dirname(__file__)
@@ -31,42 +30,59 @@ class Prisoner:
         self.left_sprite = None
         self.right_sprite = None
         self.state = 0
+        self.sprite_timer_on = False
+        self.last_sprite_movement = 0
+        self.sprite_timer = 0 # if the agent hasn't moved left or right in X frames, set sprite to "still"
 
     def set_sprite(self, s):
         self.still_sprite = get_image(s+"_still.png")
         self.left_sprite = get_image(s+"_left.png")
         self.right_sprite = get_image(s+"_right.png")
+        self.right_bound -= self.right_sprite.get_width()
 
     def set_state(self, st):
         self.state = st
     
     def get_sprite(self):
-        if self.state == 0:
+        if self.last_sprite_movement == 0:
             return self.still_sprite
-        elif self.state == 1:
+        elif self.last_sprite_movement == 1:
             return self.right_sprite
-        elif self.state == -1:
+        elif self.last_sprite_movement == -1:
             return self.left_sprite
         else:
             print("INVALID STATE",self.state)
             return self.still_sprite
 
+    def update_sprite(self, movement):
+        if movement != 0:
+            self.sprite_timer_on = True
+            self.sprite_timer = 0
+            self.last_sprite_movement = movement
+        if self.sprite_timer_on:
+            self.sprite_timer += 1
+        if self.sprite_timer > 2: # FIXME: change the number here if you want the left/right sprite to last longer on screen after moving to either direction
+            self.sprite_timer = 0
+            self.sprite_timer_on = False
+            self.last_sprite_movement = 0
+
 
 class env(AECEnv):
 
-    def __init__(self, continuous=False, vector_observation=True):
+    def __init__(self, continuous=False, vector_observation=True, max_frames=500):
         # super(env, self).__init__()
         self.num_agents = 8
         self.agents = list(range(0, self.num_agents))
         self.agent_order = self.agents[:]
         self.agent_selector_obj = agent_selector(self.agent_order)
         self.agent_selection = 0
-        self.sprite_list = ["sprites/alien", "sprites/drone", "sprites/glowy", "sprites/reptile", "sprites/ufo"]
-        self.rewards = dict(zip(self.agents,[0 for _ in self.agents]))
+        self.sprite_list = ["sprites/alien", "sprites/drone", "sprites/glowy", "sprites/reptile", "sprites/ufo", "sprites/bunny", "sprites/robot", "sprites/tank"]
+        self.rewards = dict(zip(self.agents, [0 for _ in self.agents]))
         self.dones = dict(zip(self.agents, [False for _ in self.agents]))
         self.infos = dict(zip(self.agents, [[] for _ in self.agents]))
         self.metadata = {'render.modes': ['human']} 
         self.rendering = False
+        self.max_frames = max_frames
 
         pygame.init()
         pygame.display.init()
@@ -76,9 +92,6 @@ class env(AECEnv):
         self.done_val = False
 
         self.background = get_image('background.png')
-        self.prisoner_sprite = get_image('prisoner.png')
-        self.prisoner_sprite_x = 30
-        self.prisoner_sprite_y = 46
 
         self.velocity = 8
         self.continuous = continuous
@@ -109,28 +122,30 @@ class env(AECEnv):
         self.create_walls()
 
         self.prisoners = []
-        prisoner_spawn_locs = [(200, 150-self.prisoner_sprite_y, 50, 350, (50, 50, 350, 150)), 
-                                (550, 150-self.prisoner_sprite_y, 400, 700, (400, 50, 700, 150)), 
-                                (200, 300-self.prisoner_sprite_y, 50, 350, (50, 200, 350, 300)),
-                               (550, 300-self.prisoner_sprite_y, 400, 700, (400, 200, 700, 300)), 
-                               (200, 450-self.prisoner_sprite_y, 50, 350, (50, 350, 350, 450)), 
-                               (550, 450-self.prisoner_sprite_y, 400, 700, (400, 350, 700, 450)), 
-                               (200, 600-self.prisoner_sprite_y, 50, 350, (50, 500, 350, 600)), 
-                               (550, 600-self.prisoner_sprite_y, 400, 700, (400, 500, 700, 600))]
+        prisoner_spawn_locs = [(200, 150-40, 50, 350, (50, 50, 350, 150)), 
+                                (550, 150-40, 400, 700, (400, 50, 700, 150)), 
+                                (200, 300-46, 50, 350, (50, 200, 350, 300)),
+                               (550, 300-48, 400, 700, (400, 200, 700, 300)), 
+                               (200, 450-32, 50, 350, (50, 350, 350, 450)), 
+                               (550, 450-54, 400, 700, (400, 350, 700, 450)), 
+                               (200, 600-48, 50, 350, (50, 500, 350, 600)), 
+                               (550, 600-53, 400, 700, (400, 500, 700, 600))]
         self.prisoner_mapping = {(0, 0): 0, (1, 0): 1, (0, 1): 2,
                                  (1, 1): 3, (0, 2): 4, (1, 2): 5, (0, 3): 6, (1, 3): 7}
-        for p in prisoner_spawn_locs:
-            x, y, l, r, u = p
-            self.prisoners.append(self.create_prisoner(
-                x + random.randint(-20, 20), y, l, r, u))
+        for p in prisoner_spawn_locs:	
+	            x, y, l, r, u = p	
+	            self.prisoners[p_count] = self.create_prisoner(	
+	                x + random.randint(-20, 20), y, l, r, u)	
+	            p_count += 1
 
         sprite = 0
         for p in self.prisoners:
             p.set_sprite(self.sprite_list[sprite])
             sprite = (sprite + 1) % len(self.sprite_list)
 
+        self.frames = 0	
+        self.reset()
         self.screen.blit(self.background, (0, 0))
-        self.render()
         self.render()
 
     def create_walls(self):
@@ -143,7 +158,7 @@ class env(AECEnv):
             self.walls.append((400, y, 300, 50))
 
     def create_prisoner(self, x, y, l, r, u):
-        return Prisoner((x, y), l, r - self.prisoner_sprite_x, u)
+        return Prisoner((x, y), l, r, u)
 
     def reward(self):
         return dict(zip(self.agents, self.last_rewards))
@@ -153,6 +168,7 @@ class env(AECEnv):
         prisoner = self.prisoners[prisoner_id]
         if not np.isscalar(movement):
             movement = movement[0]
+        prisoner.update_sprite(movement)
         if self.continuous:
             prisoner.position = (
                 prisoner.position[0] + movement, prisoner.position[1])
@@ -185,10 +201,8 @@ class env(AECEnv):
         pygame.quit()
 
     def draw(self):
-        self.screen.blit(self.background, (0, 0))
-        for k in self.walls:
-            pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(k))
-
+        dynamic_background = get_image('blit_background.png')
+        self.screen.blit(dynamic_background, (50,111)) 
         for p in self.prisoners:
             self.screen.blit(p.get_sprite(), p.position)
 
@@ -212,7 +226,8 @@ class env(AECEnv):
     def reset(self, observe=True):
         self.num_frames = 0
         self.done_val = False
-        prisoner_spawn_locs = [(200, 150-self.prisoner_sprite_y, 50, 350, (50, 50, 350, 150)), (550, 150-self.prisoner_sprite_y, 400, 700, (400, 50, 700, 150)), (200, 300-self.prisoner_sprite_y, 50, 350, (50, 200, 350, 300)),(550, 300-self.prisoner_sprite_y, 400, 700, (400, 200, 700, 300)), (200, 450-self.prisoner_sprite_y, 50, 350, (50, 350, 350, 450)), (550, 450-self.prisoner_sprite_y, 400, 700, (400, 350, 700, 450)), (200, 600-self.prisoner_sprite_y, 50, 350, (50, 500, 350, 600)), (550, 600-self.prisoner_sprite_y, 400, 700, (400, 500, 700, 600))]
+
+        prisoner_spawn_locs = [(200, 150-40, 50, 350, (50, 50, 350, 150)), (550, 150-40, 400, 700, (400, 50, 700, 150)), (200, 300-46, 50, 350, (50, 200, 350, 300)),(550, 300-48, 400, 700, (400, 200, 700, 300)), (200, 450-32, 50, 350, (50, 350, 350, 450)), (550, 450-54, 400, 700, (400, 350, 700, 450)), (200, 600-48, 50, 350, (50, 500, 350, 600)), (550, 600-53, 400, 700, (400, 500, 700, 600))]
         self.agent_selector_obj.reinit(self.agent_order)
         self.agent_selection = self.agent_selector_obj.next()
         for i in self.agents:
@@ -220,6 +235,8 @@ class env(AECEnv):
             x, y, l, r, u = prisoner_spawn_locs[i]
             p.position = (x + random.randint(-20,20), y)
         self.last_rewards = [0 for _ in self.agents]
+        self.frames = 0	
+        self.rendering = False
         if observe:
             return self.observe(self.agent_selection)
         
@@ -245,13 +262,13 @@ class env(AECEnv):
         
         self.rewards[agent] = reward
         if self.rendering:
-            self.clock.tick(15)
+            self.clock.tick(30)
         else:
             self.clock.tick()
         #self.draw()
 
         self.num_frames += 1
-        if (self.num_frames >= 500):
+        if (self.num_frames >= self.max_frames):
             self.done_val = True
             for d in self.dones:
                 self.dones[d] = True
@@ -262,7 +279,10 @@ class env(AECEnv):
         if observe:
             return observation
 
-    def render(self, mode='human'):
+    def render(self, mode='human'):	
+        if not self.rendering:	
+            pygame.display.init()	
+            self.screen = pygame.display.set_mode((750, 650))
         self.rendering = True
         self.draw()
         pygame.display.flip()
