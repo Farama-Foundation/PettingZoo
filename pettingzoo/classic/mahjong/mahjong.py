e22d3a069da84646b9cf2993da160a73e93569d1
lssr <lss@umd.edu>
2020-03-24 20:45:04 -0400

    Dealt with agent selecting illegal moves

--- pettingzoo/classic/mahjong/mahjong.py
+++ pettingzoo/classic/mahjong/mahjong.py
@@ -22,6 +22,7 @@ class env(AECEnv):
 
         obs, player_id = self.env.init_game()
 
+        self._last_obs = obs['obs']
         self.agent_order = list(range(self.num_agents))
         self._agent_selector = agent_selector(self.agent_order)
         self.agent_selection = self._agent_selector.reset()
@@ -36,10 +37,18 @@ class env(AECEnv):
         return obs['obs']
 
     def step(self, action, observe=True):
-        if self.env.is_over():
-            self.rewards = self._convert_to_dict(self.env.get_payoffs())
+        if self.dones[self.agent_selection]:
             self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
+            obs = False
         else:
+            if action not in self.infos[self.agent_selection]['legal_moves']:
+                self.rewards[self.agent_selection] = -1
+                self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
+                info_copy = self.infos[self.agent_selection]
+                self.infos = self._convert_to_dict([{'legal_moves': [37]} for agent in range(self.num_agents)])
+                self.infos[self.agent_selection] = info_copy
+                self.agent_selection = self._agent_selector.next()
+                return self._last_obs
             obs, next_player_id = self.env.step(action)
             self.prev_player = self.agent_selection
             prev_player_ind = self.agent_order.index(self.prev_player)
@@ -59,9 +68,9 @@ class env(AECEnv):
             self._agent_selector.reinit(self.agent_order)
             for _ in range(skip_agent):
                 self._agent_selector.next()
-            self.rewards = self._convert_to_dict(self.env.get_payoffs())
             if self.env.is_over():
-                self.infos[next_player_id]['legal_moves'] = [0]
+                self.rewards = self._convert_to_dict(self.env.get_payoffs())
+                self.infos[next_player_id]['legal_moves'] = [37]
                 self.dones = self._convert_to_dict([True if self.env.is_over() else False for _ in range(self.num_agents)])
                 self._last_obs = obs['obs']
             else:
@@ -79,6 +88,7 @@ class env(AECEnv):
         self.dones = self._convert_to_dict([False for _ in range(self.num_agents)])
         self.infos = self._convert_to_dict([{'legal_moves': []} for _ in range(self.num_agents)])
         self.infos[player_id]['legal_moves'] = obs['legal_actions']
+        self._last_obs = obs['obs']
         if observe:
             return obs['obs']
         else:
