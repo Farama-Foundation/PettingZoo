0d141bd0c764f2b0512e19212e2e7dbadb298e32
Jordan Terry <jordan@jkterry.ai>
2020-03-25 23:44:42 -0400

    tests for prints

---
+++ pettingzoo/tests/print_test.sh
@@ -0,0 +1,8 @@
+if grep -r "${PWD}/pettingzoo/sisl"
+then
+    echo There should not be print statements with the SISL games
+fi
+if grep -r "${PWD}/pettingzoo/sisl"
+then
+    echo There should not be print statements with the gamma games
+fi
