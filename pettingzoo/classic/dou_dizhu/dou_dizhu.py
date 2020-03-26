0522ac135537f55c0825863a2cc7d252916193a2
lssr <lss@umd.edu>
2020-03-26 18:50:03 -0400

    Agent names convention compliance

--- pettingzoo/classic/dou_dizhu/dou_dizhu.py
+++ pettingzoo/classic/dou_dizhu/dou_dizhu.py
@@ -13,27 +13,33 @@ class env(AECEnv):
         super(env, self).__init__()
         self.env = rlcard.make('doudizhu', **kwargs)
         self.num_agents = 3
-        self.agents = list(range(self.num_agents))
+        self.agents = ['landlord', 'peasant_0', 'peasant_1']
 
         self.rewards = self._convert_to_dict(np.array([0.0, 0.0, 0.0]))
-        self.observation_spaces = dict(zip(self.agents, [spaces.Box(low=0.0, high=1.0, shape=(6, 5, 15), dtype=np.bool) for _ in range(self.num_agents)]))
-        self.action_spaces = dict(zip(self.agents, [spaces.Discrete(self.env.game.get_action_num()) for _ in range(self.num_agents)]))
+        self.observation_spaces = self._convert_to_dict([spaces.Box(low=0.0, high=1.0, shape=(6, 5, 15), dtype=np.bool) for _ in range(self.num_agents)])
+        self.action_spaces = self._convert_to_dict([spaces.Discrete(self.env.game.get_action_num()) for _ in range(self.num_agents)])
         self.dones = self._convert_to_dict([False for _ in range(self.num_agents)])
         self.infos = self._convert_to_dict([{'legal_moves': []} for _ in range(self.num_agents)])
 
         obs, player_id = self.env.init_game()
 
         self._last_obs = obs['obs']
-        self.agent_order = list(range(self.num_agents))
+        self.agent_order = self.agents
         self._agent_selector = agent_selector(self.agent_order)
         self.agent_selection = self._agent_selector.reset()
-        self.infos[player_id]['legal_moves'] = obs['legal_actions']
+        self.infos[self._int_to_name(player_id)]['legal_moves'] = obs['legal_actions']
+
+    def _int_to_name(self, ind):
+        return self.agents[ind]
+
+    def _name_to_int(self, name):
+        return self.agents.index(name)
 
     def _convert_to_dict(self, list_of_list):
         return dict(zip(self.agents, list_of_list))
 
     def observe(self, agent):
-        obs = self.env.get_state(agent)
+        obs = self.env.get_state(self._name_to_int(agent))
         return obs['obs']
 
     def step(self, action, observe=True):
@@ -54,9 +60,9 @@ class env(AECEnv):
             if self.env.is_over():
                 self.rewards = self._convert_to_dict(self.env.get_payoffs())
                 self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
-                self.infos[next_player_id]['legal_moves'] = [308]
+                self.infos[self._int_to_name(next_player_id)]['legal_moves'] = [308]
             else:
-                self.infos[next_player_id]['legal_moves'] = obs['legal_actions']
+                self.infos[self._int_to_name(next_player_id)]['legal_moves'] = obs['legal_actions']
         self.agent_selection = self._agent_selector.next()
         if observe:
             return obs['obs'] if obs else self._last_obs
@@ -67,7 +73,7 @@ class env(AECEnv):
         self.rewards = self._convert_to_dict(np.array([0.0, 0.0, 0.0]))
         self.dones = self._convert_to_dict([False for _ in range(self.num_agents)])
         self.infos = self._convert_to_dict([{'legal_moves': []} for _ in range(self.num_agents)])
-        self.infos[player_id]['legal_moves'] = obs['legal_actions']
+        self.infos[self._int_to_name(player_id)]['legal_moves'] = obs['legal_actions']
         self._last_obs = obs['obs']
         if observe:
             return obs['obs']
@@ -76,10 +82,10 @@ class env(AECEnv):
 
     def render(self, mode='human'):
         for player in self.agents:
-            state = self.env.game.get_state(player)
-            print("\n===== Player {}'s Hand ({}) =====".format(player, 'Landlord' if player == 0 else 'Peasant'))
+            state = self.env.game.get_state(self._name_to_int(player))
+            print("\n===== {}'s Hand ({}) =====".format(player, 'Landlord' if player == 0 else 'Peasant'))
             print(state['current_hand'])
         print('\n=========== Last 3 Actions ===========')
         for action in state['trace'][:-4:-1]:
-            print('Player {}: {}'.format(action[0], action[1]))
+            print('{}: {}'.format(self._int_to_name(action[0]), action[1]))
         print('\n')
