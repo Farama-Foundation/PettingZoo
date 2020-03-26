1f492c27f4ec589ecff0a47a23e71c5e89f23fec
AnanthHari <hariananth92@gmail.com>
2020-03-26 01:39:38 -0400

    Path changes to print_test

--- pettingzoo/tests/print_test.sh
+++ pettingzoo/tests/print_test.sh
@@ -1,8 +1,8 @@
-if grep -r "${PWD}/pettingzoo/sisl"
+if grep -r print "$PWD"/../sisl/*/*.py
 then
     echo There should not be print statements with the SISL games
 fi
-if grep -r "${PWD}/pettingzoo/sisl"
+if grep -r print "$PWD"/../gamma/*/*.py
 then
     echo There should not be print statements with the gamma games
 fi
