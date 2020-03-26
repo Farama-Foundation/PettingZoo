7a2c0c22482f475e6617fc5e8cb6a57b54698f28
Ben Black <weepingwillowben@gmail.com>
2020-03-26 12:16:10 -0400

    fixed chess performance, now at 4000tps

--- pettingzoo/classic/chess/chess_env.py
+++ pettingzoo/classic/chess/chess_env.py
@@ -75,14 +75,23 @@ class env(AECEnv):
             assert chosen_move in self.board.legal_moves
             self.board.push(chosen_move)
 
+            next_legal_moves = chess_utils.legal_moves(self.board)
+
+            is_stale_or_checkmate = not any(next_legal_moves)
+
             # claim draw is set to be true to allign with normal tournament rules
-            if self.board.is_game_over(claim_draw=True):
+            is_repetition = self.board.is_repetition(3)
+            is_50_move_rule = self.board.can_claim_fifty_moves()
+            is_claimable_draw = is_repetition or is_50_move_rule
+            game_over = is_claimable_draw or is_stale_or_checkmate
+
+            if game_over:
                 result = self.board.result(claim_draw=True)
                 result_val = chess_utils.result_to_int(result)
                 self.set_game_result(result_val)
             else:
                 self.infos[current_agent] = {'legal_moves': []}
-                self.infos[next_agent] = {'legal_moves': chess_utils.legal_moves(self.board)}
+                self.infos[next_agent] = {'legal_moves': next_legal_moves}
                 assert len(self.infos[next_agent]['legal_moves'])
 
         if observe:
