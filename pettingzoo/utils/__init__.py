facd41f7b5a10cff3a7e0b5e09c71398ef7890ca
AnanthHari <hariananth92@gmail.com>
2020-03-30 01:08:58 -0400

    Extended wrapper for additional user methods

--- pettingzoo/utils/__init__.py
+++ pettingzoo/utils/__init__.py
@@ -1,4 +1,5 @@
 from .utils import save_image_observation
 from .random_demo import random_demo
 from .wrapper import wrapper
+from .wrapper_extended import wrapper_extended
 from .agent_selector import agent_selector
