d0f49c3ffb86c3cc7243af46c0206570bf2848fc
Justin Terry <justinkterry@gmail.com>
2020-04-03 03:31:04 -0400

    False to None

--- pettingzoo/tests/ci_test.py
+++ pettingzoo/tests/ci_test.py
@@ -26,7 +26,7 @@ if sys.argv[1] == 'classic/backgammon':
     print('classic/backgammon')
     from pettingzoo.classic import backgammon
     _env = backgammon.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = backgammon.env()
         bombardment_test.bombardment_test(_env)
@@ -38,7 +38,7 @@ if sys.argv[1] == 'classic/checkers':
     print('classic/checkers')
     from pettingzoo.classic import checkers
     _env = checkers.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = checkers.env()
         bombardment_test.bombardment_test(_env)
@@ -50,7 +50,7 @@ if sys.argv[1] == 'classic/chess':
     print('classic/chess')
     from pettingzoo.classic import chess
     _env = chess.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = chess.env()
         bombardment_test.bombardment_test(_env)
@@ -62,7 +62,7 @@ if sys.argv[1] == 'classic/connect_four':
     print('classic/connect_four')
     from pettingzoo.classic import connect_four
     _env = connect_four.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = connect_four.env()
         bombardment_test.bombardment_test(_env)
@@ -74,7 +74,7 @@ if sys.argv[1] == 'classic/dou_dizhu':
     print('classic/dou_dizhu')
     from pettingzoo.classic import dou_dizhu
     _env = dou_dizhu.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = dou_dizhu.env()
         bombardment_test.bombardment_test(_env)
@@ -86,7 +86,7 @@ if sys.argv[1] == 'classic/gin_rummy':
     print('classic/gin_rummy')
     from pettingzoo.classic import gin_rummy
     _env = gin_rummy.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = gin_rummy.env()
         bombardment_test.bombardment_test(_env)
@@ -98,7 +98,7 @@ if sys.argv[1] == 'classic/go':
     print('classic/go')
     from pettingzoo.classic import go
     _env = go.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = go.env()
         bombardment_test.bombardment_test(_env)
@@ -110,7 +110,7 @@ if sys.argv[1] == 'classic/hanabi':
     print('classic/hanabi')
     from pettingzoo.classic import hanabi
     _env = hanabi.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = hanabi.env()
         bombardment_test.bombardment_test(_env)
@@ -122,7 +122,7 @@ if sys.argv[1] == 'classic/leduc_holdem':
     print('classic/leduc_holdem')
     from pettingzoo.classic import leduc_holdem
     _env = leduc_holdem.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = leduc_holdem.env()
         bombardment_test.bombardment_test(_env)
@@ -134,7 +134,7 @@ if sys.argv[1] == 'classic/mahjong':
     print('classic/mahjong')
     from pettingzoo.classic import mahjong
     _env = mahjong.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = mahjong.env()
         bombardment_test.bombardment_test(_env)
@@ -146,7 +146,7 @@ if sys.argv[1] == 'classic/rps':
     print('classic/rps')
     from pettingzoo.classic import rps
     _env = rps.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = rps.env()
         bombardment_test.bombardment_test(_env)
@@ -158,7 +158,7 @@ if sys.argv[1] == 'classic/rpsls':
     print('classic/rpsls')
     from pettingzoo.classic import rpsls
     _env = rpsls.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = rpsls.env()
         bombardment_test.bombardment_test(_env)
@@ -170,7 +170,7 @@ if sys.argv[1] == 'classic/texas_holdem':
     print('classic/texas_holdem')
     from pettingzoo.classic import texas_holdem
     _env = texas_holdem.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = texas_holdem.env()
         bombardment_test.bombardment_test(_env)
@@ -182,7 +182,7 @@ if sys.argv[1] == 'classic/texas_holdem_no_limit':
     print('classic/texas_holdem_no_limit')
     from pettingzoo.classic import texas_holdem_no_limit
     _env = texas_holdem_no_limit.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = texas_holdem_no_limit.env()
         bombardment_test.bombardment_test(_env)
@@ -194,7 +194,7 @@ if sys.argv[1] == 'classic/tictactoe':
     print('classic/tictactoe')
     from pettingzoo.classic import tictactoe
     _env = tictactoe.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = tictactoe.env()
         bombardment_test.bombardment_test(_env)
@@ -206,7 +206,7 @@ if sys.argv[1] == 'classic/uno':
     print('classic/uno')
     from pettingzoo.classic import uno
     _env = uno.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = uno.env()
         bombardment_test.bombardment_test(_env)
@@ -292,7 +292,7 @@ if sys.argv[1] == 'mpe/simple':
     print('mpe/simple')
     from pettingzoo.mpe import simple
     _env = simple.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple.env()
         bombardment_test.bombardment_test(_env)
@@ -304,7 +304,7 @@ if sys.argv[1] == 'mpe/simple_adversary':
     print('mpe/simple_adversary')
     from pettingzoo.mpe import simple_adversary
     _env = simple_adversary.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_adversary.env()
         bombardment_test.bombardment_test(_env)
@@ -316,7 +316,7 @@ if sys.argv[1] == 'mpe/simple_crypto':
     print('mpe/simple_crypto')
     from pettingzoo.mpe import simple_crypto
     _env = simple_crypto.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_crypto.env()
         bombardment_test.bombardment_test(_env)
@@ -328,7 +328,7 @@ if sys.argv[1] == 'mpe/simple_push':
     print('mpe/simple_push')
     from pettingzoo.mpe import simple_push
     _env = simple_push.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_push.env()
         bombardment_test.bombardment_test(_env)
@@ -340,7 +340,7 @@ if sys.argv[1] == 'mpe/simple_reference':
     print('mpe/simple_reference')
     from pettingzoo.mpe import simple_reference
     _env = simple_reference.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_reference.env()
         bombardment_test.bombardment_test(_env)
@@ -352,7 +352,7 @@ if sys.argv[1] == 'mpe/simple_speak_listener':
     print('mpe/simple_speak_listener')
     from pettingzoo.mpe import simple_speak_listener
     _env = simple_speak_listener.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_speak_listener.env()
         bombardment_test.bombardment_test(_env)
@@ -364,7 +364,7 @@ if sys.argv[1] == 'mpe/simple_spread':
     print('mpe/simple_spread')
     from pettingzoo.mpe import simple_spread
     _env = simple_spread.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_spread.env()
         bombardment_test.bombardment_test(_env)
@@ -376,7 +376,7 @@ if sys.argv[1] == 'mpe/simple_tag':
     print('mpe/simple_tag')
     from pettingzoo.mpe import simple_tag
     _env = simple_tag.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_tag.env()
         bombardment_test.bombardment_test(_env)
@@ -388,7 +388,7 @@ if sys.argv[1] == 'mpe/simple_world_comm':
     print('mpe/simple_world_comm')
     from pettingzoo.mpe import simple_world_comm
     _env = simple_world_comm.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = simple_world_comm.env()
         bombardment_test.bombardment_test(_env)
@@ -402,7 +402,7 @@ if sys.argv[1] == 'sisl/multiwalker':
     print('sisl/multiwalker')
     from pettingzoo.sisl import multiwalker
     _env = multiwalker.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = multiwalker.env()
         bombardment_test.bombardment_test(_env)
@@ -428,7 +428,7 @@ if sys.argv[1] == 'sisl/waterworld':
     print('sisl/waterworld')
     from pettingzoo.sisl import waterworld
     _env = waterworld.env()
-    api_test.api_test(_env, render=render, manual_control=False, save_obs=False)
+    api_test.api_test(_env, render=render, manual_control=None, save_obs=False)
     if bombardment:
         _env = waterworld.env()
         bombardment_test.bombardment_test(_env)
