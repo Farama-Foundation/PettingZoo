7a7dac111074f4d30ae306a893d0c38881c82b07
lssr <samaisantos@gmail.com>
2020-04-02 23:10:58 -0400

    fixed test_agent_selector and docs

--- pettingzoo/classic/gin_rummy/gin_rummy.py
+++ pettingzoo/classic/gin_rummy/gin_rummy.py
@@ -57,14 +57,31 @@ class env(AECEnv):
                 self.agent_selection = self._agent_selector.next()
                 return self._last_obs
             obs, next_player_id = self.env.step(action)
+            next_player = self._int_to_name(next_player_id)
             self._last_obs = obs['obs']
+            self.prev_player = self.agent_selection
+            prev_player_ind = self.agent_order.index(self.prev_player)
+            curr_player_ind = self.agent_order.index(next_player)
+            if next_player == self.prev_player:
+                self.agent_order.insert(0, self.agent_order.pop(-1))
+            elif prev_player_ind == self.num_agents - 1:
+                self.agent_order.remove(next_player)
+                self.agent_order.insert(0, next_player)
+            else:
+                self.agent_order.remove(next_player)
+                if curr_player_ind < prev_player_ind:
+                    self.agent_order.insert(0, self.agent_order.pop(-1))
+                self.agent_order.insert(self.agent_order.index(self.prev_player) + 1, next_player)
+            skip_agent = prev_player_ind + 1
+            self._agent_selector.reinit(self.agent_order)
+            for _ in range(skip_agent):
+                self._agent_selector.next()
             if self.env.is_over():
                 self.rewards = self._convert_to_dict(self.env.get_payoffs())
-                self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
+                self.infos[next_player]['legal_moves'] = [37]
+                self.dones = self._convert_to_dict([True if self.env.is_over() else False for _ in range(self.num_agents)])
             else:
-                self.agent_order = [self._int_to_name(agent) for agent in [next_player_id, 0 if next_player_id == 1 else 1]]
-                self.infos[self._int_to_name(next_player_id)]['legal_moves'] = obs['legal_actions']
-                self._agent_selector.reinit(self.agent_order)
+                self.infos[next_player]['legal_moves'] = obs['legal_actions']
         self.agent_selection = self._agent_selector.next()
         if observe:
             return obs['obs'] if obs else self._last_obs
