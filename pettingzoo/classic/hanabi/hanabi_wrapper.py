5d01e664adf9d0101ca127907c54c587410e3727
Clemens Dieffendahl <cdieffendahl@gmail.com>
2020-04-09 19:23:30 +0200

    finished sumodule setup

---
+++ pettingzoo/classic/hanabi/hanabi_wrapper.py
@@ -0,0 +1,2 @@
+from ..hanabi.env.hanabi_learning_environment.rl_env import
+# ToDo: Try to import hanabi. If not possible, throw error message.
---
+++ pettingzoo/classic/hanabi/pull_prepare_and_setup_hanabi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Pulling hanabi submodule, if not existent"
+git pull --recurse-submodules
+
+echo "Installing gcc. First trying apt-get (linux). If not possible, try homebrew (macOS)"
+apt-get install g++ || brew install gcc
+
+cd env
+pip install .
+cd ..
+
+echo "Verify hanabi wrapper is working"
+python hanabi_wrapper.py --test
