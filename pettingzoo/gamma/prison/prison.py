1ef1b1d2c743275caa3020593058916746a7a36f
marioj <mario.jayakumar@gmail.com>
2020-04-10 13:28:30 -0400

    minor gamma game fixes

--- pettingzoo/gamma/prison/prison.py
+++ pettingzoo/gamma/prison/prison.py
@@ -74,16 +74,13 @@ class env(AECEnv):
 
     def __init__(self, continuous=False, vector_observation=True, max_frames=500):
         # super(env, self).__init__()
-        self.num_agents = 8
-        self.agents = ["prisoner_" + str(s) for s in range(0, self.num_agents)]
+        self.agents = ["prisoner_" + str(s) for s in range(0, 8)]
         self.agent_order = self.agents[:]
         self._agent_selector = agent_selector(self.agent_order)
-        self.agent_selection = 0
         self.sprite_list = ["sprites/alien", "sprites/drone", "sprites/glowy", "sprites/reptile", "sprites/ufo", "sprites/bunny", "sprites/robot", "sprites/tank"]
         self.metadata = {'render.modes': ['human']}
         self.rendering = False
         self.max_frames = max_frames
-        self.reinit()
         pygame.init()
         self.clock = pygame.time.Clock()
         self.num_frames = 0
@@ -110,7 +107,7 @@ class env(AECEnv):
             if vector_observation:
                 self.observation_spaces[a] = spaces.Box(low=-300, high=300, shape=(2,))
             else:
-                self.observation_spaces[a] = spaces.Box(low=0, high=255, shape=(300, 100, 3))
+                self.observation_spaces[a] = spaces.Box(low=0, high=255, shape=(100, 300, 3))
 
         self.walls = []
         self.create_walls()
@@ -138,6 +135,7 @@ class env(AECEnv):
             self.prisoners[p].set_sprite(self.sprite_list[sprite])
             sprite = (sprite + 1) % len(self.sprite_list)
 
+        self.reinit()
 
     def create_walls(self):
         self.walls = [(0, 0, 50, 700), (350, 0, 50, 700),
@@ -209,6 +207,8 @@ class env(AECEnv):
             p = self.prisoners[agent]
             x1, y1, x2, y2 = p.window
             sub_screen = np.array(capture[x1:x2, y1:y2, :])
+            sub_screen = np.rot90(sub_screen, k=3)
+            sub_screen = np.fliplr(sub_screen)
             return sub_screen
 
     def reinit(self):
@@ -217,8 +217,15 @@ class env(AECEnv):
         self.infos = dict(zip(self.agents, [{} for _ in self.agents]))
         self.done_val = False
         self.num_frames = 0
-        self.dones = {agent : False for agent in self.agents}
-        
+        self._agent_selector.reinit(self.agent_order)
+        self.agent_selection = self._agent_selector.next()
+        self.last_rewards = [0 for _ in self.agents]
+        self.frames = 0
+        self.rendering = False
+        self.screen = pygame.Surface((750, 650))
+        self.screen.blit(self.background, (0, 0))
+        self.rendering = False
+
     def reset(self, observe=True):
         self.num_frames = 0
         self.reinit()
@@ -231,20 +238,12 @@ class env(AECEnv):
                                (550, 450 - 54, 400, 700, (400, 350, 700, 450)),
                                (200, 600 - 48, 50, 350, (50, 500, 350, 600)),
                                (550, 600 - 53, 400, 700, (400, 500, 700, 600))]
-        self._agent_selector.reinit(self.agent_order)
-        self.agent_selection = self._agent_selector.next()
         p_count = 0
         for i in self.agents:
             p = self.prisoners[i]
             x, y, l, r, u = prisoner_spawn_locs[p_count]
             p.position = (x + random.randint(-20, 20), y)
             p_count += 1
-        self.last_rewards = [0 for _ in self.agents]
-        self.frames = 0
-        self.rendering = False
-        self.screen = pygame.Surface((750, 650))
-        self.screen.blit(self.background, (0, 0))
-        self.rendering = False
         if observe:
             return self.observe(self.agent_selection)
 
