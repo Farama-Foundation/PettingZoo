0a1c74d99ca5dbbe898aff24b09c2c20322764cb
marioj <mario.jayakumar@gmail.com>
2020-04-10 20:50:40 -0400

    api test prints out of bounds observation in observation test

--- pettingzoo/tests/api_test.py
+++ pettingzoo/tests/api_test.py
@@ -121,6 +121,8 @@ def play_test(env, observation_0):
         else:
             action = env.action_spaces[agent].sample()
         next_observe = env.step(action)
+        if not env.observation_spaces[agent].contains(prev_observe):
+            print("Out of bounds observation: ", prev_observe)
         assert env.observation_spaces[agent].contains(prev_observe), "Agent's observation is outside of it's observation space"
         test_obervation(prev_observe, observation_0)
         prev_observe = next_observe
