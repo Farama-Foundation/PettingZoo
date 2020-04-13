eba1523c2d971d96563cb92b5ac9b73aaa4933ed
lssr <samaisantos@gmail.com>
2020-04-12 23:50:07 -0400

    Gin Rummy reward arguments, updated Leduc Holdem observation space, and Holdem reward fix

--- pettingzoo/classic/gin_rummy/gin_rummy.py
+++ pettingzoo/classic/gin_rummy/gin_rummy.py
@@ -2,6 +2,10 @@ from pettingzoo import AECEnv
 from pettingzoo.utils.agent_selector import agent_selector
 from gym import spaces
 import rlcard
+from rlcard.games.gin_rummy.player import GinRummyPlayer
+import rlcard.games.gin_rummy.utils.melding as melding
+from rlcard.games.gin_rummy.utils.action_event import KnockAction, GinAction
+from rlcard.games.gin_rummy.utils import utils
 from rlcard.utils.utils import print_card
 import numpy as np
 
@@ -10,8 +14,10 @@ class env(AECEnv):
 
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, **kwargs):
+    def __init__(self, knock_reward: float = 0.5, gin_reward: float = 1.0, **kwargs):
         super(env, self).__init__()
+        self._knock_reward = knock_reward
+        self._gin_reward = gin_reward
         self.env = rlcard.make('gin-rummy', **kwargs)
         self.agents = ['player_0', 'player_1']
         self.num_agents = len(self.agents)
@@ -29,6 +35,7 @@ class env(AECEnv):
         self._agent_selector = agent_selector(self.agent_order)
         self.agent_selection = self._agent_selector.reset()
         self.infos[self._int_to_name(player_id)]['legal_moves'] = obs['legal_actions']
+        self.env.game.judge.scorer.get_payoff = self._get_payoff
 
     def _int_to_name(self, ind):
         return self.agents[ind]
@@ -38,6 +45,21 @@ class env(AECEnv):
 
     def _convert_to_dict(self, list_of_list):
         return dict(zip(self.agents, list_of_list))
+
+    def _get_payoff(self, player: GinRummyPlayer, game) -> float:
+        going_out_action = game.round.going_out_action
+        going_out_player_id = game.round.going_out_player_id
+        if going_out_player_id == player.player_id and type(going_out_action) is KnockAction:
+            payoff = self._knock_reward
+        elif going_out_player_id == player.player_id and type(going_out_action) is GinAction:
+            payoff = self._gin_reward
+        else:
+            hand = player.hand
+            best_meld_clusters = melding.get_best_meld_clusters(hand=hand)
+            best_meld_cluster = [] if not best_meld_clusters else best_meld_clusters[0]
+            deadwood_count = utils.get_deadwood_count(hand, best_meld_cluster)
+            payoff = -deadwood_count / 100
+        return payoff
 
     def observe(self, agent):
         obs = self.env.get_state(self._name_to_int(agent))
@@ -108,7 +130,7 @@ class env(AECEnv):
             print_card([c.__str__()[::-1] for c in state])
         state = self.env.game.get_state(0)
         print("\n==== Top Discarded Card ====")
-        print_card([c.__str__() for c in state['top_discard']] if state else [])
+        print_card([c.__str__() for c in state['top_discard']] if state else None)
         print('\n')
 
     def close(self):
