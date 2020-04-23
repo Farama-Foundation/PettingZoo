c13eba53c15a7e35742bb9843cf112a32e699642
marioj <mario.jayakumar@gmail.com>
2020-04-23 17:36:42 -0400

    fixed observation saver flake8 error

--- pettingzoo/utils/__init__.py
+++ pettingzoo/utils/__init__.py
@@ -1,4 +1,6 @@
 from .random_demo import random_demo
 from .agent_selector import agent_selector
 from .env_logger import EnvLogger
-from .observation_saver import *
+from .observation_saver import save_observation_for_agent
+from .observation_saver import save_observation_for_all
+from .observation_saver import save_observation_selected
