9ce0e35f45622728f11d77e9982e736a5536e94f
Ben Black <weepingwillowben@gmail.com>
2020-04-23 20:48:05 -0400

    changed mpe max_frames default, added docs

--- pettingzoo/mpe/simple_adversary.py
+++ pettingzoo/mpe/simple_adversary.py
@@ -3,7 +3,7 @@ from .scenarios.simple_adversary import Scenario
 
 
 class env(SimpleEnv):
-    def __init__(self, seed=None, N=2, max_frames=500):
+    def __init__(self, seed=None, N=2, max_frames=100):
         scenario = Scenario()
         world = scenario.make_world(N=2)
         super(env, self).__init__(scenario, world, max_frames, seed)
