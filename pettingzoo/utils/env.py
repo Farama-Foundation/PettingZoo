28a77d396b48003068d61764fafad57d2a628031
marioj <mario.jayakumar@gmail.com>
2020-04-24 21:03:32 -0400

    initial branch commit

--- pettingzoo/utils/env.py
+++ pettingzoo/utils/env.py
@@ -1,3 +1,6 @@
+from pettingzoo.utils import EnvLogger
+
+
 class AECEnv(object):
     def __init__(self):
         pass
@@ -20,3 +23,10 @@ class AECEnv(object):
 
     def close(self):
         pass
+
+    def __getattr__(self, value):
+        if value in {"rewards", "dones", "agent_selection"}:
+            EnvLogger.error_field_before_reset(value)
+            return None
+        else:
+            raise AttributeError("'{}' object has no attribute '{}'".format(type(self).__name__, value))
