99d7d36377e023ba85d7c4df01409fb3e9864e81
Clemens Dieffendahl <cdieffendahl@gmail.com>
2020-04-28 10:34:42 +0200

    refactored to good default environment parameters

--- pettingzoo/classic/__init__.py
+++ pettingzoo/classic/__init__.py
@@ -9,4 +9,5 @@ from .texas_holdem import texas_holdem as texas_holdem_v0
 from .texas_holdem_no_limit import texas_holdem_no_limit as texas_holdem_no_limit_v0
 from .uno import uno as uno_v0
 from .dou_dizhu import dou_dizhu as dou_dizhu_v0
-from .gin_rummy import gin_rummy as gin_rummy_v0
+from .go import go_env as go_v0
+from .hanabi.hanabi import env as hanabi_v0
--- pettingzoo/classic/hanabi/test_hanabi.py
+++ pettingzoo/classic/hanabi/test_hanabi.py
@@ -22,13 +22,6 @@ class HanabiTest(TestCase):
             "random_start_player": 1
         }
 
-        cls.incomplete_config: dict = {
-            "colors": 5,
-            "ranks": 5,
-            "players": 3,
-            "max_information_tokens": 8,
-        }
-
         cls.config_values_out_of_reach: dict = {
             "colors": 20,
             "ranks": 5,
@@ -41,20 +34,9 @@ class HanabiTest(TestCase):
             "random_start_player": 1
         }
 
-    def test_preset(self):
-        test = env(preset_name=self.preset_name)
-        self.assertEqual(test.hanabi_env.__class__.__name__, 'HanabiEnv')
-
-    def test_preset_with_players(self):
-        test = env(preset_name=self.preset_name, players=self.player_count)
-        self.assertEqual(test.hanabi_env.__class__.__name__, 'HanabiEnv')
-
     def test_full_dictionary(self):
         test = env(**self.full_config)
         self.assertEqual(test.hanabi_env.__class__.__name__, 'HanabiEnv')
-
-    def test_incomplete_dictionary(self):
-        self.assertRaises(KeyError, env, **self.incomplete_config)
 
     def test_config_values_out_of_range(self):
         self.assertRaises(ValueError, env, **self.config_values_out_of_reach)
