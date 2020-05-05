fa878c5fee90332ed4ba6fac066174a46061b8fa
marioj <mario.jayakumar@gmail.com>
2020-05-05 18:54:00 -0400

    pettingzoo pypi version 0.1.0 commit

--- setup.py
+++ setup.py
@@ -12,4 +12,17 @@ setup(
     data_files=[("", ["LICENSE.txt"])],
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        "gym>=0.15.4",
+        "pygame==2.0.0.dev6",
+        "scikit-image>=0.16.2",
+        "numpy>=1.18.0",
+        "matplotlib>=3.1.2",
+        "pymunk>=5.6.0",
+        "gym[box2d]>=0.15.4",
+        "python-chess",
+        "rlcard >= 0.1.14",
+        "pynput",
+        "opencv-python"
+    ],
 )
