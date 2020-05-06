d0eee0cae079427173a1dd124ce328b0ba64e1db
marioj <mario.jayakumar@gmail.com>
2020-05-06 17:33:39 -0400

    remove 3.5 from supported versions

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=["Reinforcement Learning", "game", "RL", "AI", "gym"],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     data_files=[("", ["LICENSE.txt"])],
     packages=find_packages(),
     include_package_data=True,
