0a1b2873599c370e49ed9fb39217f5e29d29a178
Ben Black <weepingwillowben@gmail.com>
2020-05-06 14:52:14 -0400

    added env wrappers to gamma

--- pettingzoo/gamma/pistonball/pistonball.py
+++ pettingzoo/gamma/pistonball/pistonball.py
@@ -14,6 +14,7 @@ from pettingzoo import AECEnv
 from pettingzoo.utils import agent_selector
 from pettingzoo.utils import EnvLogger
 from .manual_control import manual_control
+from pettingzoo.utils import wrappers
 
 _image_library = {}
 
@@ -25,7 +26,16 @@ def get_image(path):
     return image
 
 
-class env(AECEnv):
+def env(**kwargs):
+    env = raw_env(**kwargs)
+    env = wrappers.AssertOutOfBoundsWrapper(env)
+    default_val = np.zeros((1,)) if env.continuous else 1
+    env = wrappers.NanNoOpWrapper(env, default_val)
+    env = wrappers.OrderEnforcingWrapper(env)
+    return env
+
+
+class raw_env(AECEnv):
 
     metadata = {'render.modes': ['human']}
 
