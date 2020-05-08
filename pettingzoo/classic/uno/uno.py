be647e61b2d0084422402c9ad18bd68e5374ce05
lssr <samaisantos@gmail.com>
2020-05-08 16:44:37 -0400

    Removed obsolete error handling code

--- pettingzoo/classic/uno/uno.py
+++ pettingzoo/classic/uno/uno.py
@@ -1,6 +1,5 @@
 from pettingzoo import AECEnv
 from pettingzoo.utils.agent_selector import agent_selector
-from pettingzoo.utils.env_logger import EnvLogger
 from gym import spaces
 import rlcard
 import random
@@ -48,27 +47,15 @@ class raw_env(AECEnv):
         return dict(zip(self.agents, list_of_list))
 
     def observe(self, agent):
-        if not self.has_reset:
-            EnvLogger.error_observe_before_reset()
         obs = self.env.get_state(self._name_to_int(agent))
         return obs['obs']
 
     def step(self, action, observe=True):
-        if not self.has_reset:
-            EnvLogger.error_step_before_reset()
-        backup_policy = "Game terminating with current player losing"
-        act_space = self.action_spaces[self.agent_selection]
-        if np.isnan(action).any():
-            EnvLogger.warn_action_is_NaN(backup_policy)
-        if not act_space.contains(action):
-            EnvLogger.warn_action_out_of_bound(action, act_space, backup_policy)
-
         if self.dones[self.agent_selection]:
             self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
             obs = False
         else:
             if action not in self.infos[self.agent_selection]['legal_moves']:
-                EnvLogger.warn_on_illegal_move()
                 self.rewards[self.agent_selection] = -1
                 self.dones = self._convert_to_dict([True for _ in range(self.num_agents)])
                 info_copy = self.infos[self.agent_selection]
@@ -121,4 +108,4 @@ class raw_env(AECEnv):
         print('\n')
 
     def close(self):
-        EnvLogger.warn_close_unrendered_env()
+        pass
