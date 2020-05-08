e134e2897b5f7cf98a7c8a226f36cdbf1b18b818
Jordan Terry <jordan@jkterry.ai>
2020-05-08 00:37:48 -0400

    flake8

--- pettingzoo/mpe/simple_world_comm.py
+++ pettingzoo/mpe/simple_world_comm.py
@@ -2,7 +2,6 @@ from ._mpe_utils.simple_env import SimpleEnv, make_env
 from .scenarios.simple_world_comm import Scenario
 
 
-
 class raw_env(SimpleEnv):
     def __init__(self, seed=None, num_good=2, num_adversaries=4, num_obstacles=1, num_food=2, num_forests=2, max_frames=100):
         scenario = Scenario()
