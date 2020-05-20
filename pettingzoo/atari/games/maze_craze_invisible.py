be2c5e5b4950a180469de9ecdab3a815170b9b61
Ben Black <weepingwillowben@gmail.com>
2020-05-19 20:51:19 -0400

    fixed maze craze games

--- pettingzoo/atari/games/maze_craze_invisible.py
+++ pettingzoo/atari/games/maze_craze_invisible.py
@@ -2,7 +2,7 @@ from ..base_atari_env import BaseAtariEnv, base_env_wrapper_fn
 
 
 def raw_env(**kwargs):
-    return BaseAtariEnv(game="maze_craze", num_players=2, mode_num=285, **kwargs)
+    return BaseAtariEnv(game="maze_craze", num_players=2, mode_num=23, **kwargs)
 
 
 env = base_env_wrapper_fn(raw_env)
