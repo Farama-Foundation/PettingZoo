9829643deef0f7b08532df2785f354f0ce853979
Ben Black <weepingwillowben@gmail.com>
2020-05-26 19:45:55 -0400

    updated ale to use multi-agent ale-py, also added grayspace observation spaces, fixed docs

--- pettingzoo/atari/base_atari_env.py
+++ pettingzoo/atari/base_atari_env.py
@@ -1,4 +1,4 @@
-import ale_py
+import multi_agent_ale_py
 import os
 from pettingzoo.utils import wrappers
 from pettingzoo import AECEnv
@@ -29,20 +29,20 @@ class BaseAtariEnv(AECEnv):
             num_players,
             mode_num=None,
             seed=None,
-            obs_type='image',
-            frameskip=3,
+            obs_type='rgb_image',
+            frameskip=4,
             repeat_action_probability=0.25,
             full_action_space=True):
         """Frameskip should be either a tuple (indicating a random range to
         choose from, with the top value exclude), or an int."""
 
-        assert obs_type in ('ram', 'image'), "obs_type must  either be 'ram' or 'image'"
+        assert obs_type in ('ram', 'rgb_image', "grayscale_image"), "obs_type must  either be 'ram' or 'rgb_image' or 'grayscale_image'"
         self.obs_type = obs_type
         self.full_action_space = full_action_space
         self.num_players = num_players
         self.np_random = seeding.np_random(seed)
 
-        self.ale = ale_py.ALEInterface()
+        self.ale = multi_agent_ale_py.ALEInterface()
 
         if seed is None:
             seed = seeding.create_seed(seed, max_bytes=4)
@@ -51,7 +51,7 @@ class BaseAtariEnv(AECEnv):
         self.ale.setInt(b"frame_skip", frameskip)
         self.ale.setFloat(b'repeat_action_probability', repeat_action_probability)
 
-        pathstart = os.path.dirname(ale_py.__file__)
+        pathstart = os.path.dirname(multi_agent_ale_py.__file__)
         final_path = os.path.join(pathstart, "ROM", game, game + ".bin")
         if not os.path.exists(final_path):
             raise IOError("rom {} is not installed. Please install roms using AutoROM tool (https://github.com/PettingZoo-Team/AutoROM)".format(game))
@@ -78,7 +78,11 @@ class BaseAtariEnv(AECEnv):
             observation_space = gym.spaces.Box(low=0, high=255, dtype=np.uint8, shape=(128,))
         else:
             (screen_width, screen_height) = self.ale.getScreenDims()
-            observation_space = spaces.Box(low=0, high=255, shape=(screen_height, screen_width, 3), dtype=np.uint8)
+            if obs_type == 'rgb_image':
+                num_channels = 3
+            elif obs_type == 'grayscale_image':
+                num_channels = 1
+            observation_space = spaces.Box(low=0, high=255, shape=(screen_height, screen_width, num_channels), dtype=np.uint8)
 
         self.num_agents = num_players
         player_names = ["first","second","third","fourth"]
@@ -106,7 +110,13 @@ class BaseAtariEnv(AECEnv):
         return self.observe(self.agent_selection) if observe else None
 
     def observe(self, agent):
-        return self.ale.getScreenRGB()
+        if self.obs_type == 'ram':
+            bytes = self.ale.getRAM()
+            return bytes
+        elif self.obs_type == 'rgb_image':
+            return self.ale.getScreenRGB()
+        elif self.obs_type == 'grayscale_image':
+            return self.ale.getScreenGrayscale()
 
     def step(self, action, observe=True):
         self._actions.append(action)
