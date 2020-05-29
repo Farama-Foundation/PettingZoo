292f40a7eef775b8099b3eb4a8119e64377d1fe9
Jordan Terry <jordan@jkterry.ai>
2020-05-28 20:54:08 -0400

    Update coords.py

--- pettingzoo/classic/go/coords.py
+++ pettingzoo/classic/go/coords.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Code from: https://github.com/tensorflow/minigo
+
 """Logic for dealing with coordinates.
 
 This introduces some helpers and terminology that are used throughout Minigo.
