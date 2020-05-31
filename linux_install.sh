5b9a8a79effe1d502eac1154eaab67f84528ec1d
marioj <mario.jayakumar@gmail.com>
2020-05-30 22:39:09 -0400

    fixed linux install script

--- linux_install.sh
+++ linux_install.sh
@@ -1,7 +1,7 @@
 export AUDIODEV=null
 CMAKE_DIR="${TRAVIS_BUILD_DIR}/cmk"
 mkdir ${CMAKE_DIR} && cd ${CMAKE_DIR}
-travis_retry wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz
+wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz
 tar -xvf cmake-3.17.3-Linux-x86_64.tar.gz > /dev/null
 mv cmake-3.17.3-Linux-x86_64 cmake-install
 PATH=${CMAKE_DIR}/cmake-install:${CMAKE_DIR}/cmake-install/bin:$PATH
