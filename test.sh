fa11ef92c34ae6e11f80f1889e0d8d25b3302ccc
marioj <mario.jayakumar@gmail.com>
2020-05-31 18:48:11 -0400

    controlled ci output

--- test.sh
+++ test.sh
@@ -9,3 +9,10 @@ save_obs=True
 python3 -m pettingzoo.tests.ci_test $pz_module $render $manual_control $bombardment $performance $save_obs
 
 python3 -m pettingzoo.tests.print_test
+
+if [ -s test_output.txt ]
+then
+    exit 5
+else
+    exit 0
+fi
