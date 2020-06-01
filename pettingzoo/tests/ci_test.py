18c280b724a261a173330996cfbfd72e64899d74
marioj <mario.jayakumar@gmail.com>
2020-06-01 19:03:58 -0400

    warnings no longer cause build to fail

--- pettingzoo/tests/ci_test.py
+++ pettingzoo/tests/ci_test.py
@@ -10,7 +10,7 @@ from .render_test import test_render
 from .error_tests import error_test
 from .seed_test import seed_test
 from .save_obs_test import test_save_obs
-from flake8.api import legacy as flake8
+import subprocess
 
 render = sys.argv[2] == 'True'
 manual_control = sys.argv[3] == 'True'
@@ -26,8 +26,12 @@ def perform_ci_test(env_id, render, manual_control, bombardment, performance, sa
     print("running game {}".format(env_id))
     env_module = all_environments[env_id]
     _env = env_module.env()
-    warning_collected = api_test.api_test(_env, render=render, verbose_progress=True)
-    warning_collected = warning_collected[:]
+    error_collected = []
+    try:
+        api_test.api_test(_env, render=render, verbose_progress=True)
+    except Exception as e:
+        error_collected.append("API Test: " + str(e))
+
     seed_test(env_module.env)
     # error_test(env_module.env())
 
@@ -35,14 +39,17 @@ def perform_ci_test(env_id, render, manual_control, bombardment, performance, sa
         test_save_obs(_env)
 
     if render:
-        test_render(_env)
+        try:
+            test_render(_env)
+        except Exception as e:
+            error_collected.append("Render Test:" + str(e))
 
     if manual_control:
         manual_control_fn = getattr(env_module, "manual_control", None)
         if manual_control_fn is not None:
             status = test_manual_control.test_manual_control(manual_control_fn)
             if status != 0:
-                warning_collected.append("Manual Control test failed")
+                error_collected.append("Manual Control test failed")
 
     if performance:
         _env = env_module.env()
@@ -50,16 +57,20 @@ def perform_ci_test(env_id, render, manual_control, bombardment, performance, sa
 
     if bombardment:
         _env = env_module.env()
-        bombardment_test.bombardment_test(_env, cycles=7000)
+        try:
+            bombardment_test.bombardment_test(_env, cycles=7000)
+        except Exception as e:
+            error_collected.append("Bombardment Test: " + str(e))
 
     # flake8 test
-    style_guide = flake8.get_style_guide(ignore=["E501", "E731", "E741", "E402", "F401", "W503"])
     file_name = "pettingzoo/" + env_id
-    report = style_guide.check_files([file_name])
-    if report.total_errors > 0:
-        warning_collected.append("Flake8 test failed")
+    style_guide = ["flake8", file_name, "E501", "E731", "E741", "E402", "F401", "W503"]
+    process = subprocess.Popen(style_guide, stdout=subprocess.PIPE, sterr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    flake8_res = stdout.decode("utf-8")
+    error_collected = error_collected + flake8_res.split("\n")
 
-    return warning_collected
+    return error_collected
 
 
 if env_id in all_prefixes:
