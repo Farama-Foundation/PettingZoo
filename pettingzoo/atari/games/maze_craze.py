576fe380c7e15cc671106b72c194c23e5d57ce9c
Ben Black <weepingwillowben@gmail.com>
2020-06-25 15:37:04 -0400

    added warnings for default parameters

--- pettingzoo/atari/games/maze_craze.py
+++ pettingzoo/atari/games/maze_craze.py
@@ -9,6 +9,8 @@ avaliable_versions = {
 
 
 def raw_env(game_version="robbers", visibilty_level=0, **kwargs):
+    if visibilty_level is 0:
+        warnings.warn("maze_craze has a visibilty_level that makes the map invisible, may be interesting to set this parameter")
     assert game_version in avaliable_versions, f"`game_version` parameter must be one of {avaliable_versions.keys()}"
     assert 0 <= visibilty_level < 4, "visibility level must be between 0 and 4, where 0 is 100% visiblity and 3 is 0% visibility"
     base_mode = (avaliable_versions[game_version] - 1) * 4
