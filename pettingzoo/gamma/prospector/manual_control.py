2b430486947314cf39e135932ab13ac8c3dd66f9
Yashas Lokesh <yashas.lokesh@gmail.com>
2020-07-04 13:49:39 -0400

    Removed unused assets, updated horizontal fence asset, renamed/moved assets, fixed code to load these, and fixed np.array deprecation warning in manual_control.py
    Signed-off-by: Yashas Lokesh <yashas.lokesh@gmail.com>

--- pettingzoo/gamma/prospector/manual_control.py
+++ pettingzoo/gamma/prospector/manual_control.py
@@ -14,10 +14,11 @@ def manual_control(**kwargs):
     quit_while = False
 
     while not done:
-        agent_actions = np.array(
-            [[0, 0, 0] for _ in range(const.NUM_PROSPECTORS)]
-            + [[0, 0] for _ in range(const.NUM_BANKERS)]
+        agent_actions = (
+            [np.array([0, 0, 0]) for _ in range(const.NUM_PROSPECTORS)] + 
+            [np.array([0, 0]) for _ in range(const.NUM_BANKERS)]
         )
+        print(agent_actions)
         for event in pygame.event.get():
             # Use left/right arrow keys to switch between agents
             # Use WASD to control bankers
