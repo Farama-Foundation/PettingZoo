ece8207d91b5953d0364b7d210de564330d7282a
Ben Black <weepingwillowben@gmail.com>
2020-07-08 19:20:29 -0400

    fixed flake8 error

--- pettingzoo/atari/combat_plane_v0.py
+++ pettingzoo/atari/combat_plane_v0.py
@@ -5,6 +5,7 @@ avaliable_versions = {
     "jet": 21,
 }
 
+
 def raw_env(game_version="bi-plane", guided_missile=True, **kwargs):
     assert game_version in avaliable_versions, "game_version must be either 'jet' or 'bi-plane'"
     mode = avaliable_versions[game_version] + (0 if guided_missile else 1)
