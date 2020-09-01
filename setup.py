8a30f31f9eb34c418a6c260a83c5aa03c70f693a
Ben Black <weepingwillowben@gmail.com>
2020-08-31 20:41:03 -0400

    fixed ale depending version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_version():
     raise RuntimeError("bad version data in __init__.py")
 
 extras = {
-    "atari": ["multi_agent_ale_py==0.1.8.dev0", "pygame==2.0.0.dev10"],
+    "atari": ["multi_agent_ale_py==0.1.8", "pygame==2.0.0.dev10"],
     "classic": ["python-chess", "rlcard >= 0.2.5", "python-shogi", "hanabi_learning_environment==0.0.1"],
     "butterfly": ["pygame==2.0.0.dev10", "pymunk==5.6.0"],
     "magent": ["magent==0.1.12"],
