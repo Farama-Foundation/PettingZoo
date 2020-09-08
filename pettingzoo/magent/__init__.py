3b3f99c1b77a4a0ad67483c74727e57bbfc8fc3d
Ben Black <weepingwillowben@gmail.com>
2020-09-07 22:11:29 -0400

    bumped versions on environments with death

---
+++ pettingzoo/magent/__init__.py
@@ -0,0 +1,7 @@
+from pettingzoo.utils.depreciated_module import DepreciatedModule
+
+battle_v0 = DepreciatedModule("battle", "v0", "v1")
+battlefield_v0 = DepreciatedModule("battlefield", "v0", "v1")
+combined_arms_v0 = DepreciatedModule("combined_arms", "v0", "v1")
+gather_v0 = DepreciatedModule("gather", "v0", "v1")
+tiger_deer_v0 = DepreciatedModule("tiger_deer", "v0", "v1")
---
+++ pettingzoo/sisl/__init__.py
@@ -0,0 +1,3 @@
+from pettingzoo.utils.depreciated_module import DepreciatedModule
+
+pursuit_v0 = DepreciatedModule("pursuit", "v0", "v1")
