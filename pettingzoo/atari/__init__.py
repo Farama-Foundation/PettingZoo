5b8ff6c86abcd3f7131fa7ea18c22966ec3096a2
Ben Black <weepingwillowben@gmail.com>
2020-09-07 22:05:03 -0400

    replaced depreciated file with depreciated class

---
+++ pettingzoo/atari/__init__.py
@@ -0,0 +1,8 @@
+from pettingzoo.utils.depreciated_module import DepreciatedModule
+
+entombed_competitive_v0 = DepreciatedModule("entombed_competitive", "v0", "v1")
+video_checkers_v0 = DepreciatedModule("video_checkers", "v0", "v1")
+othello_v0 = DepreciatedModule("othello", "v0", "v1")
+tennis_v0 = DepreciatedModule("tennis", "v0", "v1")
+double_dunk_v0 = DepreciatedModule("double_dunk", "v0", "v1")
+pong_quadrapong_v0 = DepreciatedModule("pong_quadrapong", "v0", "v1")
