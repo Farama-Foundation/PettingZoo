5c0e6f22a2821eac5a7b65c41886b80bb22c7c6b
Ben Black <weepingwillowben@gmail.com>
2020-09-07 22:05:03 -0400

    replaced depreciated file with depreciated class

---
+++ pettingzoo/utils/depreciated_module.py
@@ -0,0 +1,12 @@
+from pettingzoo.utils.error import DeprecatedEnv
+
+
+class DepreciatedModule:
+    def __init__(self, name, old_version, new_version):
+        def env(*args, **kwargs):
+            raise DeprecatedEnv(f"{name}_{old_version} is now depreciated, use {name}_{new_version} instead")
+
+        self.env = env
+        self.raw_env = env
+        self.parallel_env = env
+        self.manual_control = env
