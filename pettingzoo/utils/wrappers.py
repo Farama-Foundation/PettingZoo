d602f81deadf5803a2be6abf18af170132e3844f
Ben Black <weepingwillowben@gmail.com>
2020-09-21 00:31:01 -0400

    fixed broken variable

--- pettingzoo/utils/wrappers.py
+++ pettingzoo/utils/wrappers.py
@@ -123,7 +123,7 @@ class AECIterable:
         self.max_iter = max_iter
 
     def __iter__(self):
-        return AECOrderEnforcingIterator(self.env, self.max_agent_iter)
+        return AECOrderEnforcingIterator(self.env, self.max_iter)
 
 
 class AECIterator:
