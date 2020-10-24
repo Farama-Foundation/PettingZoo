5c806e4429c5dfd6e30623523e8651c74cc218e5
Ben Black <weepingwillowben@gmail.com>
2020-10-24 17:30:28 -0400

    fixed tests

--- pettingzoo/tests/bombardment_test.py
+++ pettingzoo/tests/bombardment_test.py
@@ -14,7 +14,9 @@ def bombardment_test(env, cycles=10000):
             print("\t50% through bombardment test")
         for agent in env.agent_iter(env.num_agents):  # step through every agent once with observe=True
             obs, reward, done, info = env.last()
-            if not done and 'legal_moves' in env.infos[agent]:
+            if done:
+                action = None
+            elif 'legal_moves' in env.infos[agent]:
                 action = random.choice(env.infos[agent]['legal_moves'])
             else:
                 action = env.action_spaces[agent].sample()
