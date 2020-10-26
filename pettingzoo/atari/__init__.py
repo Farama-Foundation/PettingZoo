b72bf971219b4abc73584ee85e0e29af9ad84cbd
Ben Black <weepingwillowben@gmail.com>
2020-10-26 10:20:54 -0400

    updated entombed cooperative reward docs for ALE change

--- pettingzoo/atari/__init__.py
+++ pettingzoo/atari/__init__.py
@@ -2,6 +2,7 @@ from pettingzoo.utils.depreciated_module import DepreciatedModule
 
 double_dunk_v0 = DepreciatedModule("double_dunk", "v0", "v1")
 entombed_competitive_v0 = DepreciatedModule("entombed_competitive", "v0", "v1")
+entombed_cooperative_v0 = DepreciatedModule("entombed_cooperative", "v0", "v1")
 joust_v0 = DepreciatedModule("joust", "v0", "v1")
 mario_bros_v0 = DepreciatedModule("mario_bros", "v0", "v1")
 maze_craze_v0 = DepreciatedModule("maze_craze", "v0", "v1")
