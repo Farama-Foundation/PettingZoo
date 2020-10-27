61867a6ccf1fe08bd76e5d5f346088b2e79a3fdd
Ben Black <weepingwillowben@gmail.com>
2020-10-26 22:11:24 -0400

    fixed api test

--- pettingzoo/tests/api_test.py
+++ pettingzoo/tests/api_test.py
@@ -219,8 +219,8 @@ def api_test(env, render=False, verbose_progress=False):
     observation_0, _, _, _ = env.last()
     test_observation(observation_0, observation_0)
 
-    observation_0, _, _, _ = env.last(observe=False)
-    assert observation_0 is None, "last must return a None when observe=False"
+    non_observe, _, _, _ = env.last(observe=False)
+    assert non_observe is None, "last must return a None when observe=False"
 
     progress_report("Finished test_observation")
 
