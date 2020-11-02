d073ee1b01d1440e6bc723212652130d51cb81c7
Jordan Terry <jordan@jkterry.ai>
2020-11-01 20:39:36 -0500

    uncomment things

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ manual_control=False
 performance=False
 save_obs=False
 
-# runs all tests in .travis.yml
-# pytest ./pettingzoo/tests/pytest_runner.py
-# bash ./check_style.sh
-# pytest ./pettingzoo/tests/all_parameter_combs.py
-# python3 -m pettingzoo.tests.ci_test atari $render $manual_control $performance $save_obs
-# python3 -m pettingzoo.tests.ci_test classic $render $manual_control $performance $save_obs
+runs all tests in .travis.yml
+pytest ./pettingzoo/tests/pytest_runner.py
+bash ./check_style.sh
+pytest ./pettingzoo/tests/all_parameter_combs.py
+python3 -m pettingzoo.tests.ci_test atari $render $manual_control $performance $save_obs
+python3 -m pettingzoo.tests.ci_test classic $render $manual_control $performance $save_obs
 python3 -m pettingzoo.tests.ci_test butterfly $render $manual_control $performance $save_obs
-# python3 -m pettingzoo.tests.ci_test mpe $render $manual_control $performance $save_obs
-# python3 -m pettingzoo.tests.ci_test magent $render $manual_control $performance $save_obs
+python3 -m pettingzoo.tests.ci_test mpe $render $manual_control $performance $save_obs
+python3 -m pettingzoo.tests.ci_test magent $render $manual_control $performance $save_obs
 python3 -m pettingzoo.tests.ci_test sisl $render $manual_control $performance $save_obs
