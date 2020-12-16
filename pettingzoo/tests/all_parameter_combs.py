b40780dc1f274a726e7f58f7ed08e33a70f769ff
Ben Black <weepingwillowben@gmail.com>
2020-12-15 22:38:54 -0500

    fixed battlefield minimum map size

--- pettingzoo/tests/all_parameter_combs.py
+++ pettingzoo/tests/all_parameter_combs.py
@@ -79,7 +79,7 @@ parameterized_envs = [
     (battle_v2.env, dict(minimap_mode=False)),
     (adversarial_pursuit_v2.env, dict(map_size=15)),
     (battle_v2.env, dict(map_size=15)),
-    (battlefield_v2.env, dict(map_size=45)),
+    (battlefield_v2.env, dict(map_size=46)),
     (combined_arms_v3.env, dict(map_size=16)),
     (tiger_deer_v3.env, dict(map_size=15)),
 
