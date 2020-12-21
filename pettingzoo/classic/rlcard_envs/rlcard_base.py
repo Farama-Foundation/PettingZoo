6a8b2c70b1faaeb4be4bede0b09d197db58615e3
Paaras Bhandari <paarasbhandari@gmail.com>
2020-12-21 00:29:39 -0500

    texas holdem, mahjong, leduc holdem

--- pettingzoo/classic/rlcard_envs/rlcard_base.py
+++ pettingzoo/classic/rlcard_envs/rlcard_base.py
@@ -26,7 +26,10 @@ class RLCardBase(AECEnv):
         else:
             self._dtype = dtype
 
-        self.observation_spaces = self._convert_to_dict([spaces.Box(low=0.0, high=1.0, shape=obs_shape, dtype=self._dtype) for _ in range(self.num_agents)])
+        self.observation_spaces = self._convert_to_dict(
+            [spaces.Dict({'observation': spaces.Box(low=0.0, high=1.0, shape=obs_shape, dtype=self._dtype),
+                          'action_mask': spaces.Box(low=0, high=1, shape=(self.env.game.get_action_num(),),
+                                                    dtype=np.int8)}) for _ in range(self.num_agents)])
         self.action_spaces = self._convert_to_dict([spaces.Discrete(self.env.game.get_action_num()) for _ in range(self.num_agents)])
 
     def seed(self, seed=None):
@@ -46,7 +49,14 @@ class RLCardBase(AECEnv):
 
     def observe(self, agent):
         obs = self.env.get_state(self._name_to_int(agent))
-        return obs['obs'].astype(self._dtype)
+        observation = obs['obs'].astype(self._dtype)
+
+        legal_moves = self.infos[agent]['legal_moves']
+        action_mask = np.zeros(self.env.game.get_action_num(), int)
+        for i in legal_moves:
+            action_mask[i] = 1
+
+        return {'observation': observation, 'action_mask': action_mask}
 
     def step(self, action):
         if self.dones[self.agent_selection]:
