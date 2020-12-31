412bb9381a023cceed89dc6cf3a07cf9f649f0b0
Jordan Terry <jordan@jkterry.ai>
2020-12-31 02:43:33 -0500

    more fixes

--- pettingzoo/test/ci_test.py
+++ pettingzoo/test/ci_test.py
@@ -6,7 +6,6 @@ from .manual_control_test import manual_control_test
 from .all_modules import all_environments, manual_environments
 from .all_modules import all_prefixes
 from .render_test import render_test
-from .error_tests import error_test
 from .seed_test import seed_test
 from .save_obs_test import test_save_obs
 from .max_cycles_test import max_cycles_test
