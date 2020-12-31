2f462641b7885cb43367e06a9fad64ac1a78fc72
Jordan Terry <jordan@jkterry.ai>
2020-12-30 20:50:55 -0500

    allow for external test calling

---
+++ pettingzoo/tests/__init__.py
@@ -0,0 +1,8 @@
+from .all_modules import all_environments, manual_environments
+from .all_modules import all_prefixes
+from .render_test import render_test
+from .error_tests import error_test
+from .seed_test import seed_test
+from .save_obs_test import test_save_obs
+from .max_cycles_test import max_cycles_test
+from .parallel_test import parallel_play_test
