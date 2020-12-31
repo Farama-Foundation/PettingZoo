33019e394a2ad94e47761c6c5bd95bf96d128114
Justin Terry <justinkterry@gmail.com>
2020-12-31 00:17:14 -0500

    fix typo

--- pettingzoo/tests/ci_test.py
+++ pettingzoo/tests/ci_test.py
@@ -2,7 +2,7 @@ import sys
 from .api_test import api_test
 from .bombardment_test import bombardment_test
 from .performance_benchmark import performance_benchmark
-from .manual_control_test import test_manual_control
+from .manual_control_test import manual_control_test
 from .all_modules import all_environments, manual_environments
 from .all_modules import all_prefixes
 from .render_test import render_test
