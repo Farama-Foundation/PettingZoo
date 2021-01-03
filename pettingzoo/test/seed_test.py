62546f1e217d0d61bf968f7779e235325a225cfb
Ben Black <weepingwillowben@gmail.com>
2021-01-02 22:38:59 -0700

    made default no-op for seed() method, removed warning from seed test

--- pettingzoo/test/seed_test.py
+++ pettingzoo/test/seed_test.py
@@ -96,8 +96,3 @@ def seed_test(env_constructor, num_cycles=10):
 
     assert check_environment_deterministic(env1, env2, num_cycles), \
         ("The environment gives different results on multiple runs when intialized with the same seed. This is usually a sign that you are using np.random or random modules directly, which uses a global random state.")
-
-    env1.seed(base_seed)
-    env2.seed(base_seed + 1)
-    if check_environment_deterministic(env1, env2, num_cycles):
-        warnings.warn("The environment gives same results on multiple runs when intialized by default. By default, environments that take a seed argument should be nondeterministic")
