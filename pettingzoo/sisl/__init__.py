f5318f8eb6644b00acdd703be765b7cf106a8e54
Ben Black <weepingwillowben@gmail.com>
2021-01-02 19:10:43 -0700

    bumped multiwalker version

--- pettingzoo/sisl/__init__.py
+++ pettingzoo/sisl/__init__.py
@@ -1,10 +1,11 @@
 from pettingzoo.utils.depreciated_module import DepreciatedModule
 
-multiwalker_v0 = DepreciatedModule("multiwalker", "v0", "v5")
-multiwalker_v1 = DepreciatedModule("multiwalker", "v1", "v5")
-multiwalker_v2 = DepreciatedModule("multiwalker", "v2", "v5")
-multiwalker_v3 = DepreciatedModule("multiwalker", "v3", "v5")
-multiwalker_v4 = DepreciatedModule("multiwalker", "v4", "v5")
+multiwalker_v0 = DepreciatedModule("multiwalker", "v0", "v6")
+multiwalker_v1 = DepreciatedModule("multiwalker", "v1", "v6")
+multiwalker_v2 = DepreciatedModule("multiwalker", "v2", "v6")
+multiwalker_v3 = DepreciatedModule("multiwalker", "v3", "v6")
+multiwalker_v4 = DepreciatedModule("multiwalker", "v4", "v6")
+multiwalker_v5 = DepreciatedModule("multiwalker", "v5", "v6")
 pursuit_v0 = DepreciatedModule("pursuit", "v0", "v3")
 pursuit_v1 = DepreciatedModule("pursuit", "v1", "v3")
 pursuit_v2 = DepreciatedModule("pursuit", "v2", "v3")
