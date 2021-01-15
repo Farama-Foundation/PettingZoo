cff2ef3474184af412a60e0020765bed27cbcaf4
Justin Terry <justinkterry@gmail.com>
2021-01-15 13:18:44 -0500

    fix save_observation import

--- pettingzoo/utils/__init__.py
+++ pettingzoo/utils/__init__.py
@@ -1,3 +1,4 @@
 from .random_demo import random_demo
 from .agent_selector import agent_selector
 from .average_total_reward import average_total_reward
+from .save_observation import save_observation
