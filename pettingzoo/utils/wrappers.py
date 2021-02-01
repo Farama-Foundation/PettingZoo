720bdae7cc7d8fce43d321ffd3594c402ce3063d
Ben Black <weepingwillowben@gmail.com>
2021-01-31 20:02:40 -0500

    moved nan error

--- pettingzoo/utils/wrappers.py
+++ pettingzoo/utils/wrappers.py
@@ -168,6 +168,8 @@ class ClipOutOfBoundsWrapper(BaseWrapper):
         assert all(isinstance(space, Box) for space in self.action_spaces.values()), "should only use ClipOutOfBoundsWrapper for Box spaces"
 
     def step(self, action):
+        if np.isnan(action):
+            EnvLogger.error_nan_action()
         space = self.action_spaces[self.agent_selection]
         if not (action is None and self.dones[self.agent_selection]) and not space.contains(action):
             assert space.shape == action.shape, "action should have shape {}, has shape {}".format(space.shape, action.shape)
@@ -229,8 +231,6 @@ class OrderEnforcingWrapper(BaseWrapper):
         self._has_reset = False
 
     def step(self, action):
-        if np.isnan(action):
-            EnvLogger.error_nan_action()
         if not self._has_reset:
             EnvLogger.error_step_before_reset()
         elif not self.agents:
