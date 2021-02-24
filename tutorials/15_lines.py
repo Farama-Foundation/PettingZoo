9e83d24a8f584fd303c0f416fa59629e1c85d61c
Justin Terry <justinkterry@gmail.com>
2021-02-23 21:17:50 -0500

    Update 15_lines.py

--- tutorials/15_lines.py
+++ tutorials/15_lines.py
@@ -5,9 +5,7 @@ import supersuit as ss
 
 env = pistonball_v3.parallel_env(n_pistons=20, local_ratio=0, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
 env = ss.color_reduction_v0(env, mode='B')
-env = ss.dtype_v0(env, 'float32')
 env = ss.resize_v0(env, x_size=84, y_size=84)
-env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
 env = ss.frame_stack_v1(env, 3)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 8, num_cpus=4, base_class='stable_baselines')
@@ -20,9 +18,7 @@ model.save("policy")
 
 env = pistonball_v3.env()
 env = ss.color_reduction_v0(env, mode='B')
-env = ss.dtype_v0(env, 'float32')
 env = ss.resize_v0(env, x_size=84, y_size=84)
-env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
 env = ss.frame_stack_v1(env, 3)
 
 model = PPO2.load("policy")
