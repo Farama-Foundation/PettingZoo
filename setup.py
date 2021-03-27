8dd28b9176e14d72b8242649bd85ce99334b7758
Jordan Terry <jordan@jkterry.ai>
2021-03-27 11:56:26 -0400

    comma

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Reinforcement Learning", "game", "RL", "AI", "gym"],
     python_requires=">=3.6, <3.10",
-    package_data={'':['*.txt']}
+    package_data={'':['*.txt']},
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
