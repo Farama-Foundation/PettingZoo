18a33efb2ae261380d68e257e488c21e86743c05
Jordan Terry <jordan@jkterry.ai>
2021-03-27 12:43:28 -0400

    finally fix licenses

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Reinforcement Learning", "game", "RL", "AI", "gym"],
     python_requires=">=3.6, <3.10",
-    package_data={'':['*.txt']},
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
