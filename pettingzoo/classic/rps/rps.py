441f271658c08ca796e5f84efc24956971e07c53
Rodrigo de Lazcano <r.l.p.v96@gmail.com>
2021-05-26 10:40:53 -0400

    merge rps and rpsls

--- pettingzoo/classic/rps/rps.py
+++ pettingzoo/classic/rps/rps.py
@@ -8,9 +8,10 @@ from pettingzoo.utils.conversions import parallel_wrapper_fn
 ROCK = 0
 PAPER = 1
 SCISSORS = 2
-NONE = 3
-MOVES = ["ROCK", "PAPER", "SCISSORS", "None"]
-NUM_ITERS = 100
+LIZARD = 3
+SPOCK = 4
+NONE = 5
+MOVES = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK", "None"]
 
 
 def env(**kwargs):
@@ -26,19 +27,23 @@ parallel_env = parallel_wrapper_fn(env)
 
 class raw_env(AECEnv):
     """Two-player environment for rock paper scissors.
+    Expandable environment to rock paper scissors lizard spock.
     The observation is simply the last opponent action."""
 
     metadata = {'render.modes': ['human'], "name": "rps_v1"}
 
-    def __init__(self, max_cycles=150):
+    def __init__(self, max_cycles=150, lizard_spock=False):
         self.max_cycles = max_cycles
         
         self.agents = ["player_" + str(r) for r in range(2)]
         self.possible_agents = self.agents[:]
         self.agent_name_mapping = dict(zip(self.agents, list(range(self.num_agents))))
-
-        self.action_spaces = {agent: Discrete(3) for agent in self.agents}
-        self.observation_spaces = {agent: Discrete(4) for agent in self.agents}
+        if lizard_spock:
+            self.action_spaces = {agent: Discrete(5) for agent in self.agents}
+            self.observation_spaces = {agent: Discrete(6) for agent in self.agents}
+        else:
+            self.action_spaces = {agent: Discrete(3) for agent in self.agents}
+            self.observation_spaces = {agent: Discrete(4) for agent in self.agents}
 
         self.reinit()
 
@@ -85,12 +90,32 @@ class raw_env(AECEnv):
                 (ROCK, ROCK): (0, 0),
                 (ROCK, PAPER): (-1, 1),
                 (ROCK, SCISSORS): (1, -1),
+                (ROCK, LIZARD): (1, -1),
+                (ROCK, SPOCK): (-1, 1),
+
                 (PAPER, ROCK): (1, -1),
                 (PAPER, PAPER): (0, 0),
                 (PAPER, SCISSORS): (-1, 1),
+                (PAPER, LIZARD): (-1, 1),
+                (PAPER, SPOCK): (1, -1),
+
                 (SCISSORS, ROCK): (-1, 1),
                 (SCISSORS, PAPER): (1, -1),
                 (SCISSORS, SCISSORS): (0, 0),
+                (SCISSORS, LIZARD): (1, -1),
+                (SCISSORS, SPOCK): (-1, 1),
+
+                (LIZARD, ROCK): (-1, 1),
+                (LIZARD, PAPER): (1, -1),
+                (LIZARD, SCISSORS): (-1, 1),
+                (LIZARD, LIZARD): (0, 0),
+                (LIZARD, SPOCK): (1, -1),
+
+                (SPOCK, ROCK): (1, -1),
+                (SPOCK, PAPER): (-1, 1),
+                (SPOCK, SCISSORS): (1, -1),
+                (SPOCK, LIZARD): (-1, 1),
+                (SPOCK, SPOCK): (0, 0),
             }[(self.state[self.agents[0]], self.state[self.agents[1]])]
 
             self.num_moves += 1
