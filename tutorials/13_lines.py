c05000d4a4dad9e5959e74be79284ad660e227b8
Justin Terry <justinkterry@gmail.com>
2021-06-19 11:18:30 -0400

    update hyperparameters

--- tutorials/13_lines.py
+++ tutorials/13_lines.py
@@ -9,8 +9,7 @@ env = ss.resize_v0(env, x_size=84, y_size=84)
 env = ss.frame_stack_v1(env, 3)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 8, num_cpus=4, base_class='stable_baselines3')
-
-model = PPO(CnnPolicy, env, verbose=3, gamma=0.99, n_steps=125, ent_coef=0.01, learning_rate=0.00025, vf_coef=0.5, max_grad_norm=0.5, gae_lambda=0.95, n_epochs=4, clip_range=0.2, clip_range_vf=1)
+model = PPO(CnnPolicy, env, verbose=3, gamma=0.95, n_steps=256, ent_coef=0.0905168, learning_rate=0.00062211, vf_coef=0.042202, max_grad_norm=0.9, gae_lambda=0.99, n_epochs=5, clip_range=0.3, batch_size=256)
 model.learn(total_timesteps=2000000)
 model.save("policy")
 
