6122923c6f746a1ddc955cac8f899a6251894736
Kyle Sang <sang.kyle@gmail.com>
2021-06-23 00:38:05 -0400

    Fixed rgb_array for go

--- pettingzoo/classic/go/go_env.py
+++ pettingzoo/classic/go/go_env.py
@@ -5,6 +5,7 @@ from . import go
 from . import coords
 import numpy as np
 import os
+import pygame
 from pettingzoo.utils import wrappers
 
 
@@ -139,64 +140,65 @@ class raw_env(AECEnv):
         screen_width = 1026
         screen_height = 1026
 
-        if mode == "human":
-            import pygame
-
-            if self.screen is None:
+        if self.screen is None:
+            if mode == "human":
                 pygame.init()
                 self.screen = pygame.display.set_mode((screen_width, screen_height))
-
+            else:
+                self.screen = pygame.Surface((screen_width, screen_height))
+        if mode == "human":
             pygame.event.get()
 
-            size = go.N
+        size = go.N
 
-            # Load and scale all of the necessary images
-            tile_size = (screen_width) / size
+        # Load and scale all of the necessary images
+        tile_size = (screen_width) / size
 
-            black_stone = get_image(os.path.join('img', 'GoBlackPiece.png'))
-            black_stone = pygame.transform.scale(black_stone, (int(tile_size * (5 / 6)), int(tile_size * (5 / 6))))
+        black_stone = get_image(os.path.join('img', 'GoBlackPiece.png'))
+        black_stone = pygame.transform.scale(black_stone, (int(tile_size * (5 / 6)), int(tile_size * (5 / 6))))
 
-            white_stone = get_image(os.path.join('img', 'GoWhitePiece.png'))
-            white_stone = pygame.transform.scale(white_stone, (int(tile_size * (5 / 6)), int(tile_size * (5 / 6))))
+        white_stone = get_image(os.path.join('img', 'GoWhitePiece.png'))
+        white_stone = pygame.transform.scale(white_stone, (int(tile_size * (5 / 6)), int(tile_size * (5 / 6))))
 
-            tile_img = get_image(os.path.join('img', 'GO_Tile0.png'))
+        tile_img = get_image(os.path.join('img', 'GO_Tile0.png'))
+        tile_img = pygame.transform.scale(tile_img, ((int(tile_size * (7 / 6))), int(tile_size * (7 / 6))))
+
+        # blit board tiles
+        for i in range(1, size - 1):
+            for j in range(1, size - 1):
+                self.screen.blit(tile_img, ((i * (tile_size)), int(j) * (tile_size)))
+
+        for i in range(1, 9):
+            tile_img = get_image(os.path.join('img', 'GO_Tile' + str(i) + '.png'))
             tile_img = pygame.transform.scale(tile_img, ((int(tile_size * (7 / 6))), int(tile_size * (7 / 6))))
+            for j in range(1, size - 1):
+                if i == 1:
+                    self.screen.blit(tile_img, (0, int(j) * (tile_size)))
+                elif i == 2:
+                    self.screen.blit(tile_img, ((int(j) * (tile_size)), 0))
+                elif i == 3:
+                    self.screen.blit(tile_img, ((size - 1) * (tile_size), int(j) * (tile_size)))
+                elif i == 4:
+                    self.screen.blit(tile_img, ((int(j) * (tile_size)), (size - 1) * (tile_size)))
+            if i == 5:
+                self.screen.blit(tile_img, (0, 0))
+            elif i == 6:
+                self.screen.blit(tile_img, ((size - 1) * (tile_size), 0))
+            elif i == 7:
+                self.screen.blit(tile_img, ((size - 1) * (tile_size), (size - 1) * (tile_size)))
+            elif i == 8:
+                self.screen.blit(tile_img, (0, (size - 1) * (tile_size)))
 
-            # blit board tiles
-            for i in range(1, size - 1):
-                for j in range(1, size - 1):
-                    self.screen.blit(tile_img, ((i * (tile_size)), int(j) * (tile_size)))
+        offset = tile_size * (1 / 6)
+        # Blit the necessary chips and their positions
+        for i in range(0, size):
+            for j in range(0, size):
+                if self._go.board[i][j] == -1:
+                    self.screen.blit(black_stone, ((i * (tile_size) + offset), int(j) * (tile_size) + offset))
+                elif self._go.board[i][j] == 1:
+                    self.screen.blit(white_stone, ((i * (tile_size) + offset), int(j) * (tile_size) + offset))
 
-            for i in range(1, 9):
-                tile_img = get_image(os.path.join('img', 'GO_Tile' + str(i) + '.png'))
-                tile_img = pygame.transform.scale(tile_img, ((int(tile_size * (7 / 6))), int(tile_size * (7 / 6))))
-                for j in range(1, size - 1):
-                    if i == 1:
-                        self.screen.blit(tile_img, (0, int(j) * (tile_size)))
-                    elif i == 2:
-                        self.screen.blit(tile_img, ((int(j) * (tile_size)), 0))
-                    elif i == 3:
-                        self.screen.blit(tile_img, ((size - 1) * (tile_size), int(j) * (tile_size)))
-                    elif i == 4:
-                        self.screen.blit(tile_img, ((int(j) * (tile_size)), (size - 1) * (tile_size)))
-                if i == 5:
-                    self.screen.blit(tile_img, (0, 0))
-                elif i == 6:
-                    self.screen.blit(tile_img, ((size - 1) * (tile_size), 0))
-                elif i == 7:
-                    self.screen.blit(tile_img, ((size - 1) * (tile_size), (size - 1) * (tile_size)))
-                elif i == 8:
-                    self.screen.blit(tile_img, (0, (size - 1) * (tile_size)))
-
-            offset = tile_size * (1 / 6)
-            # Blit the necessary chips and their positions
-            for i in range(0, size):
-                for j in range(0, size):
-                    if self._go.board[i][j] == -1:
-                        self.screen.blit(black_stone, ((i * (tile_size) + offset), int(j) * (tile_size) + offset))
-                    elif self._go.board[i][j] == 1:
-                        self.screen.blit(white_stone, ((i * (tile_size) + offset), int(j) * (tile_size) + offset))
-
+        if mode == "human":
             pygame.display.update()
 
         observation = np.array(pygame.surfarray.pixels3d(self.screen))
