0cec62b2503ed3d11e5cd9357bce5b081460700b
Rohan138 <rohanpotdar138@gmail.com>
2021-07-08 01:18:24 -0400

    Ran release_test.sh

--- release_test.sh
+++ release_test.sh
@@ -6,13 +6,13 @@ performance=True
 save_obs=True
 num_cycles=1000
 
-pytest pettingzoo/test/pytest_runner.py
+pytest ./test/pytest_runner.py
 bash ./check_style.sh
-python3 -m pettingzoo.test.print_test
-pytest ./pettingzoo/test/all_parameter_combs.py
-python3 -m pettingzoo.test.ci_test atari $num_cycles $render $manual_control $performance $save_obs
-python3 -m pettingzoo.test.ci_test classic $num_cycles $render $manual_control $performance $save_obs
-python3 -m pettingzoo.test.ci_test butterfly $num_cycles $render $manual_control $performance $save_obs
-python3 -m pettingzoo.test.ci_test mpe $num_cycles $render $manual_control $performance $save_obs
-python3 -m pettingzoo.test.ci_test magent $num_cycles $render $manual_control $performance $save_obs
-python3 -m pettingzoo.test.ci_test sisl $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.print_test
+pytest ./test/all_parameter_combs.py
+python3 -m test.ci_test atari $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.ci_test classic $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.ci_test butterfly $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.ci_test mpe $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.ci_test magent $num_cycles $render $manual_control $performance $save_obs
+python3 -m test.ci_test sisl $num_cycles $render $manual_control $performance $save_obs
