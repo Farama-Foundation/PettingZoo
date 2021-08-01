73fca80169c277902b62e33191f9c9406e77eb3d
Ben Black <weepingwillowben@gmail.com>
2021-08-01 14:35:52 -0600

    fixed butterfly rgb_array with no display, replaced convert calls

---
+++ pettingzoo/butterfly/knights_archers_zombies/src/img.py
@@ -0,0 +1,10 @@
+import pygame
+from os import path as os_path
+
+
+def get_image(path):
+    cwd = os_path.dirname(os_path.dirname(__file__))
+    image = pygame.image.load(cwd + '/' + path)
+    sfc = pygame.Surface(image.get_size(), flags=pygame.SRCALPHA)
+    sfc.blit(image, (0, 0))
+    return sfc
--- pettingzoo/classic/rlcard_envs/texas_holdem.py
+++ pettingzoo/classic/rlcard_envs/texas_holdem.py
@@ -15,8 +15,10 @@ import os
 def get_image(path):
     from os import path as os_path
     cwd = os_path.dirname(__file__)
-    image = pygame.image.load(cwd + '/' + path).convert()
-    return image
+    image = pygame.image.load(cwd + '/' + path)
+    sfc = pygame.Surface(image.get_size(), flags=pygame.SRCALPHA)
+    sfc.blit(image, (0, 0))
+    return sfc
 
 
 def get_font(path, size):
