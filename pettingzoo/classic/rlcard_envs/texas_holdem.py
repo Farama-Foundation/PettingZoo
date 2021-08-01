8c2736b70c8d38e25b2ebc638c04e54e1bc517fa
Ben Black <weepingwillowben@gmail.com>
2021-08-01 15:29:13 -0600

    reverted render fix in texas holdem, where it is slower

--- pettingzoo/classic/rlcard_envs/texas_holdem.py
+++ pettingzoo/classic/rlcard_envs/texas_holdem.py
@@ -16,9 +16,7 @@ def get_image(path):
     from os import path as os_path
     cwd = os_path.dirname(__file__)
     image = pygame.image.load(cwd + '/' + path)
-    sfc = pygame.Surface(image.get_size(), flags=pygame.SRCALPHA)
-    sfc.blit(image, (0, 0))
-    return sfc
+    return image
 
 
 def get_font(path, size):
