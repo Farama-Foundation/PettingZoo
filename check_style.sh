4907be6d455ff2ed6499ff9a94b31e20507847d6
Ben Black <weepingwillowben@gmail.com>
2021-09-06 12:39:20 -0600

    updated isort checkstyle

--- check_style.sh
+++ check_style.sh
@@ -2,4 +2,4 @@
 bandit --recursive --skip B101,B301,B303,B311,B403,B404,B603,B607 .
 codespell
 flake8 pettingzoo/ test/
-isort -w 100 -o magent -p pettingzoo -c -rc pettingzoo
+isort -w 100 -o magent -p pettingzoo -c pettingzoo
