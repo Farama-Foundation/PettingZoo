97ad4cd6ec8e8ec4404a90c631fc249604a54c0b
Rohan138 <rapotdar@purdue.edu>
2021-12-04 14:24:42 -0500

    Fixed capture_stdout

--- pettingzoo/utils/wrappers/capture_stdout.py
+++ pettingzoo/utils/wrappers/capture_stdout.py
@@ -9,16 +9,16 @@ class CaptureStdoutWrapper(BaseWrapper):
 
     def render(self, mode="human"):
         if mode == "human":
-            super().render()
+            super().render(mode)
         elif mode == "ansi":
             with capture_stdout() as stdout:
 
-                super().render()
+                super().render(mode)
 
                 val = stdout.getvalue()
             return val
         elif mode == "rgb_array":
-            return super().render()
+            return super().render(mode)
 
     def __str__(self):
         return str(self.env)
