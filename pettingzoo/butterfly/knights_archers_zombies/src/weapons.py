47d888233e18a09e3c6d9d7f0dd9a252f05ceb1f
Jet <taijunjet@hotmail.com>
2022-01-03 13:26:21 +0000

    add FPS metadata and control to KAZ

--- pettingzoo/butterfly/knights_archers_zombies/src/weapons.py
+++ pettingzoo/butterfly/knights_archers_zombies/src/weapons.py
@@ -8,9 +8,8 @@ from .img import get_image
 BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 ARROW_SPEED = 45
-SPEED_VAL = 3
 ANGLE_RATE = 10
-KNIGHT_SPEED = 5
+SWORD_SPEED = 20
 HEIGHT = 720
 WIDTH = 1280
 i = 1
@@ -18,7 +17,7 @@ i = 1
 
 class Arrow(pygame.sprite.Sprite):
 
-    def __init__(self, archer):
+    def __init__(self, archer, FPS):
         super().__init__()
         self.image = pygame.Surface([6, 6])
         self.image = get_image(os.path.join('img', 'arrow.png'))
@@ -29,6 +28,11 @@ class Arrow(pygame.sprite.Sprite):
         self.fired = True
         self.angle = self.archer.angle
         self.image = pygame.transform.rotate(self.image, self.angle)
+
+        self.arrow_speed = ARROW_SPEED * 15. / FPS
+        if self.arrow_speed % 1. != 0.:
+            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.arrow_speed} for arrow_speed.')
+        self.arrow_speed = int(self.arrow_speed)
 
     def update(self):
         if self.archer.alive:
@@ -50,7 +54,7 @@ class Arrow(pygame.sprite.Sprite):
 
 class Sword(pygame.sprite.Sprite):
 
-    def __init__(self, knight):
+    def __init__(self, knight, FPS):
         super().__init__()
         self.image = pygame.Surface((4, 25), pygame.SRCALPHA)
         # self.image.fill(GRAY)
@@ -60,11 +64,27 @@ class Sword(pygame.sprite.Sprite):
         self.direction = self.knight.direction
         self.org_image = self.image.copy()
         self.angle = self.knight.angle
-        self.pos = self.knight.pos
-        self.speed = 20
-        self.phase = 3
-        self.MIN_PHASE = -3
-        self.MAX_PHASE = 3
+        self.pos = self.knight
+
+        self.speed = SWORD_SPEED * 15. / FPS
+        self.phase = 3 * 15. / FPS
+        self.min_phase = -3 / 15. * FPS
+        self.max_phase = 3 / 15. * FPS
+
+        if self.speed % 1. != 0.:
+            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.speed} for knight_speed.')
+        if self.phase % 1. != 0.:
+            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.phase} for phase.')
+        if self.min_phase % 1. != 0.:
+            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.min_phase} for min_phase.')
+        if self.max_phase % 1. != 0.:
+            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.max_phase} for max_phase.')
+
+        self.speed = int(self.speed)
+        self.phase = int(self.phase)
+        self.min_phase = int(self.min_phase)
+        self.max_phase = int(self.max_phase)
+
         self.active = False
 
     def update(self):
@@ -73,7 +93,7 @@ class Sword(pygame.sprite.Sprite):
             self.active = True
 
         if self.active and self.knight.alive:
-            if self.phase > self.MIN_PHASE:
+            if self.phase > self.min_phase:
                 self.phase -= 1
                 self.knight.attacking = True
 
@@ -82,7 +102,7 @@ class Sword(pygame.sprite.Sprite):
                 self.rect.x += (math.cos(angle) * (self.rect.width / 2)) + (math.cos(angle) * (self.knight.rect.width / 2))
                 self.rect.y -= (math.sin(angle) * (self.rect.height / 2)) + (math.sin(angle) * (self.knight.rect.height / 2))
             else:
-                self.phase = self.MAX_PHASE
+                self.phase = self.max_phase
                 self.active = False
                 self.knight.attacking = False
 
