5b3f5abb53a3d18e75c7ee7f76411ca4d5a29a67
Jet <taijunjet@hotmail.com>
2022-01-05 17:39:52 +0000

    Cleanup and streamline KAZ.

--- pettingzoo/butterfly/knights_archers_zombies/src/players.py
+++ pettingzoo/butterfly/knights_archers_zombies/src/players.py
@@ -4,153 +4,75 @@ import os
 import pygame
 
 from .img import get_image
-from .weapons import Arrow, Sword
+from . import constants as const
 
-WIDTH = 1280
-HEIGHT = 720
-ARCHER_SPEED = 25
-KNIGHT_SPEED = 25
-ARCHER_X, ARCHER_Y = 400, 610
-KNIGHT_X, KNIGHT_Y = 800, 610
-ANGLE_RATE = 10
+class Player(pygame.sprite.Sprite):
 
-
-class Archer(pygame.sprite.Sprite):
-
-    def __init__(self, agent_name, FPS):
+    def __init__(self):
         super().__init__()
-        self.image = get_image(os.path.join('img', 'archer.png'))
-        self.rect = self.image.get_rect(center=(ARCHER_X, ARCHER_Y))
-        self.org_image = self.image.copy()
+        self.agent_name = None
+
+        self.rect = pygame.Rect(0., 0., 0., 0.)
+        self.image = None
+        self.org_image = None
+
         self.angle = 0
         self.pos = pygame.Vector2(self.rect.center)
         self.direction = pygame.Vector2(0, -1)
-        self.attacking = False  # disable movement during attacking
-        self.weapon = Arrow(self, FPS)
+
         self.alive = True
         self.score = 0
-        self.is_archer = True
-        self.is_knight = False
-        self.agent_name = agent_name
 
-        self.knight_speed = KNIGHT_SPEED * 15. / FPS
-        self.angle_rate = ANGLE_RATE * 15. / FPS
-
-        if self.knight_speed % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.knight_speed} for knight_speed.')
-        if self.angle_rate % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.angle_rate} for angle_rate.')
-
-        self.knight_speed = int(self.knight_speed)
-        self.angle_rate = int(self.angle_rate)
-
-    def update(self, action):
-        went_out_of_bounds = False
-
-        if not self.attacking:
-            move_angle = math.radians(self.angle + 90)
-            # Up and Down movement
-            if action == 1 and self.rect.y > 20:
-                self.rect.x += math.cos(move_angle) * self.knight_speed
-                self.rect.y -= math.sin(move_angle) * self.knight_speed
-            elif action == 2 and self.rect.y < HEIGHT - 40:
-                self.rect.x += math.cos(move_angle) * self.knight_speed
-                self.rect.y += math.sin(move_angle) * self.knight_speed
-            # Turn CCW & CW
-            elif action == 3:
-                self.angle += self.angle_rate
-            elif action == 4:
-                self.angle -= self.angle_rate
-            elif action == 5 and self.alive:
-                self.weapon.fired = True
-                # self.attacking = True # gets reset to False in weapon attack
-            elif action == 6:
-                pass
-
-            # Clamp to stay inside the screen
-            if self.rect.y < 0 or self.rect.y > (HEIGHT - 40):
-                went_out_of_bounds = True
-
-            self.rect.x = max(min(self.rect.x, WIDTH - 132), 100)
-            self.rect.y = max(min(self.rect.y, HEIGHT - 40), 0)
-
-        self.direction = pygame.Vector2(0, -1).rotate(-self.angle)
-        self.image = pygame.transform.rotate(self.org_image, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        return went_out_of_bounds
-
-    def offset(self, x_offset, y_offset):
-        self.rect.x += x_offset
-        self.rect.y += y_offset
-
-    def is_done(self):
-        return not self.alive
-
-
-class Knight(pygame.sprite.Sprite):
-    def __init__(self, agent_name, FPS):
-        super().__init__()
-        self.image = get_image(os.path.join('img', 'knight.png'))
-        self.rect = self.image.get_rect(center=(KNIGHT_X, KNIGHT_Y))
-        self.org_image = self.image.copy()
-        self.angle = 0
-        self.pos = pygame.Vector2(self.rect.center)
-        self.direction = pygame.Vector2(1, 0)
-        # self.radius = radius
-        self.attacking = False  # Used to disable movement during attacking
-        self.attack_phase = -5
-        self.weapon = Sword(self, FPS)
-        self.alive = True  # This flag is used to immediately delete the mace object when the knight dies
-        self.action = -1
-        self.score = 0
         self.is_archer = False
-        self.is_knight = True
-        self.agent_name = agent_name
+        self.is_knight = False
 
-        self.knight_speed = KNIGHT_SPEED * 15. / FPS
-        self.angle_rate = ANGLE_RATE * 15. / FPS
+        self.speed = 0
+        self.ang_rate = 0
 
-        if self.knight_speed % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.knight_speed} for knight_speed.')
-        if self.angle_rate % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.angle_rate} for angle_rate.')
+        self.action = 6
+        self.attacking = False
+        self.weapon_timeout = 99
 
-        self.knight_speed = int(self.knight_speed)
-        self.angle_rate = int(self.angle_rate)
 
     def update(self, action):
         self.action = action
         went_out_of_bounds = False
+
         if not self.attacking:
             move_angle = math.radians(self.angle + 90)
             # Up and Down movement
             if action == 1 and self.rect.y > 20:
-                self.rect.x += math.cos(move_angle) * self.knight_speed
-                self.rect.y -= math.sin(move_angle) * self.knight_speed
-            elif action == 2 and self.rect.y < HEIGHT - 40:
-                self.rect.x += math.cos(move_angle) * self.knight_speed
-                self.rect.y += math.sin(move_angle) * self.knight_speed
+                self.rect.x += math.cos(move_angle) * self.speed
+                self.rect.y -= math.sin(move_angle) * self.speed
+            elif action == 2 and self.rect.y < const.SCREEN_HEIGHT - 40:
+                self.rect.x += math.cos(move_angle) * self.speed
+                self.rect.y += math.sin(move_angle) * self.speed
             # Turn CCW & CW
             elif action == 3:
-                self.angle += self.angle_rate
+                self.angle += self.ang_rate
             elif action == 4:
-                self.angle -= self.angle_rate
-            elif action == 5:
-                self.attacking = True  # gets reset to False in weapon attack
+                self.angle -= self.ang_rate
+            # weapon and do nothing
+            elif action == 5 and self.alive:
+                pass
             elif action == 6:
                 pass
 
             # Clamp to stay inside the screen
-            if self.rect.y < 0 or self.rect.y > (HEIGHT - 40):
+            if self.rect.y < 0 or self.rect.y > (const.SCREEN_HEIGHT - 40):
                 went_out_of_bounds = True
 
-            self.rect.x = max(min(self.rect.x, WIDTH - 132), 100)
-            self.rect.y = max(min(self.rect.y, HEIGHT - 40), 0)
+            self.rect.x = max(min(self.rect.x, const.SCREEN_WIDTH - 132), 100)
+            self.rect.y = max(min(self.rect.y, const.SCREEN_HEIGHT - 40), 0)
+
+            # add to weapon timeout when we know we're not attacking
+            self.weapon_timeout += 1
+        else:
+            self.weapon_timeout = 0
 
         self.direction = pygame.Vector2(0, -1).rotate(-self.angle)
         self.image = pygame.transform.rotate(self.org_image, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
-
         return went_out_of_bounds
 
     def offset(self, x_offset, y_offset):
@@ -159,3 +81,30 @@ class Knight(pygame.sprite.Sprite):
 
     def is_done(self):
         return not self.alive
+
+
+class Archer(Player):
+
+    def __init__(self, agent_name):
+        super().__init__()
+        self.agent_name = agent_name
+        self.image = get_image(os.path.join('img', 'archer.png'))
+        self.rect = self.image.get_rect(center=(const.ARCHER_X, const.ARCHER_Y))
+        self.org_image = self.image.copy()
+        self.pos = pygame.Vector2(self.rect.center)
+        self.is_archer = True
+        self.speed = const.ARCHER_SPEED
+        self.ang_rate = const.PLAYER_ANG_RATE
+
+class Knight(Player):
+
+    def __init__(self, agent_name):
+        super().__init__()
+        self.agent_name = agent_name
+        self.image = get_image(os.path.join('img', 'knight.png'))
+        self.rect = self.image.get_rect(center=(const.KNIGHT_X, const.KNIGHT_Y))
+        self.org_image = self.image.copy()
+        self.pos = pygame.Vector2(self.rect.center)
+        self.is_knight = True
+        self.speed = const.KNIGHT_SPEED
+        self.ang_rate = const.PLAYER_ANG_RATE
--- pettingzoo/butterfly/knights_archers_zombies/src/weapons.py
+++ pettingzoo/butterfly/knights_archers_zombies/src/weapons.py
@@ -4,105 +4,71 @@ import os
 import pygame
 
 from .img import get_image
-
-BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
-ARROW_SPEED = 45
-ANGLE_RATE = 10
-SWORD_SPEED = 20
-HEIGHT = 720
-WIDTH = 1280
-i = 1
-
+from . import constants as const
 
 class Arrow(pygame.sprite.Sprite):
 
-    def __init__(self, archer, FPS):
+    def __init__(self, archer):
         super().__init__()
-        self.image = pygame.Surface([6, 6])
-        self.image = get_image(os.path.join('img', 'arrow.png'))
         self.archer = archer
+        self.image = get_image(os.path.join('img', 'arrow.png'))
         self.rect = self.image.get_rect(center=self.archer.pos)
-        self.direction = self.archer.direction
-        self.pos = pygame.Vector2(self.archer.rect.center)
-        self.fired = True
-        self.angle = self.archer.angle
-        self.image = pygame.transform.rotate(self.image, self.angle)
+        self.image = pygame.transform.rotate(self.image, self.archer.angle)
 
-        self.arrow_speed = ARROW_SPEED * 15. / FPS
-        if self.arrow_speed % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.arrow_speed} for arrow_speed.')
-        self.arrow_speed = int(self.arrow_speed)
+        self.pos = pygame.Vector2(self.archer.rect.center)
+        self.fire_direction = self.archer.direction
+
+        # reset the archer timeout when arrow fired
+        archer.weapon_timeout = 0
+
 
     def update(self):
         if self.archer.alive:
-            self.pos += self.direction * ARROW_SPEED
+            self.pos += self.fire_direction * const.ARROW_SPEED
             self.rect.center = self.pos
         else:
             self.rect.x = -100
-            # self.pos = pygame.Vector2(-10, -10)
 
     def is_active(self):
         if not self.archer.alive:
             return False
         if self.rect.x < 0 or self.rect.y < 0:
             return False
-        if self.rect.x > WIDTH or self.rect.y > HEIGHT:
+        if self.rect.x > const.SCREEN_WIDTH or self.rect.y > const.SCREEN_HEIGHT:
             return False
         return True
 
 
 class Sword(pygame.sprite.Sprite):
 
-    def __init__(self, knight, FPS):
+    def __init__(self, knight):
+        # the sword is actually a mace, but we refer to it as sword everywhere
         super().__init__()
-        self.image = pygame.Surface((4, 25), pygame.SRCALPHA)
-        # self.image.fill(GRAY)
-        self.image = get_image(os.path.join('img', 'mace.png'))
         self.knight = knight
+        self.image = get_image(os.path.join('img', 'mace.png'))
         self.rect = self.image.get_rect(center=self.knight.rect.center)
-        self.direction = self.knight.direction
-        self.org_image = self.image.copy()
-        self.angle = self.knight.angle
-        self.pos = self.knight
-
-        self.speed = SWORD_SPEED * 15. / FPS
-        self.phase = 3 * 15. / FPS
-        self.min_phase = -3 / 15. * FPS
-        self.max_phase = 3 / 15. * FPS
-
-        if self.speed % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.speed} for knight_speed.')
-        if self.phase % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.phase} for phase.')
-        if self.min_phase % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.min_phase} for min_phase.')
-        if self.max_phase % 1. != 0.:
-            raise ValueError(f'FPS of {FPS} leads to decimal place value of {self.max_phase} for max_phase.')
-
-        self.speed = int(self.speed)
-        self.phase = int(self.phase)
-        self.min_phase = int(self.min_phase)
-        self.max_phase = int(self.max_phase)
-
         self.active = False
 
+        # phase of the sword, starts at the left most part
+        self.phase = const.MAX_PHASE
+
     def update(self):
-        # Attack
         if self.knight.action == 5:
             self.active = True
 
         if self.active and self.knight.alive:
-            if self.phase > self.min_phase:
+            # phase goes from max to min because
+            # it counts positive from CCW
+            if self.phase > const.MIN_PHASE:
                 self.phase -= 1
                 self.knight.attacking = True
 
-                angle = math.radians(self.knight.angle + 90 + self.speed * self.phase)
+                angle = math.radians(self.knight.angle + 90 + const.SWORD_SPEED * self.phase)
                 self.rect = self.image.get_rect(center=self.knight.rect.center)
                 self.rect.x += (math.cos(angle) * (self.rect.width / 2)) + (math.cos(angle) * (self.knight.rect.width / 2))
                 self.rect.y -= (math.sin(angle) * (self.rect.height / 2)) + (math.sin(angle) * (self.knight.rect.height / 2))
             else:
-                self.phase = self.max_phase
+                self.phase = const.MAX_PHASE
                 self.active = False
                 self.knight.attacking = False
 
