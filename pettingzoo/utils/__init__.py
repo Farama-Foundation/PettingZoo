4850f99dc087d212a9e6885315ad948b187079ba
Jet <taijunjet@hotmail.com>
2022-01-05 20:35:22 +0000

    potayto potahto, this import must be perfecto

--- pettingzoo/utils/__init__.py
+++ pettingzoo/utils/__init__.py
@@ -4,7 +4,6 @@ from .conversions import aec2parallel, parallel2aec
 from .env import AECEnv, ParallelEnv
 from .random_demo import random_demo
 from .save_observation import save_observation
-from .wrappers import (AssertOutOfBoundsWrapper, BaseParallelWraper,
-                       BaseWrapper, CaptureStdoutWrapper,
-                       ClipOutOfBoundsWrapper, OrderEnforcingWrapper,
+from .wrappers import (AssertOutOfBoundsWrapper, BaseParallelWraper, BaseWrapper,
+                       CaptureStdoutWrapper, ClipOutOfBoundsWrapper, OrderEnforcingWrapper,
                        TerminateIllegalWrapper)
