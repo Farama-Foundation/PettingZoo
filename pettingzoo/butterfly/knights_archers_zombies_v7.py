3e7b076bab5a6b84322d171b6510f7a18a9f9e89
Jet <taijunjet@hotmail.com>
2022-01-05 18:04:41 +0000

    wasn't supposed to change that apparently

--- pettingzoo/butterfly/knights_archers_zombies_v7.py
+++ pettingzoo/butterfly/knights_archers_zombies_v7.py
@@ -1,4 +1,2 @@
-from .knights_archers_zombies.knights_archers_zombies import (env,
-                                                              manual_control,
-                                                              parallel_env,
+from .knights_archers_zombies.knights_archers_zombies import (env, manual_control, parallel_env,
                                                               raw_env)
