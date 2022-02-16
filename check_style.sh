a40c20f98cc4466a29e98d6d6fe4b53e1d99b799
Ben Black <weepingwillowben@gmail.com>
2022-02-15 20:34:21 -0700

    disabled codespell

--- check_style.sh
+++ check_style.sh
@@ -1,5 +1,5 @@
 # See setup.cfg for codespell and flake8 settings
 bandit --recursive --skip B101,B301,B303,B311,B403,B404,B603,B607 .
-codespell
+# codespell
 flake8 pettingzoo/ test/
 isort -w 100 -o magent -p pettingzoo -c pettingzoo
