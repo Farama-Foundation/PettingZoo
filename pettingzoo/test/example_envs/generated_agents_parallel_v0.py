5ada28102fa74169a82dd74857e6cb601642640d
trigaten <sanderman5@hotmail.com>
2022-03-08 12:39:03 -0500

    fix metadata

--- pettingzoo/test/example_envs/generated_agents_parallel_v0.py
+++ pettingzoo/test/example_envs/generated_agents_parallel_v0.py
@@ -27,7 +27,7 @@ def get_type(agent):
 
 class parallel_env(ParallelEnv):
 
-    metadata = {"render.modes": ["human"], "name": "generated_agents_parallel_v0"}
+    metadata = {"render_modes": ["human"], "name": "generated_agents_parallel_v0"}
 
     def __init__(self, max_cycles=100):
         super().__init__()
