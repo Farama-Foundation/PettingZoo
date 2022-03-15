3dee1d02f0aac0bcfb5c15fcb5aa3e3314f30e21
Jet <taijunjet@hotmail.com>
2022-03-15 14:19:24 +0000

    revert check style

--- check_style.sh
+++ check_style.sh
@@ -1,6 +1,6 @@
 # See setup.cfg for codespell and flake8 settings
 set -e
 
-# codespell pettingzoo/*/*.py pettingzoo/*/*/*.py
+codespell pettingzoo/*/*.py pettingzoo/*/*/*.py
 flake8 pettingzoo/ test/
 isort -w 100 -o magent -p pettingzoo -c pettingzoo
