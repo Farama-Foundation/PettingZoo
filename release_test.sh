b01b85442c5a99fa9d77347a672b17df3b9d3393
Jet <38184875+jjshoots@users.noreply.github.com>
2022-04-27 14:57:40 +0100

    Update release_test.sh

--- release_test.sh
+++ release_test.sh
@@ -3,7 +3,6 @@
 xvfb-run -as "-screen 0 1024x768x24" pytest ./test/pytest_runner.py
 xvfb-run -as "-screen 0 1024x768x24" pytest ./test/all_parameter_combs.py
 pytest ./test/unwrapped_test.py
-
 pytest ./test/variable_env_test.py
 pytest ./test/doc_examples_test.py
 pytest ./test/specific_env_tests.py
