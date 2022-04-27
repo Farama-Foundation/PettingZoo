e5e4f05efb6c2e0342d610c95566de082956e0ca
Bolun Dai <bd1555@nyu.edu>
2022-04-27 11:10:32 -0400

    updated all_parameter_combs.py

--- release_test.sh
+++ release_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# xvfb-run -s "-screen 0 1024x768x24" pytest ./test/pytest_runner.py
+xvfb-run -s "-screen 0 1024x768x24" -a pytest ./test/pytest_runner.py
 xvfb-run -s "-screen 0 1024x768x24" -a pytest ./test/all_parameter_combs.py
-# pytest ./test/unwrapped_test.py
-# pytest ./test/variable_env_test.py
-# pytest ./test/doc_examples_test.py
-# pytest ./test/specific_env_tests.py
+pytest ./test/unwrapped_test.py
+pytest ./test/variable_env_test.py
+pytest ./test/doc_examples_test.py
+pytest ./test/specific_env_tests.py
 
-# bash ./check_style.sh
-# python3 -m test.print_test
+bash ./check_style.sh
+python3 -m test.print_test
