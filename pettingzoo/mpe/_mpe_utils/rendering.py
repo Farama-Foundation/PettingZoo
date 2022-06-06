8fc43c77131bba5aec0c2fc9079068d818a6b505
Ram Rachum <ram@rachum.com>
2022-06-06 14:48:16 +0300

    Fix exception causes all over the codebase

--- pettingzoo/mpe/_mpe_utils/rendering.py
+++ pettingzoo/mpe/_mpe_utils/rendering.py
@@ -26,10 +26,10 @@ from pyglet.gl import (
 
 try:
     import pyglet
-except ImportError:
+except ImportError as e:
     raise ImportError(
         "HINT: you can install pyglet directly via 'pip install pyglet'. But if you really just want to install all Gym dependencies and not have to think about it, 'pip install -e .[all]' or 'pip install gym[all]' will do it."
-    )
+    ) from e
 
 try:
     from pyglet.gl import (
@@ -52,11 +52,11 @@ try:
         glVertex2f,
         glVertex3f,
     )
-except ImportError:
+except ImportError as e:
     raise ImportError(
         """Error occurred while running `from pyglet.gl import ...`
             HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'. If you're running on a server, you may need a virtual frame buffer; something like this should work: 'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'"""
-    )
+    ) from e
 
 
 if "Apple" in sys.version:
