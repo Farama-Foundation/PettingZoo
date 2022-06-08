65d5d3617da696ffd6658d1f34595d6300b2acf6
RushivArora <rushivarora@gmail.com>
2022-06-07 22:39:15 -0400

    Test Fix Push

--- pettingzoo/atari/entombed_competitive_v3.py
+++ pettingzoo/atari/entombed_competitive_v3.py
@@ -1,17 +1,1 @@
-import os
-
-from .base_atari_env import BaseAtariEnv, base_env_wrapper_fn, parallel_wrapper_fn
-
-
-def raw_env(**kwargs):
-    return BaseAtariEnv(
-        game="entombed",
-        num_players=2,
-        mode_num=2,
-        env_name=os.path.basename(__file__)[:-3],
-        **kwargs
-    )
-
-
-env = base_env_wrapper_fn(raw_env)
-parallel_env = parallel_wrapper_fn(env)
+from .entombed_competitive.entombed_competitive import env, raw_env, parallel_env  # noqa: F401
