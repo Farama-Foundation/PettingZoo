b183bf38f3b985f7282b8b87d3ba1c682f3af62b
snow-fox <taijunjet@hotmail.com>
2022-06-09 13:39:10 +0100

    add core test

--- test/core_test.py
+++ test/core_test.py
@@ -1,13 +1,6 @@
+from pettingzoo.test.api_test import api_test
 from pettingzoo.butterfly import pistonball_v6
 
 def test_core():
-    env = pistonball_v6.env(n_pistons=5)
-    for i in range(10):
-        env.reset()
-        for agent in env.agent_iter():
-            observation, reward, done, info = env.last()
-            if not done:
-                action = env.action_space(agent).sample()
-                env.step(action)
-            else:
-                break
+    _env = pistonball_v6.env()
+    api_test(_env)
