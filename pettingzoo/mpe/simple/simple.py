d294f857385add077d94d499f90a1e02d1be2242
RushivArora <rushivarora@gmail.com>
2022-06-15 23:57:26 -0400

    mpe streamline

---
+++ pettingzoo/mpe/simple/simple.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from pettingzoo.utils.conversions import parallel_wrapper_fn
+
+from .._mpe_utils.core import Agent, Landmark, World
+from .._mpe_utils.simple_env import SimpleEnv, make_env
+from .._mpe_utils.scenario import BaseScenario
+
+
+class raw_env(SimpleEnv):
+    def __init__(self, max_cycles=25, continuous_actions=False):
+        scenario = Scenario()
+        world = scenario.make_world()
+        super().__init__(scenario, world, max_cycles, continuous_actions)
+        self.metadata["name"] = "simple_v2"
+
+
+env = make_env(raw_env)
+parallel_env = parallel_wrapper_fn(env)
+
+
+
+class Scenario(BaseScenario):
+    def make_world(self):
+        world = World()
+        # add agents
+        world.agents = [Agent() for i in range(1)]
+        for i, agent in enumerate(world.agents):
+            agent.name = f"agent_{i}"
+            agent.collide = False
+            agent.silent = True
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(1)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = "landmark %d" % i
+            landmark.collide = False
+            landmark.movable = False
+        return world
+
+    def reset_world(self, world, np_random):
+        # random properties for agents
+        for i, agent in enumerate(world.agents):
+            agent.color = np.array([0.25, 0.25, 0.25])
+        # random properties for landmarks
+        for i, landmark in enumerate(world.landmarks):
+            landmark.color = np.array([0.75, 0.75, 0.75])
+        world.landmarks[0].color = np.array([0.75, 0.25, 0.25])
+        # set random initial states
+        for agent in world.agents:
+            agent.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.c = np.zeros(world.dim_c)
+        for i, landmark in enumerate(world.landmarks):
+            landmark.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def reward(self, agent, world):
+        dist2 = np.sum(np.square(agent.state.p_pos - world.landmarks[0].state.p_pos))
+        return -dist2
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        entity_pos = []
+        for entity in world.landmarks:
+            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        return np.concatenate([agent.state.p_vel] + entity_pos)
