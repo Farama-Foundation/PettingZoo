a37b6710c2393429c53f36772645f9e18560c98d
RushivArora <rushivarora@gmail.com>
2022-06-21 01:11:43 -0400

    Fix Precommit

--- pettingzoo/atari/combat_plane/combat_plane.py
+++ pettingzoo/atari/combat_plane/combat_plane.py
@@ -1,7 +1,7 @@
 import os
+from glob import glob
 
 from ..base_atari_env import BaseAtariEnv, base_env_wrapper_fn, parallel_wrapper_fn
-from glob import glob
 
 avaliable_versions = {
     "bi-plane": 15,
@@ -15,15 +15,11 @@ def raw_env(game_version="bi-plane", guided_missile=True, **kwargs):
     ), "game_version must be either 'jet' or 'bi-plane'"
     mode = avaliable_versions[game_version] + (0 if guided_missile else 1)
     name = os.path.basename(__file__).split(".")[0]
-    parent_file = glob('./pettingzoo/atari/' + name + '*.py')
-    version_num = parent_file[0].split('_')[-1].split('.')[0]
+    parent_file = glob("./pettingzoo/atari/" + name + "*.py")
+    version_num = parent_file[0].split("_")[-1].split(".")[0]
     name = name + "_" + version_num
     return BaseAtariEnv(
-        game="combat",
-        num_players=2,
-        mode_num=mode,
-        env_name=name,
-        **kwargs
+        game="combat", num_players=2, mode_num=mode, env_name=name, **kwargs
     )
 
 
