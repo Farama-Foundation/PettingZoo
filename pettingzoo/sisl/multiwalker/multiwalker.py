31ca1fae8650ee44716a1b8fa5a69adaf43ebafa
Jet <taijunjet@hotmail.com>
2022-08-18 15:15:15 +0100

    remove truncations from was_done_step

--- pettingzoo/sisl/multiwalker/multiwalker.py
+++ pettingzoo/sisl/multiwalker/multiwalker.py
@@ -77,10 +77,7 @@ class raw_env(AECEnv, EzPickle):
         return self.env.observe(self.agent_name_mapping[agent])
 
     def step(self, action):
-        if (
-            self.terminations[self.agent_selection]
-            or self.truncations[self.agent_selection]
-        ):
+        if self.terminations[self.agent_selection]:
             self._was_dead_step(action)
             return
         agent = self.agent_selection
