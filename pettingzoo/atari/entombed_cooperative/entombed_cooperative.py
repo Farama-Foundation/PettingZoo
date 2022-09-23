f62038096129cdd3059f78af9f30dc0d1b2ed644
Manuel Goulão <msilvagoulao@gmail.com>
2022-09-23 23:30:43 +0100

    Docs automation (#790)
    Co-authored-by: Jet <38184875+jjshoots@users.noreply.github.com>
    Co-authored-by: will <will2346@live.co.uk>
    Co-authored-by: Will Dudley <14932240+WillDudley@users.noreply.github.com>
    Co-authored-by: FilipinoGambino <ngorichs1@gmail.com>
    Co-authored-by: Reginald McLean <reginald.mclean@ryerson.ca>
    Co-authored-by: Jordan Terry <jkterry0@gmail.com>
    Co-authored-by: Mark Towers <mark.m.towers@gmail.com>
    Co-authored-by: Andrea PIERRÉ <andrea_pierre@brown.edu>

--- pettingzoo/atari/entombed_cooperative/entombed_cooperative.py
+++ pettingzoo/atari/entombed_cooperative/entombed_cooperative.py
@@ -1,3 +1,89 @@
+# noqa
+"""
+# Emtombed: Cooperative
+
+```{figure} atari_entombed_cooperative.gif
+:width: 140px
+:name: entombed_cooperative
+```
+
+This environment is part of the <a href='..'>Atari environments</a>. Please read that page first for general information.
+
+| Import               | `from pettingzoo.atari import entombed_cooperative_v3` |
+|----------------------|--------------------------------------------------------|
+| Actions              | Discrete                                               |
+| Parallel API         | Yes                                                    |
+| Manual Control       | No                                                     |
+| Agents               | `agents= ['first_0', 'second_0']`                      |
+| Agents               | 2                                                      |
+| Action Shape         | (1,)                                                   |
+| Action Values        | [0,17]                                                 |
+| Observation Shape    | (210, 160, 3)                                          |
+| Observation Values   | (0,255)                                                |
+| Average Total Reward | 6.23                                                   |
+
+```{figure} ../../_static/img/aec/atari_entombed_cooperative_aec.svg
+:width: 200px
+:name: entombed_cooperative
+```
+
+Entombed's cooperative version is an exploration game
+where you need to work with your teammate to make it
+as far as possible into the maze.
+
+You both need to quickly navigate down a constantly generating
+maze you can only see part of. If you get stuck, you lose.
+Note you can easily find yourself in a dead-end escapable only through the use of rare power-ups.
+If players help each other by the use of these powerups, they can last longer. Note that optimal coordination requires that the agents be on opposite sides of the map, because powerups appear on one side or the other, but can be used to break through walls on both sides
+(the break is symmetric and effects both halves of the screen).
+In addition, there dangerous zombies lurking around to avoid.
+
+The reward was designed to be identical to the single player rewards. In particular, an entombed stage is divided into 5 invisible sections. You receive reward immediately after changing sections, or after resetting the stage. Note that this means that you receive a reward when you lose a life,
+because it resets the stage, but not when you lose your last life, because the game terminates without the stage resetting.
+
+
+[Official Entombed manual](https://atariage.com/manual_html_page.php?SoftwareLabelID=165)
+
+
+#### Environment parameters
+
+Environment parameters are common to all Atari environments and are described in the [base Atari documentation](../atari) .
+
+### Action Space
+
+In any given turn, an agent can choose from one of 18 actions.
+
+| Action    | Behavior  |
+|:---------:|-----------|
+| 0         | No operation |
+| 1         | Fire |
+| 2         | Move up |
+| 3         | Move right |
+| 4         | Move left |
+| 5         | Move down |
+| 6         | Move upright |
+| 7         | Move upleft |
+| 8         | Move downright |
+| 9         | Move downleft |
+| 10        | Fire up |
+| 11        | Fire right |
+| 12        | Fire left |
+| 13        | Fire down |
+| 14        | Fire upright |
+| 15        | Fire upleft |
+| 16        | Fire downright |
+| 17        | Fire downleft |
+
+### Version History
+
+* v3: Minimal Action Space (1.18.0)
+* v2: Breaking changes to entire API, fixed Entombed rewards (1.4.0)
+* v1: Fixes to how all environments handle premature death (1.3.0)
+* v0: Initial versions release (1.0.0)
+
+
+"""
+
 import os
 from glob import glob
 
--- pettingzoo/mpe/simple_speaker_listener/simple_speaker_listener.py
+++ pettingzoo/mpe/simple_speaker_listener/simple_speaker_listener.py
@@ -1,3 +1,58 @@
+# noqa
+"""
+# Simple Speaker Listener
+
+```{figure} mpe_simple_speaker_listener.gif
+:width: 140px
+:name: simple_speaker_listener
+```
+
+This environment is part of the <a href='..'>MPE environments</a>. Please read that page first for general information.
+
+| Import               | `from pettingzoo.mpe import simple_speaker_listener_v3` |
+|----------------------|---------------------------------------------------------|
+| Actions              | Discrete/Continuous                                     |
+| Parallel API         | Yes                                                     |
+| Manual Control       | No                                                      |
+| Agents               | `agents=[speaker_0, listener_0]`                        |
+| Agents               | 2                                                       |
+| Action Shape         | (3),(5)                                                 |
+| Action Values        | Discrete(3),(5)/Box(0.0, 1.0, (3)), Box(0.0, 1.0, (5))  |
+| Observation Shape    | (3),(11)                                                |
+| Observation Values   | (-inf,inf)                                              |
+| State Shape          | (14,)                                                   |
+| State Values         | (-inf,inf)                                              |
+| Average Total Reward | -80.9                                                   |
+
+```{figure} ../../_static/img/aec/mpe_simple_speaker_listener_aec.svg
+:width: 200px
+:name: simple_speaker_listener
+```
+
+This environment is similar to simple_reference, except that one agent is the 'speaker' (gray) and can speak but cannot move, while the other agent is the listener (cannot speak, but must navigate to correct landmark).
+
+Speaker observation space: `[goal_id]`
+
+Listener observation space: `[self_vel, all_landmark_rel_positions, communication]`
+
+Speaker action space: `[say_0, say_1, say_2, say_3, say_4, say_5, say_6, say_7, say_8, say_9]`
+
+Listener action space: `[no_action, move_left, move_right, move_down, move_up]`
+
+### Arguments
+
+``` python
+simple_speaker_listener_v2.env(max_cycles=25, continuous_actions=False)
+```
+
+
+
+`max_cycles`:  number of frames (a step for each agent) until game terminates
+
+`continuous_actions`: Whether agent action spaces are discrete(default) or continuous
+
+"""
+
 import numpy as np
 from gym.utils import EzPickle
 
