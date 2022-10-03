b6bb1c2eaa46866b27d7a3a266a013ec0ea422e3
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2022-10-03 07:27:43 -0400

    deleted trailing white space

--- pettingzoo/sisl/waterworld/waterworld.py
+++ pettingzoo/sisl/waterworld/waterworld.py
@@ -133,7 +133,7 @@ thrust_penalty=-0.5, local_ratio=1.0, speed_features=True, max_cycles=500)
 
 `max_cycles`: After max_cycles steps all agents will return done
 
-* v4: Major refactor (2.0.0) 
+* v4: Major refactor (2.0.0)
 * v3: Refactor and major bug fixes (1.5.0)
 * v2: Misc bug fixes (1.4.0)
 * v1: Various fixes and environment argument changes (1.3.1)
