dfe5779fde75e1adca57cb1e554688bff90c4828
WillDudley <Will2346@live.co.uk>
2022-10-04 16:17:14 +0100

    change "dones"

--- docs/code_examples/aec_rps.py
+++ docs/code_examples/aec_rps.py
@@ -131,7 +131,6 @@ class raw_env(AECEnv):
         - _cumulative_rewards
         - terminations
         - truncations
-        - dones
         - infos
         - agent_selection
         And must set up the environment so that render(), step(), and observe()
