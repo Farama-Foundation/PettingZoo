39084b1a10b5e886505e9f21aa641bca6d773c66
snow-fox <taijunjet@hotmail.com>
2023-01-04 00:03:20 +0000

    die liste

--- pettingzoo/utils/average_total_reward.py
+++ pettingzoo/utils/average_total_reward.py
@@ -27,7 +27,7 @@ def average_total_reward(env, max_episodes=100, max_steps=10000000000):
             if termination or truncation:
                 action = None
             elif isinstance(obs, dict) and "action_mask" in obs:
-                action = random.choice(np.flatnonzero(obs["action_mask"]))
+                action = random.choice(np.flatnonzero(obs["action_mask"]).tolist())
             else:
                 action = env.action_space(agent).sample()
             env.step(action)
