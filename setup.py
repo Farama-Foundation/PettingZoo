754f5d8bcfd868182e4108c6321f027ec88f0a4e
snow-fox <taijunjet@hotmail.com>
2023-01-06 16:13:18 +0000

    add back long description and fix macos(?)

--- setup.py
+++ setup.py
@@ -18,4 +18,19 @@ def get_version():
     raise RuntimeError("bad version data in __init__.py")
 
 
-setup(name="pettingzoo", version=get_version())
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
+
+
+setup(name="pettingzoo", version=get_version(), long_description=get_description())
