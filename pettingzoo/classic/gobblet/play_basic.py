65cce7a8e4e57e0bda831437f75ff36688eeb5f2
elliottower <etower@umass.edu>
2023-01-27 01:42:48 -0500

    Added command line argument for render_mode for play_basic.py (allows to view either the full 3-d board or flat board only showing top pieces)

--- pettingzoo/classic/gobblet/play_basic.py
+++ pettingzoo/classic/gobblet/play_basic.py
@@ -1,13 +1,29 @@
 from pettingzoo.classic import gobblet_v1
+import argparse
 
-env = gobblet_v1.env(render_mode="human_full")
-env.reset()
-for agent in env.agent_iter():
-    observation, reward, termination, truncation, info = env.last()
-    action = None if termination or truncation else env.action_space(agent).sample()  # this is where you would insert your policy
-    if not termination and not truncation:
-        pos = action % 9; piece = (action // 9) + 1
-        print(f"AGENT: {agent}, ACTION: {action}, POSITION: {pos}, PIECE: {piece}")
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--render_mode", type=str, default="human", help="render modes: human, human_full"
+    )
+    return parser
 
-    env.step(action)
+def get_args() -> argparse.Namespace:
+    parser = get_parser()
+    return parser.parse_known_args()[0]
+
+if __name__ == "__main__":
+    # train the agent and watch its performance in a match!
+    args = get_args()
+
+    env = gobblet_v1.env(render_mode=args.render_mode)
+    env.reset()
+    for agent in env.agent_iter():
+        observation, reward, termination, truncation, info = env.last()
+        action = None if termination or truncation else env.action_space(agent).sample()  # this is where you would insert your policy
+        if not termination and not truncation:
+            pos = action % 9; piece = (action // 9) + 1
+            print(f"AGENT: {agent}, ACTION: {action}, POSITION: {pos}, PIECE: {piece}")
+
+        env.step(action)
 
