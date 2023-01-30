35f71b129d88196cd9ce03244d98f2ff3e628951
Elliot Tower <etower@umass.edu>
2023-01-29 22:50:00 -0500

    Added previews for moves, ability for human to play as both agents, or to choose which agent to play as

--- tutorials/Pygame/connect_four_interactive.py
+++ tutorials/Pygame/connect_four_interactive.py
@@ -2,19 +2,26 @@ from pettingzoo.classic import connect_four_v3
 import argparse
 import numpy as np
 import sys
+import time
 import pygame
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--render_mode", type=str, default="human", help="options: human, rgb_array"
+        "--render-mode", type=str, default="human", help="options: human, rgb_array"
     )
     parser.add_argument(
-        "--agent_type", type=str, default="random", help="options: random, random_admissable"
+        "--agent-type", type=str, default="random_admissible", help="options: random, random_admissible"
     )
     parser.add_argument(
         "--seed", type=int, default=None, help="random seed for board and policy"
+    )
+    parser.add_argument(
+        "--no-cpu", action="store_true", help="disable CPU players and play as both teams"
+    )
+    parser.add_argument(
+        "--player", type=int, default=0, help="which player to play as"
     )
 
     return parser
@@ -24,7 +31,6 @@ def get_args() -> argparse.Namespace:
     return parser.parse_known_args()[0]
 
 if __name__ == "__main__":
-    # train the agent and watch its performance in a match!
     args = get_args()
 
     env = connect_four_v3.env(render_mode=args.render_mode)
@@ -34,6 +40,7 @@ if __name__ == "__main__":
     else:
         env.reset()
     turn = 0
+    env.render()  # need to render the environment before pygame can take user input
     for agent in env.agent_iter():
         observation, reward, termination, truncation, info = env.last()
         if termination:
@@ -44,11 +51,12 @@ if __name__ == "__main__":
         else:
             if args.agent_type == "random":
                 action = env.action_space(agent).sample()
+                time.sleep(.25)
             if args.agent_type == "random_admissable":
                 action_mask = observation['action_mask']
                 action = np.random.choice(np.arange(len(action_mask)), p=action_mask / np.sum(action_mask))
-            if agent == env.agents[1]:
-                pygame.event.clear()
+                time.sleep(.25)
+            if agent == env.agents[args.player] or args.no_cpu:
                 while True:
                     event = pygame.event.wait()
                     if event.type == pygame.QUIT:
@@ -70,7 +78,10 @@ if __name__ == "__main__":
                         action = 5
                     elif 1070 <= mousex < 1240:
                         action = 6
-
+                    env.unwrapped.preview[agent] = action
+                    env.render()
+                    pygame.display.update()
                     if event.type == pygame.MOUSEBUTTONDOWN:
+                        env.unwrapped.preview[agent] = -1
                         break
             env.step(action)
