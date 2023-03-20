8a31f3715c4afd74b3cf7cf65f8a9a576aa8c182
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2023-03-20 13:32:20 +0100

    Fix typo: `BaseParallelWraper` renamed to `BaseParallelWrapper` (#876)

--- pettingzoo/utils/wrappers/__init__.py
+++ pettingzoo/utils/wrappers/__init__.py
@@ -1,6 +1,6 @@
 from .assert_out_of_bounds import AssertOutOfBoundsWrapper
 from .base import BaseWrapper
-from .base_parallel import BaseParallelWraper
+from .base_parallel import BaseParallelWrapper
 from .capture_stdout import CaptureStdoutWrapper
 from .clip_out_of_bounds import ClipOutOfBoundsWrapper
 from .order_enforcing import OrderEnforcingWrapper
