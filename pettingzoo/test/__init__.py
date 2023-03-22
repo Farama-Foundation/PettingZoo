a8312067b1e581e00df54d9342da8098b780c0b5
Elliot Tower <32176771+elliottower@users.noreply.github.com>
2023-03-22 15:38:06 -0400

    Add parallel_seed_test import to test init (#913)

--- pettingzoo/test/__init__.py
+++ pettingzoo/test/__init__.py
@@ -6,5 +6,5 @@ from .parallel_test import parallel_api_test
 from .performance_benchmark import performance_benchmark
 from .render_test import collect_render_results, render_test
 from .save_obs_test import test_save_obs
-from .seed_test import seed_test
+from .seed_test import parallel_seed_test, seed_test
 from .state_test import state_test
