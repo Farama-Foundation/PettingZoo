a51f093b151e49bd3df973b5857377644d76aa73
Oren <zbenmo@gmail.com>
2023-04-26 17:23:46 +0200

    added a wrapper for parallel env

--- test/pz_to_gymnasium_test.py
+++ test/pz_to_gymnasium_test.py
@@ -1,6 +1,7 @@
 import pytest
-from pettingzoo.utils.wrappers.aec_to_gymnasium import Aec2GymnasiumEnv
+from pettingzoo.utils.wrappers.pz_to_gymnasium import aec_to_gymnasium, parallel_to_gymnasium
 from pettingzoo.classic import tictactoe_v3
+from pettingzoo.butterfly import pistonball_v6
 from gymnasium.utils.env_checker import check_env
 import numpy as np
 
@@ -26,10 +27,41 @@ def test_tictactoe(me, other):
         other: pick_a_free_square
     }
 
-    gym_env = Aec2GymnasiumEnv(
+    gym_env = aec_to_gymnasium(
         aec_env=aec_env,
         external_agent=me,
         act_others=lambda agent, observation: other_agents_logic[agent](observation)
+    )
+
+    check_env(gym_env)
+
+    observation, info = gym_env.reset(seed=42)
+    for _ in range(10):
+        action = gym_env.action_space.sample()  # this is where you would insert your policy
+        observation, reward, terminated, truncated, info = gym_env.step(action)
+
+        if terminated or truncated:
+            observation, info = gym_env.reset()
+    gym_env.close()
+
+
+@pytest.mark.parametrize(
+    "me",
+    [
+        "piston_0",
+        "piston_1",
+    ]
+)
+def test_pistonball(me):
+    parallel_env = pistonball_v6.parallel_env()
+    
+    def pick_a_random_action(obs):
+        return np.random.uniform(-1.0, 1, size=(1,))
+
+    gym_env = parallel_to_gymnasium(
+        parallel_env=parallel_env,
+        external_agent=me,
+        act_others=lambda agent, observation: pick_a_random_action(observation)
     )
 
     check_env(gym_env)
