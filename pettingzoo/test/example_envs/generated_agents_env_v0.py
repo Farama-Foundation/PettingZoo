b04fc284f12cfb9c7a1a9cb28fc58bbdb7799051
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-05-11 05:50:06 +0200

    Update API to fully match Gymnasium (#930)
    Co-authored-by: Jet <38184875+jjshoots@users.noreply.github.com>
    Co-authored-by: Elliot Tower <32176771+elliottower@users.noreply.github.com>
    Co-authored-by: Lorenzo Cavuoti <46611988+LetteraUnica@users.noreply.github.com>
    Co-authored-by: Manuel Goulão <msilvagoulao@gmail.com>
    Co-authored-by: Jordan Terry <jkterry0@gmail.com>
    Co-authored-by: Elliot Tower <etower@umass.edu>

--- pettingzoo/test/example_envs/generated_agents_env_v0.py
+++ pettingzoo/test/example_envs/generated_agents_env_v0.py
@@ -26,7 +26,7 @@ class raw_env(AECEnv):
         self.types = []
         self._agent_counters = {}
         self.max_cycles = max_cycles
-        self.seed()
+        self._seed()
         self.render_mode = render_mode
         for i in range(3):
             self.add_type()
@@ -67,7 +67,7 @@ class raw_env(AECEnv):
 
     def reset(self, seed=None, options=None):
         if seed is not None:
-            self.seed(seed=seed)
+            self._seed(seed=seed)
         self.agents = []
         self.rewards = {}
         self._cumulative_rewards = {}
@@ -94,7 +94,7 @@ class raw_env(AECEnv):
         for i, agent in enumerate(self.agents):
             self.action_space(agent).seed(seed)
 
-    def seed(self, seed=None):
+    def _seed(self, seed=None):
         self.np_random, _ = gymnasium.utils.seeding.np_random(seed)
 
     def step(self, action):
