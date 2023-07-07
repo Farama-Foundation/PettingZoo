cf2bef7e55de60b35657b109a03c2679ea79506a
elliottower <elliot@elliottower.com>
2023-07-07 15:51:58 -0400

    Update SB3 tutorial to have __main__ (error on macOS)

--- tutorials/SB3/sb3_pistonball.py
+++ tutorials/SB3/sb3_pistonball.py
@@ -12,45 +12,45 @@ from stable_baselines3.ppo import CnnPolicy
 
 from pettingzoo.butterfly import pistonball_v6
 
-env = pistonball_v6.parallel_env(
-    n_pistons=20,
-    time_penalty=-0.1,
-    continuous=True,
-    random_drop=True,
-    random_rotate=True,
-    ball_mass=0.75,
-    ball_friction=0.3,
-    ball_elasticity=1.5,
-    max_cycles=125,
-)
+if __name__ == "__main__":
+    env = pistonball_v6.parallel_env(
+        n_pistons=20,
+        time_penalty=-0.1,
+        continuous=True,
+        random_drop=True,
+        random_rotate=True,
+        ball_mass=0.75,
+        ball_friction=0.3,
+        ball_elasticity=1.5,
+        max_cycles=125,
+    )
 
-env = ss.color_reduction_v0(env, mode="B")
-env = ss.resize_v1(env, x_size=84, y_size=84)
-env = ss.frame_stack_v1(env, 3)
+    env = ss.color_reduction_v0(env, mode="B")
+    env = ss.resize_v1(env, x_size=84, y_size=84)
+    env = ss.frame_stack_v1(env, 3)
 
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 8, num_cpus=4, base_class="stable_baselines3")
 
-env = ss.pettingzoo_env_to_vec_env_v1(env)
-env = ss.concat_vec_envs_v1(env, 8, num_cpus=4, base_class="stable_baselines3")
+    model = PPO(
+        CnnPolicy,
+        env,
+        verbose=3,
+        gamma=0.95,
+        n_steps=256,
+        ent_coef=0.0905168,
+        learning_rate=0.00062211,
+        vf_coef=0.042202,
+        max_grad_norm=0.9,
+        gae_lambda=0.99,
+        n_epochs=5,
+        clip_range=0.3,
+        batch_size=256,
+    )
 
-model = PPO(
-    CnnPolicy,
-    env,
-    verbose=3,
-    gamma=0.95,
-    n_steps=256,
-    ent_coef=0.0905168,
-    learning_rate=0.00062211,
-    vf_coef=0.042202,
-    max_grad_norm=0.9,
-    gae_lambda=0.99,
-    n_epochs=5,
-    clip_range=0.3,
-    batch_size=256,
-)
+    model.learn(total_timesteps=2_000_000)
 
-model.learn(total_timesteps=2_000_000)
+    model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
 
-model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
-
-
-print("Model has been saved.")
+    print("Model has been saved.")
+    env.close()
--- tutorials/SB3/sb3_rps.py
+++ tutorials/SB3/sb3_rps.py
@@ -13,31 +13,36 @@ from stable_baselines3.ppo import MlpPolicy
 from pettingzoo.classic import rps_v2
 from pettingzoo.utils import turn_based_aec_to_parallel
 
-env = rps_v2.env()
-env = turn_based_aec_to_parallel(env)
+if __name__ == "__main__":
+    env = rps_v2.env()
+    env = turn_based_aec_to_parallel(env)
 
-env = ss.pettingzoo_env_to_vec_env_v1(env)
-env = ss.concat_vec_envs_v1(env, 8, num_cpus=4, base_class="stable_baselines3")
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 8, num_cpus=4, base_class="stable_baselines3")
 
-# TODO: find hyperparameters that make the model actually learn
-model = PPO(
-    MlpPolicy,
-    env,
-    verbose=3,
-    gamma=0.95,
-    n_steps=256,
-    ent_coef=0.0905168,
-    learning_rate=0.00062211,
-    vf_coef=0.042202,
-    max_grad_norm=0.9,
-    gae_lambda=0.99,
-    n_epochs=5,
-    clip_range=0.3,
-    batch_size=256,
-)
+    # TODO: find hyperparameters that make the model actually learn
+    model = PPO(
+        MlpPolicy,
+        env,
+        verbose=3,
+        gamma=0.95,
+        n_steps=256,
+        ent_coef=0.0905168,
+        learning_rate=0.00062211,
+        vf_coef=0.042202,
+        max_grad_norm=0.9,
+        gae_lambda=0.99,
+        n_epochs=5,
+        clip_range=0.3,
+        batch_size=256,
+    )
 
-model.learn(total_timesteps=2_000_000)
+    model.learn(total_timesteps=10_000)
 
-model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
+    model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
 
-print("Model has been saved.")
+    print("Model has been saved.")
+    env.close()
+    import sys
+
+    sys.exit(0)
