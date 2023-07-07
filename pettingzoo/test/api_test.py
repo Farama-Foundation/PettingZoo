6208ff3a4b185cc9030dd4e7aa893e343283dbd8
elliottower <elliot@elliottower.com>
2023-07-07 03:55:09 -0400

    Remove commented out test which fails (can't dynamically skip if pytest is installed)

--- pettingzoo/test/api_test.py
+++ pettingzoo/test/api_test.py
@@ -128,7 +128,6 @@ env_neg_inf_obs = [
 ]
 
 
-@pytest.mark.skip(reason="fixtures not found even though they are defined")
 def test_observation(observation, observation_0, env_name=None):
     if not isinstance(observation, np.ndarray) or (
         env_name is not None and env_name not in env_obs_dicts
