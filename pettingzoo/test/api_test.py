d175506e3e6658bfd4f94736c0db593b8b2e97e9
elliottower <elliot@elliottower.com>
2023-08-17 12:10:55 -0400

    Fix API test warning for obs not being numpy array for every custom env

--- pettingzoo/test/api_test.py
+++ pettingzoo/test/api_test.py
@@ -125,7 +125,7 @@ env_neg_inf_obs = [
 
 
 def test_observation(observation, observation_0, env_name=None):
-    if not isinstance(observation, np.ndarray) or (
+    if not isinstance(observation, np.ndarray) and (
         env_name is not None and env_name not in env_obs_dicts
     ):
         warnings.warn("Observation is not NumPy array")
