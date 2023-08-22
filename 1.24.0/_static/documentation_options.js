3510a0753b2d92f777d3c8bccc985a2c4a5f02da
elliottower <etower@farama.org>
2023-08-22 16:19:45 +0000

    Deploying to gh-pages from @ Farama-Foundation/PettingZoo@ab6e5432e28ca8e5c2cc58089f2bfa7489b7d467 🚀

--- 1.24.0/_static/documentation_options.js
+++ 1.24.0/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.23.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.24.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
