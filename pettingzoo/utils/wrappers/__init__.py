e44817026d9a2f2d760be996fcb14cf4a4a3d5c3
BertrandDecoster <70576987+BertrandDecoster@users.noreply.github.com>
2023-08-31 20:15:20 +0200

    Update __init__.py to include MonitorWrapper

--- pettingzoo/utils/wrappers/__init__.py
+++ pettingzoo/utils/wrappers/__init__.py
@@ -5,3 +5,4 @@ from pettingzoo.utils.wrappers.capture_stdout import CaptureStdoutWrapper
 from pettingzoo.utils.wrappers.clip_out_of_bounds import ClipOutOfBoundsWrapper
 from pettingzoo.utils.wrappers.order_enforcing import OrderEnforcingWrapper
 from pettingzoo.utils.wrappers.terminate_illegal import TerminateIllegalWrapper
+from pettingzoo.utils.wrappers.monitor import MonitorWrapper
