105383ad61f002abe9597220aca032aa48382d90
elliottower <elliot@elliottower.com>
2023-09-01 15:33:35 -0400

    pre-commit, add check for GitHub workflows (validate them locally before running on CI)

--- tutorials/CustomEnvironment/tutorial4_testing_the_environment.py
+++ tutorials/CustomEnvironment/tutorial4_testing_the_environment.py
@@ -1,5 +1,5 @@
-from tutorial3_action_masking import CustomActionMaskedEnvironment
 from tutorial2_adding_game_logic import CustomEnvironment
+from tutorial3_action_masking import CustomActionMaskedEnvironment
 
 from pettingzoo.test import parallel_api_test
 
