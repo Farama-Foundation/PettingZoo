f109783fa212bdcb9d4d4fd993ebf57dea589548
elliottower <elliot@elliottower.com>
2023-09-02 13:45:17 -0400

    Add imports for all modules, allow testing of in-line argument examples from env docs (don't include import)

--- conftest.py
+++ conftest.py
@@ -4,10 +4,17 @@ def pytest_markdown_docs_globals():
     import supersuit
 
     import pettingzoo
+    from pettingzoo.utils.all_modules import all_environments
 
-    return {
+    libs = {
         "math": pettingzoo,
         "gymnasium": gymnasium,
         "shimmy": shimmy,
         "supersuit": supersuit,
     }
+    # Format: {"go_v5": <module ' pettingzoo.classic.go_v5'>, ...}
+    envs = {
+        name.split("/")[1]: module for name, module in all_environments.items()
+    }
+    libs.update(envs)
+    return libs.update(envs)
