019ea464c4b52961793b3af3a915739b9e0fdcfe
Elliot Tower <elliot@elliottower.com>
2023-10-13 13:28:46 -0400

    Update Ray tutorials to RLlib 2.7.0 (#1112)

--- tutorials/Ray/render_rllib_pistonball.py
+++ tutorials/Ray/render_rllib_pistonball.py
@@ -12,10 +12,39 @@ from PIL import Image
 from ray.rllib.algorithms.ppo import PPO
 from ray.rllib.env.wrappers.pettingzoo_env import PettingZooEnv
 from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.tune.registry import register_env
-from tutorials.Ray.rllib_pistonball import CNNModelV2
+from torch import nn
 
 from pettingzoo.butterfly import pistonball_v6
+
+
+class CNNModelV2(TorchModelV2, nn.Module):
+    def __init__(self, obs_space, act_space, num_outputs, *args, **kwargs):
+        TorchModelV2.__init__(self, obs_space, act_space, num_outputs, *args, **kwargs)
+        nn.Module.__init__(self)
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, [8, 8], stride=(4, 4)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, [4, 4], stride=(2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, [3, 3], stride=(1, 1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            (nn.Linear(3136, 512)),
+            nn.ReLU(),
+        )
+        self.policy_fn = nn.Linear(512, num_outputs)
+        self.value_fn = nn.Linear(512, 1)
+
+    def forward(self, input_dict, state, seq_lens):
+        model_out = self.model(input_dict["obs"].permute(0, 3, 1, 2))
+        self._value_out = self.value_fn(model_out)
+        return self.policy_fn(model_out), state
+
+    def value_function(self):
+        return self._value_out.flatten()
+
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -28,6 +57,10 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+
+if args.checkpoint_path is None:
+    print("The following arguments are required: --checkpoint-path")
+    exit(0)
 
 checkpoint_path = os.path.expanduser(args.checkpoint_path)
 
