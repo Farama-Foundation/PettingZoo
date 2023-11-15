Fix seed test to work with action_mask in info, add tests to ensure info action masking works (#1134)
Co-authored-by: Kenny <ckennyc@tamu.edu>

pettingzoo/test/example_envs/generated_agents_env_action_mask_info_v0.py
@@ -10,6 +10,7 @@ from pettingzoo.utils.agent_selector import agent_selector
 
 def env():
     env = raw_env()
+    env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
     env = wrappers.AssertOutOfBoundsWrapper(env)
     env = wrappers.OrderEnforcingWrapper(env)
     return env
@@ -73,7 +74,12 @@ class raw_env(AECEnv[str, np.ndarray, Union[int, None]]):
         self.truncations[agent] = False
         self.rewards[agent] = 0
         self._cumulative_rewards[agent] = 0
-        self.infos[agent] = {}
+        num_actions = self._act_spaces[type].n
+        self.infos[agent] = {
+            "action_mask": np.eye(num_actions)[
+                self.np_random.choice(num_actions)
+            ].astype(np.int8)
+        }
         return agent
 
     def reset(self, seed=None, options=None):
@@ -146,6 +152,15 @@ class raw_env(AECEnv[str, np.ndarray, Union[int, None]]):
 
         self._accumulate_rewards()
         self._deads_step_first()
+
+        # Sample info action mask randomly
+        type = self.agent_selection.split("_")[0]
+        num_actions = self._act_spaces[type].n
+        self.infos[self.agent_selection] = {
+            "action_mask": np.eye(num_actions)[
+                self.np_random.choice(num_actions)
+            ].astype(np.int8)
+        }
 
         # Cycle agents
         self.agent_selection = self._agent_selector.next()

pettingzoo/test/example_envs/generated_agents_env_action_mask_obs_v0.py
@@ -10,6 +10,7 @@ from pettingzoo.utils.agent_selector import agent_selector
 
 def env():
     env = raw_env()
+    env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
     env = wrappers.AssertOutOfBoundsWrapper(env)
     env = wrappers.OrderEnforcingWrapper(env)
     return env
@@ -55,7 +56,14 @@ class raw_env(AECEnv[str, np.ndarray, Union[int, None]]):
         type_id = len(self.types)
         num_actions = self.np_random.integers(3, 10)
         obs_size = self.np_random.integers(10, 50)
-        obs_space = gymnasium.spaces.Box(low=0, high=1, shape=(obs_size,))
+        obs_space = gymnasium.spaces.Dict(
+            {
+                "observation": gymnasium.spaces.Box(low=0, high=1, shape=(obs_size,)),
+                "action_mask": gymnasium.spaces.Box(
+                    low=0, high=1, shape=(num_actions,), dtype=np.int8
+                ),
+            }
+        )
         act_space = gymnasium.spaces.Discrete(num_actions)
         new_type = f"type{type_id}"
         self.types.append(new_type)