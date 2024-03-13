918ba128641ae287c441129d626683159831da0a
Elliot Tower <elliot@elliottower.com>
2024-03-13 12:29:56 -0400

    Fix obs space documentation in simple_adversary.py (#1189)

--- pettingzoo/mpe/simple_adversary/simple_adversary.py
+++ pettingzoo/mpe/simple_adversary/simple_adversary.py
@@ -28,7 +28,7 @@ In this environment, there is 1 adversary (red), N good agents (green), N landma
 target landmark, but negatively rewarded based on how close the adversary is to the target landmark. The adversary is rewarded based on distance to the target, but it doesn't know which landmark is the target landmark. All rewards are unscaled Euclidean distance (see main MPE documentation for
 average distance). This means good agents have to learn to 'split up' and cover all landmarks to deceive the adversary.
 
-Agent observation space: `[self_pos, self_vel, goal_rel_position, landmark_rel_position, other_agent_rel_positions]`
+Agent observation space: `[goal_rel_position, landmark_rel_position, other_agent_rel_positions]`
 
 Adversary observation space: `[landmark_rel_position, other_agents_rel_positions]`
 
