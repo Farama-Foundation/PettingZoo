5cfb1f7ad130b08f0b8d83a4d5fedaf180e565a7
David GERARD <david.gerard.23@ucl.ac.uk>
2025-03-27 15:56:43 +0000

    feat(mpe): add deprecation warning to all environments

--- pettingzoo/mpe/__init__.py
+++ pettingzoo/mpe/__init__.py
@@ -3,3 +3,7 @@ from pettingzoo.utils.deprecated_module import deprecated_handler
 
 def __getattr__(env_name):
     return deprecated_handler(env_name, __path__, __name__)
+
+
+# TODO: in a future release, remove all the MPE environments and uncomment the following lines to raise a warning
+# instead of importing the environments
