405e71c912dc3f787bb12c7f8463f18fcce31bb1
David Ackerman <145808634+dm-ackerman@users.noreply.github.com>
2025-05-15 04:18:06 -0400

    Add type hinting to Atari envs (#1285)

--- pettingzoo/atari/__init__.py
+++ pettingzoo/atari/__init__.py
@@ -1,5 +1,9 @@
-from pettingzoo.utils.deprecated_module import deprecated_handler
+from __future__ import annotations
+
+from types import ModuleType
+
+from pettingzoo.utils.deprecated_module import DeprecatedModule, deprecated_handler
 
 
-def __getattr__(env_name):
+def __getattr__(env_name: str) -> DeprecatedModule | ModuleType:
     return deprecated_handler(env_name, __path__, __name__)
