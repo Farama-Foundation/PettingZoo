cc155ff46c445b401ed089e80f6297fc00a5c9e7
virgilt <travisjvirgil@gmail.com>
2026-04-22 17:51:04 -0400

    Remove MPE references.

--- pettingzoo/mpe/_mpe_utils/scenario.py
+++
@@ -1,6 +0,0 @@
-class BaseScenario:  # defines scenario upon which the world is built
-    def make_world(self):  # create elements of the world
-        raise NotImplementedError()
-
-    def reset_world(self, world, np_random):  # create initial conditions of the world
-        raise NotImplementedError()
