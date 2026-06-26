34a74346335bdeeac8d1b9d70bab80c61d7c37cd
Philip Sinitsin <ph.sinitsyn@gmail.com>
2026-06-26 16:23:20 +0100

    Add state() support to the pursuit environment (#1342)

--- pettingzoo/sisl/all_modules.py
+++ pettingzoo/sisl/all_modules.py
@@ -1,6 +1,6 @@
-from pettingzoo.sisl import multiwalker_v9, pursuit_v4
+from pettingzoo.sisl import multiwalker_v9, pursuit_v5
 
 sisl_environments = {
     "sisl/multiwalker_v9": multiwalker_v9,
-    "sisl/pursuit_v4": pursuit_v4,
+    "sisl/pursuit_v5": pursuit_v5,
 }
