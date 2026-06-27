93c4eb3c13bc3dbf662cbf3236d75b703c2fd7a5
virgilt <travisjvirgil@gmail.com>
2026-06-27 15:03:31 -0400

    trailing whitespace.

--- docs/_scripts/gen_envs_mds.py
+++ docs/_scripts/gen_envs_mds.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 
             if not full_env_names:
                 continue
-                
+
             env_name_version = full_env_names[0].split("/")[1]
             if env_type == "classic" and env_name != "rps":
                 with open(f"{docs_dir}/code_examples/usage_aec_action_mask.py") as f:
