c9040abb77d79f338dec3ac68981a03baf9d7fc8
virgilt <travisjvirgil@gmail.com>
2026-06-27 13:35:53 -0400

    fix(docs): skip test files when generating environment docs.

--- docs/_scripts/gen_envs_mds.py
+++ docs/_scripts/gen_envs_mds.py
@@ -98,6 +98,10 @@ if __name__ == "__main__":
                 for full_name in all_environments.keys()
                 if env_name == full_name.split("/")[1].rsplit("_", 1)[0]
             ]
+
+            if not full_env_names:
+                continue
+                
             env_name_version = full_env_names[0].split("/")[1]
             if env_type == "classic" and env_name != "rps":
                 with open(f"{docs_dir}/code_examples/usage_aec_action_mask.py") as f:
