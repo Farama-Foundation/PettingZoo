495c9cbddb50f29c9c7023cb83b2f83c90326f6c
Hugo <hugo@hugohuang.com>
2026-07-08 02:49:15 +0100

    feat: add gymnasium-like registry feature (#1388)

--- pettingzoo/utils/all_modules.py
+++ pettingzoo/utils/all_modules.py
@@ -1,6 +1,7 @@
 from pettingzoo.atari.all_modules import atari_environments
 from pettingzoo.butterfly.all_modules import butterfly_environments
 from pettingzoo.classic.all_modules import classic_environments
+from pettingzoo.env_registry.registration import aec_registry, parallel_registry
 from pettingzoo.sisl.all_modules import sisl_environments
 
 all_prefixes = ["atari", "classic", "butterfly", "sisl"]
@@ -19,3 +20,14 @@ all_environments = {
     **classic_environments,
     **sisl_environments,
 }
+
+# Verify that every env declared in all_modules is registered
+_declared_ids = set(all_environments.keys())
+_missing_aec = _declared_ids - set(aec_registry.keys())
+_missing_parallel = _declared_ids - set(parallel_registry.keys())
+assert not _missing_aec, (
+    f"Environments missing from AEC registry: {sorted(_missing_aec)}"
+)
+assert not _missing_parallel, (
+    f"Environments missing from Parallel registry: {sorted(_missing_parallel)}"
+)
