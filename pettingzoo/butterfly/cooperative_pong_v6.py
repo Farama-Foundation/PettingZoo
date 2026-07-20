21e3c74b35f35d77abbb46f132ec07ef8d97478c
Hugo <hugo@hugohuang.com>
2026-07-20 11:50:56 +0100

    chore: deprecate old env creation API

--- pettingzoo/butterfly/cooperative_pong_v6.py
+++ pettingzoo/butterfly/cooperative_pong_v6.py
@@ -1,8 +1,13 @@
+import warnings
+
 from pettingzoo.butterfly.cooperative_pong.cooperative_pong import (
     env,
     parallel_env,
     raw_env,
 )
 from pettingzoo.butterfly.cooperative_pong.manual_policy import ManualPolicy
+from pettingzoo.utils.deprecated_module import CREATE_ENV_WITHOUT_REGISTRY
+
+warnings.warn(CREATE_ENV_WITHOUT_REGISTRY, DeprecationWarning, stacklevel=2)
 
 __all__ = ["ManualPolicy", "env", "parallel_env", "raw_env"]
